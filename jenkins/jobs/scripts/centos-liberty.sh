d9c92732a1aa3722ceda0f479fb957d6d5e5a943
John Trowbridge <trown@redhat.com>
2016-01-19 11:24:49 -0500

    Add jjb for delorean promote job
    The delorean promote job defines the criteria for
    promoting a new repo in delorean to 'current-passed-ci'.
    
    This jjb creates a multijob for both liberty and mitaka,
    with 5 phases:
    1. get the delorean hash of the latest delorean repo so
       that all sub jobs can use the same hash
    2. Build all images used in later jobs. This phase also
       runs a packstack allinone since it is essentially free,
       and it does not make sense to run the rest of the tests
       if something that simple fails.
    3. All other tests would go in this phase
    4. Upload the images used to the stable location
    5. Promote the repo hash tested to 'current-passed-ci'
    
    Change-Id: I0fb801caed1959a0aaaf4bb6519fa5d9c3a372da

---
+++ jenkins/jobs/scripts/centos-liberty.sh
@@ -0,0 +1,6 @@
+set -e
+export RDO_VERSION='centos-liberty';
+export DELOREAN_URL='http://trunk.rdoproject.org/centos7-liberty/current/delorean.repo';
+export RDO_VERSION_DIR='liberty';
+export DELOREAN_HOST='trunk.rdoproject.org'
+export HASH_FILE='delorean-liberty-hash'
---
+++ jenkins/jobs/scripts/centos-mitaka.sh
@@ -0,0 +1,6 @@
+set -e
+export RDO_VERSION='centos-master';
+export DELOREAN_URL='http://trunk.rdoproject.org/centos7/current/delorean.repo';
+export RDO_VERSION_DIR='mitaka';
+export DELOREAN_HOST='trunk.rdoproject.org'
+export HASH_FILE='/tmp/delorean_mitaka_hash'
