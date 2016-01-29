080e1773f6ac5683aba9fc83fa21edfffc951396
John Trowbridge <trown@redhat.com>
2016-01-29 09:28:50 -0500

    Promote only consistent repos
    As agreed on the last meeting[1], we will only promote a
    repo if it is fully consistent. This patch also adds a
    check that we have anything new to promote, because there
    is not any point to running the job otherwise.
    
    [1] http://meetbot.fedoraproject.org/rdo/2016-01-27/rdo_meeting_(2016-01-27).2016-01-27-15.01.log.html
    Change-Id: I9384d4e56eb2a8ca77586879f2241adf08a7b7d7

--- jenkins/jobs/scripts/centos-mitaka.sh
+++ jenkins/jobs/scripts/centos-mitaka.sh
@@ -1,6 +1,7 @@
 set -e
 export RDO_VERSION='centos-master'
-export DELOREAN_URL='http://trunk.rdoproject.org/centos7/current/delorean.repo'
+export DELOREAN_URL='http://trunk.rdoproject.org/centos7/consistent/delorean.repo'
+export LAST_PROMOTED_URL='http://trunk.rdoproject.org/centos7/current-passed-ci/delorean.repo'
 export RDO_VERSION_DIR='mitaka'
 export DELOREAN_HOST='trunk.rdoproject.org'
 export HASH_FILE='/tmp/delorean_mitaka_hash'
--- jenkins/jobs/scripts/promote-get-hash.sh
+++ jenkins/jobs/scripts/promote-get-hash.sh
@@ -1,3 +1,9 @@
-export HASH=`curl $DELOREAN_URL | grep baseurl | awk -F '/' '{ print $5"/"$6"/"$7 }'`
+export NEW_HASH=`curl $DELOREAN_URL | grep baseurl | awk -F '/' '{ print $5"/"$6"/"$7 }'`
+export OLD_HASH=`curl $LAST_PROMOTED_URL | grep baseurl | awk -F '/' '{ print $5"/"$6"/"$7 }'`
 
-echo "delorean_current_hash = $HASH" > $HASH_FILE
+# No need to run the whole promote pipeline if there is nothing new to promote
+if [ $OLD_HASH == $NEW_HASH ]; then
+    exit 23
+fi
+
+echo "delorean_current_hash = $NEW_HASH" > $HASH_FILE
