3f31da6ddc89b4a8289320ec1589e58990cd92c0
John Trowbridge <trown@redhat.com>
2016-03-14 08:44:55 -0400

    Change mitaka promote job to use mitaka repo
    The master trunk repo will start to have newton
    dependencies as projects start to branch stable/mitaka.
    
    This changes the mitaka promote job to use our new mitaka
    trunk repo.
    
    Change-Id: I62cef6c6eab7fa313caf05781deaf8d4f38ac34a

--- jenkins/jobs/scripts/centos-mitaka.sh
+++ jenkins/jobs/scripts/centos-mitaka.sh
@@ -1,7 +1,7 @@
 set -e
-export RDO_VERSION='centos-master'
-export DELOREAN_URL='http://trunk.rdoproject.org/centos7/consistent/delorean.repo'
-export LAST_PROMOTED_URL='http://trunk.rdoproject.org/centos7/current-passed-ci/delorean.repo'
+export RDO_VERSION='centos-mitaka'
+export DELOREAN_URL='http://trunk.rdoproject.org/centos7-mitaka/consistent/delorean.repo'
+export LAST_PROMOTED_URL='http://trunk.rdoproject.org/centos7-mitaka/current-passed-ci/delorean.repo'
 export RDO_VERSION_DIR='mitaka'
 export DELOREAN_HOST='trunk.rdoproject.org'
 export HASH_FILE='/tmp/delorean_mitaka_hash'
