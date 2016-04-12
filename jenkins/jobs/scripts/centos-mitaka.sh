e6cc3f1d6f5bba7cb61aab3c0d3a3f7fc207537d
John Trowbridge <trown@redhat.com>
2016-04-12 09:26:39 -0400

    Create DLRN promotion pipeline for master
    This also fixes a couple of minor issues with our
    promote jobs.
    
    - Use explicit trunk-primary.rdoproject.org URL
      This avoids us accidentally promoting the backup.
      We set it everywhere, so that the get repo phase
      fails when trunk-primary is down.
    
    - Change job timings
      We only need to run the master promote job every
      4 hours, since that is where the bulk of new commits
      happen. The stable jobs are set to run every 12 hours.
      We can always manually trigger stable jobs if there
      was an obvious transient issue in the previous run.
      In order to avoid all 3 jobs starting at the same
      time we use explicit times as follows:
      master = 0,4,8,12,16,20
      mitaka = 2,14
      liberty = 6,18
    
    Change-Id: I1137b157a1e7b4690438b69410867506fd239012

--- jenkins/jobs/scripts/centos-mitaka.sh
+++ jenkins/jobs/scripts/centos-mitaka.sh
@@ -1,7 +1,7 @@
 set -e
 export RDO_VERSION='centos-mitaka'
-export DELOREAN_URL='http://trunk.rdoproject.org/centos7-mitaka/consistent/delorean.repo'
-export LAST_PROMOTED_URL='http://trunk.rdoproject.org/centos7-mitaka/current-passed-ci/delorean.repo'
+export DELOREAN_HOST='trunk-primary.rdoproject.org'
+export DELOREAN_URL="http://$DELOREAN_HOST/centos7-mitaka/consistent/delorean.repo"
+export LAST_PROMOTED_URL="http://$DELOREAN_HOST/centos7-mitaka/current-passed-ci/delorean.repo"
 export RDO_VERSION_DIR='mitaka'
-export DELOREAN_HOST='trunk.rdoproject.org'
 export HASH_FILE='/tmp/delorean_mitaka_hash'
