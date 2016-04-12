10d8dda89bcd319fc6242a093fb4f041a2cc029d
John Trowbridge <trown@redhat.com>
2016-04-12 09:26:39 -0400

    Create DLRN promotion pipeline for master
    Change-Id: I1137b157a1e7b4690438b69410867506fd239012

---
+++ jenkins/jobs/scripts/centos-master.sh
@@ -0,0 +1,7 @@
+set -e
+export RDO_VERSION='centos-master'
+export DELOREAN_URL='http://trunk.rdoproject.org/centos7-master/consistent/delorean.repo'
+export LAST_PROMOTED_URL='http://trunk.rdoproject.org/centos7-master/current-passed-ci/delorean.repo'
+export RDO_VERSION_DIR='master'
+export DELOREAN_HOST='trunk.rdoproject.org'
+export HASH_FILE='/tmp/delorean_master_hash'
