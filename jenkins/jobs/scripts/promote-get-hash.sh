78aeff2bf2d4a30c2c6c0e3ca2779897a61dc529
David Moreau Simard <dms@redhat.com>
2016-04-25 15:20:32 -0400

    Revert "Use installed file to retrieve repository hash"
    It turns out that we can keep on using delorean.repo to parse the hash. It is synced to buildlogs repositories.
    
    This reverts commit 189433a7e134e7a56d7bd7ecea7f9302fbcb4343.
    
    Change-Id: Ia84added7a52f15683398c5a7ce8275b5433b26e

--- jenkins/jobs/scripts/promote-get-hash.sh
+++ jenkins/jobs/scripts/promote-get-hash.sh
@@ -1,9 +1,9 @@
-new_hash=$(get_trunk_repository_hash $DELOREAN_URL)
-old_hash=$(get_trunk_repository_hash $LAST_PROMOTED_URL)
+export NEW_HASH=`curl $DELOREAN_URL | grep baseurl | awk -F '/' '{ print $5"/"$6"/"$7 }'`
+export OLD_HASH=`curl $LAST_PROMOTED_URL | grep baseurl | awk -F '/' '{ print $5"/"$6"/"$7 }'`
 
 # No need to run the whole promote pipeline if there is nothing new to promote
-if [ $old_hash == $new_hash ]; then
+if [ $OLD_HASH == $NEW_HASH ]; then
     exit 23
 fi
 
-echo "delorean_current_hash = $new_hash" > $HASH_FILE
+echo "delorean_current_hash = $NEW_HASH" > $HASH_FILE
