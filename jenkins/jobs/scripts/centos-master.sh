ce400deeeaa598de81d070753a58953b04a5cecb
Wesley Hayutin <weshayutin@gmail.com>
2016-08-11 14:31:44 -0400

    Create an additional pipeline job that builds from tripleo promoted repos
    The current master promote job does not start from the tripleo promoted
    delorean repo ( centos7-master/current-tripleo/ ), the current job starts
    with master trunk ( centos7-master/consistent ).  The jobs used to promote
    content through the pipeline need to start where the previous pipeline promoted
    
    At the moment we are testing the following way
    upstream trunk tripleo ->  stop
    upstream trunk rdo -> RHOSP
    
    In this case we must test content starting w/ upstream tripleo -> rdo -> rhosp
    
    I have left a pipeline in rdo that is testing upstream trunk so that rdo
    developers, ci, test can check what is broken and what has been fixed.
    
    Depends-On-Review: https://review.openstack.org/354365
    Depends-On-Review: https://review.gerrithub.io/287153
    
    Change-Id: Ied08dc70b5eb79c639a201206330440069708b2c

--- jenkins/jobs/scripts/centos-master.sh
+++ jenkins/jobs/scripts/centos-master.sh
@@ -4,4 +4,5 @@ export DELOREAN_HOST='trunk-primary.rdoproject.org'
 export DELOREAN_URL="http://$DELOREAN_HOST/centos7-master/consistent/delorean.repo"
 export LAST_PROMOTED_URL="http://$DELOREAN_HOST/centos7-master/current-passed-ci/delorean.repo"
 export RDO_VERSION_DIR='master'
+export LOCATION='testing-consistent'
 export HASH_FILE='/tmp/delorean_master_hash'
