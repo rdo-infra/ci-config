0c9c6d8782d90c2d93daf7747ddee5780af85593
Wesley Hayutin <weshayutin@gmail.com>
2016-08-11 14:31:44 -0400

    Create promote-master-current-tripleo pipeline
    This additional pipeline uses the tripleo promoted delorean repo
    (centos7-master/current-tripleo/) and promotes a new repository as
    'current-tripleo-rdo' on the DLRN server.
    
    The current master promote job does not start from the tripleo promoted
    delorean repo (centos7-master/current-tripleo/), the current job starts
    with master trunk (centos7-master/consistent). The jobs used to promote
    content through the pipeline need to start where the previous pipeline
    promoted.
    
    At the moment we are testing the following way
    upstream trunk tripleo -> stop
    upstream trunk rdo -> RHOSP
    
    In this case we must test content starting with upstream
    tripleo -> rdo -> rhosp
    
    I have left a pipeline in rdo that is testing upstream trunk so that rdo
    developers, ci, test can check what is broken and what has been fixed.
    
    Depends-On-Review: https://review.openstack.org/354365
    Depends-On-Review: https://review.gerrithub.io/287153
    
    Change-Id: Ied08dc70b5eb79c639a201206330440069708b2c

--- jenkins/jobs/scripts/centos-master-current-tripleo.sh
+++ jenkins/jobs/scripts/centos-master-current-tripleo.sh
@@ -1,9 +1,9 @@
 set -e
 export RDO_VERSION='centos-master'
 export DELOREAN_HOST='trunk-primary.rdoproject.org'
-export DELOREAN_URL="http://$DELOREAN_HOST/centos7-master/consistent/delorean.repo"
-export LINKNAME='current-passed-ci'
+export DELOREAN_URL="http://$DELOREAN_HOST/centos7-master/current-tripleo/delorean.repo"
+export LINKNAME='current-tripleo-rdo'
 export LAST_PROMOTED_URL="http://$DELOREAN_HOST/centos7-master/$LINKNAME/delorean.repo"
 export RDO_VERSION_DIR='master'
-export LOCATION='testing-consistent'
-export HASH_FILE='/tmp/delorean_master_hash'
+export LOCATION='testing'
+export HASH_FILE='/tmp/delorean_master_current_tripleo_hash'
--- jenkins/jobs/scripts/centos-mitaka.sh
+++ jenkins/jobs/scripts/centos-mitaka.sh
@@ -2,6 +2,7 @@ set -e
 export RDO_VERSION='centos-mitaka'
 export DELOREAN_HOST='trunk-primary.rdoproject.org'
 export DELOREAN_URL="http://$DELOREAN_HOST/centos7-mitaka/consistent/delorean.repo"
-export LAST_PROMOTED_URL="http://$DELOREAN_HOST/centos7-mitaka/current-passed-ci/delorean.repo"
+export LINKNAME='current-passed-ci'
+export LAST_PROMOTED_URL="http://$DELOREAN_HOST/centos7-mitaka/$LINKNAME/delorean.repo"
 export RDO_VERSION_DIR='mitaka'
 export HASH_FILE='/tmp/delorean_mitaka_hash'
