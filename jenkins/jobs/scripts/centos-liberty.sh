361994c109286fb547f6f53f47bbc96ab603333e
Wesley Hayutin <weshayutin@gmail.com>
2016-08-11 14:31:44 -0400

    Create promote-master-current-tripleo pipeline
    This additional pipeline uses the tripleo promoted delorean repo
    (centos7-master/current-tripleo/) and promotes a new repository as
    'current-tripleo-rdo' on the DLRN server.
    
    The current master promote job does not start from the tripleo promoted
    delorean repo (centos7-master/current-tripleo/), the current job starts
    with master trunk (centos7-master/consistent). The jobs used to promote
    content through the pipeline need to start where the previous pipeline
    promoted.
    
    At the moment we are testing the following way
    upstream trunk tripleo -> stop
    upstream trunk rdo -> RHOSP
    
    In this case we must test content starting with upstream
    tripleo -> rdo -> rhosp
    
    I have left a pipeline in rdo that is testing upstream trunk so that rdo
    developers, ci, test can check what is broken and what has been fixed.
    
    Depends-On-Review: https://review.openstack.org/354365
    Depends-On-Review: https://review.gerrithub.io/287153
    
    Change-Id: Ied08dc70b5eb79c639a201206330440069708b2c

--- jenkins/jobs/scripts/centos-liberty.sh
+++ jenkins/jobs/scripts/centos-liberty.sh
@@ -2,6 +2,7 @@ set -e
 export RDO_VERSION='centos-liberty'
 export DELOREAN_HOST='trunk-primary.rdoproject.org'
 export DELOREAN_URL="http://$DELOREAN_HOST/centos7-liberty/consistent/delorean.repo"
-export LAST_PROMOTED_URL="http://$DELOREAN_HOST/centos7-liberty/current-passed-ci/delorean.repo"
+export LINKNAME='current-passed-ci'
+export LAST_PROMOTED_URL="http://$DELOREAN_HOST/centos7-liberty/$LINKNAME/delorean.repo"
 export RDO_VERSION_DIR='liberty'
 export HASH_FILE='/tmp/delorean_liberty_hash'
--- jenkins/jobs/scripts/promote-repo-promote.sh
+++ jenkins/jobs/scripts/promote-repo-promote.sh
@@ -1,3 +1,3 @@
 export PROMOTE_HASH=`echo $delorean_current_hash | awk -F '/' '{ print $3}'`
 
-ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i ~/.ssh/rhos-ci -p 3300 promoter@$DELOREAN_HOST "sudo /usr/local/bin/promote.sh $PROMOTE_HASH" $RDO_VERSION
+ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i ~/.ssh/rhos-ci -p 3300 promoter@$DELOREAN_HOST "sudo /usr/local/bin/promote.sh $PROMOTE_HASH" $RDO_VERSION $LINKNAME
