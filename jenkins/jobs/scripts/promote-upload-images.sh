5d14fdca025f1e3313ab7211274b0a1cfb2c88c6
John Trowbridge <trown@redhat.com>
2016-09-06 12:52:18 -0400

    Add RDO file server upload to promote image publishing
    Change-Id: I37e3f2de3e6e7d26df28d85f41a80dcf9620d540

--- jenkins/jobs/scripts/promote-upload-images.sh
+++ jenkins/jobs/scripts/promote-upload-images.sh
@@ -1,4 +1,16 @@
+image_path="$RDO_VERSION_DIR/$BUILD_SYS/$LOCATION"
+scp_cmd='scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null'
+
 mkdir -p stable
-rsync -av rdo@artifacts.ci.centos.org::rdo/images/$RDO_VERSION_DIR/delorean/$LOCATION/testing/ stable/
-rsync -av stable/ rdo@artifacts.ci.centos.org::rdo/images/$RDO_VERSION_DIR/delorean/$LOCATION/stable/
+
+# get testing images from the ci.centos artifacts server
+rsync -av rdo@artifacts.ci.centos.org::rdo/images/$image_path/testing/ stable/
+
+# push images to ci.centos artifacts server
+rsync -av stable/ rdo@artifacts.ci.centos.org::rdo/images/$image_path/stable/
+
+# push images to RDO file server
+$scp_cmd stable/* images.rdoproject.org:/var/www/html/images/$image_path/stable/
+
+# remove images from jenkins slave
 rm -rf stable
