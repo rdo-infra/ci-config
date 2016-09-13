5e1a6909607ee4f6af862f93bb05bd69e806aa99
Matt Young <myoung@redhat.com>
2016-09-13 12:17:48 -0400

    Change to a direct publish for testing images
    This commit eliminates the copy from build host to slave
    
    We publish the oooq image artifacts to 2 locations:
    
    - RDO File Server  (images.rdoproject.org)
    - centos artifacts (artifacts.ci.centos.org)
    
    Note: This is untested presently against the actual servers
          in RDO infra.  Need eyeballs (trown, weshay) with the
          ability to play up there.  I did test this (substituting
          destinations) on local VM's
    
    Change-Id: Iad1fb20d363561a97db0320e4a307ab923c16d4d

--- jenkins/jobs/scripts/tripleo-quickstart-publish-testing-images.sh
+++ jenkins/jobs/scripts/tripleo-quickstart-publish-testing-images.sh
@@ -1,21 +1,42 @@
+set -eux
+
 export VIRTHOST=$(head -n1 $WORKSPACE/virthost)
+
+# this script is injected via raw-escape (in JJB).  It assumes a few variables are set.
 echo $VIRTHOST
+echo $RDO_VERSION_DIR
+echo $BUILD_SYS
+echo $LOCATION
 
-image_path="$RDO_VERSION_DIR/$BUILD_SYS/$LOCATION/testing/"
+# relative path used to publish images
+dest_image_path="$RDO_VERSION_DIR/$BUILD_SYS/$LOCATION/testing/"
 
-mkdir -p testing
+# ci.centos *MUST* use rsync (note "::", see rsync man page)
+dest_centos_artifacts="rdo@artifacts.ci.centos.org::rdo/images/$dest_image_path"
 
-# get images from the virthost
-scp_cmd='scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null'
-for image in undercloud.qcow2 ironic-python-agent.tar overcloud-full.tar; do
-    $scp_cmd root@$VIRTHOST:/var/lib/oooq-images/$image* ./testing/
-done
+# RDO file server can use scp
+dest_rdo_filer="images.rdoproject.org:/var/www/html/images/$dest_image_path"
 
-# push images to ci.centos artifacts server
-rsync -av testing/ rdo@artifacts.ci.centos.org::rdo/images/$image_path
+# where the files are parked on the virthost post-build
+virthost_source_location="/var/lib/oooq-images"
 
-# push images to RDO file server
-$scp_cmd testing/* images.rdoproject.org:/var/www/html/images/$image_path
+# what to archive.
+artifact_list="\
+undercloud.qcow2 \
+undercloud.qcow2.md5 \
+ironic-python-agent.tar \
+ironic-python-agent.tar.md5 \
+overcloud-full.tar \
+overcloud-full.tar.md5 \
+artib-logs.tar.gz \
+artib-logs.tar.gz.md5"
 
-# remove images from jenkins slave
-rm -rf testing
+echo $artifact_list
+
+# push --> artifacts server (rsync)
+ssh root@$VIRTHOST "cd $virthost_source_location && rsync -av $artifact_list $dest_centos_artifacts"
+
+# push --> RDO file server (scp)
+scp_opts="-v -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null"
+ssh root@$VIRTHOST "cd $virthost_source_location && scp $scp_opts $artifact_list $dest_rdo_filer"
+
