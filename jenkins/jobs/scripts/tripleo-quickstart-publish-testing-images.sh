161da24976790f762f2065705957803cdfef54d9
Matt Young <myoung@redhat.com>
2016-09-13 12:17:48 -0400

    Change to a direct publish for testing images
    This commit eliminates the copy from build host to slave
    
    We publish the oooq image artifacts to 2 locations:
    
    - RDO File Server  (images.rdoproject.org)
    - centos artifacts (artifacts.ci.centos.org)
    
    Note: This is untested presently against the actual servers
          in RDO infra.  Need eyeballs (trown, weshay) with the
          ability to play up there.  I did test this (substituting
          destinations) on local VM's
    
    Change-Id: Iad1fb20d363561a97db0320e4a307ab923c16d4d

--- jenkins/jobs/scripts/tripleo-quickstart-publish-testing-images.sh
+++ jenkins/jobs/scripts/tripleo-quickstart-publish-testing-images.sh
@@ -1,21 +1,56 @@
+set -eux
+
 export VIRTHOST=$(head -n1 $WORKSPACE/virthost)
+
+# this script is injected via raw-escape (in JJB).  It assumes a few variables are set.
 echo $VIRTHOST
+echo $RDO_VERSION_DIR
+echo $BUILD_SYS
+echo $LOCATION
 
-image_path="$RDO_VERSION_DIR/$BUILD_SYS/$LOCATION/testing/"
+# relative path used to publish images
+dest_image_path="$RDO_VERSION_DIR/$BUILD_SYS/$LOCATION/testing/"
 
-mkdir -p testing
+# ci.centos *MUST* use rsync (note "::", see rsync man page)
+dest_centos_artifacts="rdo@artifacts.ci.centos.org::rdo/images/$dest_image_path"
 
-# get images from the virthost
-scp_cmd='scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null'
-for image in undercloud.qcow2 ironic-python-agent.tar overcloud-full.tar; do
-    $scp_cmd root@$VIRTHOST:/var/lib/oooq-images/$image* ./testing/
-done
+# images.rdoproject will use rsync as well, but via ssh 
+dest_rdo_filer="fedora@images.rdoproject.org:/var/www/html/images/$dest_image_path"
 
-# push images to ci.centos artifacts server
-rsync -av testing/ rdo@artifacts.ci.centos.org::rdo/images/$image_path
+ssh_args="-v -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null"
 
-# push images to RDO file server
-$scp_cmd testing/* images.rdoproject.org:/var/www/html/images/$image_path
+# $RSYNC_PASSWORD is set as part of JJB / jenkins config, and is needed for artifacts server, but not for images.rdoproject.org
+rsync_base_cmd="rsync --verbose --archive --delay-updates"
+rsync_artifacts_cmd="RSYNC_PASSWORD=$RSYNC_PASSWORD $rsync_base_cmd"
 
-# remove images from jenkins slave
-rm -rf testing
+# where the files are parked on the virthost post-build
+virthost_source_location="/var/lib/oooq-images"
+
+# what to archive
+artifact_list="\
+undercloud.qcow2 \
+undercloud.qcow2.md5 \
+ironic-python-agent.tar \
+ironic-python-agent.tar.md5 \
+overcloud-full.tar \
+overcloud-full.tar.md5 \
+artib-logs.tar.gz \
+artib-logs.tar.gz.md5"
+
+echo $artifact_list
+
+# rsync isn't on duffy nodes by default
+ssh $ssh_args root@$VIRTHOST "yum install -y rsync"
+
+# push --> artifacts server (rsync)
+ssh $ssh_args root@$VIRTHOST "cd $virthost_source_location && $rsync_artifacts_cmd $artifact_list $dest_centos_artifacts"
+
+# TODO: we've talked about using ssh agent fwd'ing here, but it involves a number of config steps
+# TODO: on multiple hosts/nodes.  For now just doing the slightly less awesome "copy key and use it"
+
+# copy the key
+scp $ssh_args ~/.ssh/rdo-ci-public.pem root@$VIRTHOST:$virthost_source_location
+
+# use key to rsync to images.rdoproject.org
+ssh $ssh_args root@$VIRTHOST "cd $virthost_source_location && $rsync_base_cmd -e 'ssh -i rdo-ci-public.pem' $artifact_list $dest_rdo_filer"
+
