01f61d15e83d7b5f7906004ec74fbc74b5a10ca5
John Trowbridge <trown@redhat.com>
2016-10-04 12:12:54 -0400

    Update LOCATION and BUILD_SYS in cloudsig-testing jobs
    We rely on the LOCATION variable in particular in the image consuming
    jobs, in order to provide quickstart with the correct content.
    
    The original patch reused "consistent" for location, even though this
    does not make any since outside of a DLRN context. That also had the
    side effect of clashing with the existing "consistent" release configs
    in tripleo-quickstart.
    
    This patch updates the image path to be: $RELEASE/cbs/cloudsig-testing
    
    This allows us to create "cloudsig-testing" configs in quickstart which
    point to the correct testing content:
    
    https://review.openstack.org/#/c/381957/
    
    Change-Id: I5ff537bae07434185b98ac58facde412b7645939

--- jenkins/jobs/scripts/centos-cloudsig-testing-mitaka.sh
+++ jenkins/jobs/scripts/centos-cloudsig-testing-mitaka.sh
@@ -2,8 +2,8 @@ set -e
 export RDO_VERSION_DIR='mitaka'
 # The LOCATION var stores what repo symlink was used. It becomes part of the
 # path where images are stored.
-export LOCATION='consistent'
+export LOCATION='cloudsig-testing'
 # The BUILD_SYS var stores what build system was used. It becomes part of the
 # path where images are stored.
-export BUILD_SYS='cloudsig-testing'
+export BUILD_SYS='cbs'
 export HASH_FILE='/tmp/cloudsig-testing_mitaka_hash'
