02c3ca5efe8db770559429d789db622891f28cef
Wes Hayutin <weshayutin@gmail.com>
2017-01-12 12:11:52 -0500

    Do not fail job on duplicate delorean hash.
    We should be rerunning jobs to try and find consistency in builds
    The only job that would have a duplicate hash would be
    master-current-tripleo.  The other jobs would most likely always
    have a new hash.
    
    Change-Id: Idac8d3f175b9e93aa4eefee6a7d2fc5fa2268896

--- jenkins/jobs/scripts/promote-get-hash.sh
+++ jenkins/jobs/scripts/promote-get-hash.sh
@@ -1,11 +1,6 @@
 export NEW_HASH=`curl $DELOREAN_URL | grep baseurl | awk -F '/' '{ print $5"/"$6"/"$7 }'`
 export OLD_HASH=`curl $LAST_PROMOTED_URL | grep baseurl | awk -F '/' '{ print $5"/"$6"/"$7 }'`
 
-# No need to run the whole promote pipeline if there is nothing new to promote
-if [ $OLD_HASH == $NEW_HASH ]; then
-    exit 23
-fi
-
 echo "delorean_current_hash = $NEW_HASH" > $HASH_FILE
 
 # These variables are used for the tripleo-quickstart-publish-testing-images
