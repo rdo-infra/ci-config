076a7ecc13e08edca6f36a1b208d807f54e8bbfe
John Trowbridge <trown@redhat.com>
2017-01-26 15:07:58 -0500

    Update image building script to use build-images role
    This updates the master job image builder to use the new
    build-images role in tripleo-quickstart-extras. It replaces the
    fake ansible script we have that was used to test the methodology
    which became the new build-images role.
    
    This is also the first ci-script which will live in this repo
    rather than the ci-scripts directory in tripleo-quickstart. We have
    been asked to move all of the ci.centos related ci scripts from
    the upstream quickstart repo. As such, this will prove the approach.
    
    We do not put the script in jjb, but rather in a top level ci-scripts
    directory. This will allow us to setup a gate in jjb that runs on
    patches to this repo that change the ci-scripts. It is not possible
    to gate scripts which are directly in the jjb in this way.
    
    **NOTE**
     - checkout-patches.sh is temporary to include the unmerged quickstart
       patches for the new build-images role. We are under pressure to get
       the Ocata pipeline setup, and this is blocking that effort. The
       approach in the patches included is agreed upon, so they are very
       low risk.
    
    Change-Id: I6a487386d8d9cb0d1d46e3b9cf0b95bc5758d587

---
+++ ci-scripts/checkout-patches.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+# This is temporary, but needed to checki out the needed patches for the new image build role.
+# We need it to happen before we get a node from cico so that the correct changes end up in
+# the quickstart venv
+set -eux
+
+# TODO(trown): Actually merge needed patches for new image building role.
+pushd tripleo-quickstart
+git fetch https://git.openstack.org/openstack/tripleo-quickstart refs/changes/19/424319/2 && git checkout FETCH_HEAD
+popd
+
+pushd tripleo-quickstart-extras
+git fetch https://git.openstack.org/openstack/tripleo-quickstart-extras refs/changes/36/414336/15 && git checkout FETCH_HEAD
+popd
+
+cp $WORKSPACE/tripleo-quickstart/requirements.txt $WORKSPACE/local-requires.txt
+echo "file://$WORKSPACE/tripleo-quickstart-extras/#egg=tripleo-quickstart-extras" >> local-requires.txt
+
+bash $WORKSPACE/tripleo-quickstart/quickstart.sh \
+    --working-dir $WORKSPACE/.quickstart \
+    --no-clone \
+    --clean \
+    --bootstrap \
+    --requirements $WORKSPACE/local-requires.txt \
+    --playbook noop.yml \
+    localhost
+
