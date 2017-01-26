5943a81d1bb3616f5c5d98416352d4f5572c7b4a
John Trowbridge <trown@redhat.com>
2017-01-26 15:07:58 -0500

    WIP Update image building script to use build-images role
    Change-Id: I6a487386d8d9cb0d1d46e3b9cf0b95bc5758d587

--- ci-scripts/build-images.sh
+++ ci-scripts/build-images.sh
@@ -1,163 +1,83 @@
 #!/bin/bash
-
+# CI test that builds images for both promote and gate jobs.
+# For the promote jobs it uses the provided trunk repo hash and only builds
+# an image.
+# For the gate jobs it tests the current-passed-ci repo with a full deploy.
+# Usage: images.sh <release> <config> <job_type>
 set -eux
 
-curl -o centos.qcow2 http://cloud.centos.org/centos/7/images/CentOS-7-x86_64-GenericCloud.qcow2
+RELEASE=$1
+CONFIG=$2
+JOB_TYPE=$3
+WORKSPACE=${WORKSPACE:-"~/"}
 
-sudo yum install -y libguestfs-tools
-
-# Create build script
-cat <<EOS > build-images-vc.sh
-#!/bin/bash
-# virt-customize script to build master overcloud and ipa images in an isolated environment
-
-set -eux
-
-# NOTE(trown): DIB expects /dev/pts to exist and libguestfs is not mounting the guest with it
-# so we just manually mount it in the guest
-# This will be fixed when https://www.redhat.com/archives/libguestfs/2016-December/msg00024.html
-# is available in CentOS version of libguestfs
-mkdir /dev/pts
-mount devpts /dev/pts -t devpts
-
-curl -Lo /etc/yum.repos.d/delorean.repo \
-http://trunk.rdoproject.org/centos7-master/$delorean_current_hash/delorean.repo
-
-curl -Lo /etc/yum.repos.d/delorean-deps.repo \
-http://trunk.rdoproject.org/centos7-master/delorean-deps.repo
-
-yum install -y yum-plugin-priorities
-
-# Enable Storage/SIG Ceph repo
-rpm -q centos-release-ceph-jewel || sudo yum -y install --enablerepo=extras centos-release-ceph-jewel
-sudo sed -i -e 's%gpgcheck=.*%gpgcheck=0%' /etc/yum.repos.d/CentOS-Ceph-Jewel.repo
-
-yum update -y
-
-yum -y install python-tripleoclient
-
-export DIB_YUM_REPO_CONF="\$(ls /etc/yum.repos.d/delorean*) /etc/yum.repos.d/CentOS-Ceph-Jewel.repo"
-
-openstack overcloud image build \
-  --config-file /usr/share/tripleo-common/image-yaml/overcloud-images-centos7.yaml \
-  --config-file /usr/share/tripleo-common/image-yaml/overcloud-images.yaml
-
-EOS
-
-# Build images using virt-customize for isolation from build host
-sudo LIBGUESTFS_BACKEND=direct \
-virt-customize -m 16000 --smp 4 -a centos.qcow2 --run build-images-vc.sh
-
-# Extract images from isolated env
-copy_out_cmd='sudo LIBGUESTFS_BACKEND=direct virt-copy-out -a centos.qcow2'
-$copy_out_cmd /ironic-python-agent.initramfs .
-$copy_out_cmd /ironic-python-agent.kernel .
-$copy_out_cmd /overcloud-full.qcow2 .
-$copy_out_cmd /overcloud-full.initrd .
-$copy_out_cmd /overcloud-full.vmlinuz .
-
-# TODO(trown) extract logs as well.
-# For now just touch the files CI expects to be there
-touch artib-logs.tar.gz artib-logs.tar.gz.md5
-
-
-# Create undercloud from overcloud
-cp overcloud-full.qcow2 undercloud.qcow2
-qemu-img resize undercloud.qcow2 20G
-
-cat <<EOS > convert-overcloud-vc.sh
-#!/bin/bash
-
-# Script to convert an overcloud-full.qcow2 to an undercloud.qcow2 via virt-customize
-# Copied from https://github.com/openstack/tripleo-quickstart/blob/24437dd45e39dc1964aacf14dc5d73f5915ba3f5/roles/libvirt/setup/undercloud/templates/convert_image.sh.j2
-# With added repo setup, since overcloud-full is built with DIB_YUM_REPO_CONF
-
-set -eux
-
-FS_TYPE=`findmnt -o FSTYPE -fn /`
-
-if [ "\$FS_TYPE" = "xfs" ]; then
-    xfs_growfs /
-elif [ "\$FS_TYPE" = "ext4" ]; then
-    resize2fs /dev/sda
+if [ "$JOB_TYPE" = "gate" ] || [ "$JOB_TYPE" = "periodic" ]; then
+    dlrn_hash='current-passed-ci'
+elif [ "$JOB_TYPE" = "promote" ]; then
+    dlrn_hash=${delorean_current_hash:-"consistent"}
 else
-    echo "ERROR: Unknown filesystem, cannot resize."
+    echo "Job type must be one of gate, periodic, or promote"
     exit 1
 fi
 
-curl -Lo /etc/yum.repos.d/delorean.repo \
-http://trunk.rdoproject.org/centos7-master/$delorean_current_hash/delorean.repo
+mkdir -p $WORKSPACE
+pushd $WORKSPACE
 
-curl -Lo /etc/yum.repos.d/delorean-deps.repo \
-http://trunk.rdoproject.org/centos7-master/delorean-deps.repo
+# TODO(trown): Actually merge needed patches for new image building role.
+[[ ! -e tripleo-quickstart ]] && git clone https://github.com/openstack/tripleo-quickstart
+pushd tripleo-quickstart
+git fetch https://git.openstack.org/openstack/tripleo-quickstart refs/changes/19/424319/1 && git checkout FETCH_HEAD
+popd
 
-yum install -y yum-plugin-priorities
+[[ ! -e tripleo-quickstart-extras ]] && git clone https://github.com/openstack/tripleo-quickstart-extras
+pushd tripleo-quickstart-extras
+git fetch https://git.openstack.org/openstack/tripleo-quickstart-extras refs/changes/36/414336/5 && git checkout FETCH_HEAD
+popd
 
-# Enable Storage/SIG Ceph repo
-rpm -q centos-release-ceph-jewel || sudo yum -y install --enablerepo=extras centos-release-ceph-jewel
-sudo sed -i -e 's%gpgcheck=.*%gpgcheck=0%' /etc/yum.repos.d/CentOS-Ceph-Jewel.repo
+cp tripleo-quickstart/requirements.txt $WORKSPACE/local-requires.txt
+echo "file://$WORKSPACE/tripleo-quickstart-extras/#egg=tripleo-quickstart-extras" >> local-requires.txt
 
-yum update -y
+bash tripleo-quickstart/quickstart.sh \
+    --working-dir $WORKSPACE/.quickstart \
+    --no-clone \
+    --clean \
+    --bootstrap \
+    --requirements $WORKSPACE/local-requires.txt \
+    --playbook noop.yml \
+    localhost
 
-yum remove -y cloud-init python-django-horizon openstack-dashboard
-yum install -y python-tripleoclient
-# NOTE(trown) Install tempest and test packages in a seperate yum transaction
-# so that we do not fail the conversion if this install fails. There is a period
-# after TripleO uploads a new image, but before the buildlogs repo gets synced,
-# where this will fail because we try to install older test packages than the
-# service packages already installed in the image.
-yum install -y openstack-tempest python-aodh-tests python-ceilometer-tests \
-python-heat-tests python-ironic-tests python-keystone-tests python-manila-tests \
-python-mistral-tests python-neutron-tests python-sahara-tests python-zaqar-tests \
-|| /bin/true
+# Only run the get-node script if running in CI
+if [ -v CI_ENV ]; then
+    bash tripleo-quickstart/ci-scripts/get-node.sh
 
-useradd stack
-echo "stack ALL=(root) NOPASSWD:ALL" > /etc/sudoers.d/stack
-chmod 0440 /etc/sudoers.d/stack
+    export VIRTHOST=$(head -n1 $WORKSPACE/virthost)
+    echo $VIRTHOST
+fi
 
-mkdir /home/stack/.ssh
+# (trown) This is so that we ensure separate ssh sockets for
+# concurrent jobs. Without this, two jobs running in parallel
+# would try to use the same undercloud-stack socket.
+socketdir=$(mktemp -d /tmp/sockXXXXXX)
+export ANSIBLE_SSH_CONTROL_PATH=$socketdir/%%h-%%r
 
-echo "127.0.0.1  localhost undercloud" > /etc/hosts
-echo "HOSTNAME=undercloud" >> /etc/sysconfig/network
-echo "undercloud" > /etc/hostname
+bash tripleo-quickstart/quickstart.sh \
+    --tags all \
+    --config $WORKSPACE/.quickstart/config/general_config/$CONFIG.yml \
+    -e dlrn_hash=$dlrn_hash \
+    --working-dir $WORKSPACE/.quickstart \
+    -e images_working_dir=/var/lib/oooq-images \
+    --playbook build-images-v2.yml \
+    --no-clone \
+    --release ${CI_ENV:+$CI_ENV/}$RELEASE \
+    $VIRTHOST
 
-chown -R stack:stack /home/stack/
-
-# Add a 4GB swap file to the Undercloud
-dd if=/dev/zero of=/swapfile bs=1024 count=4194304
-mkswap /swapfile
-chmod 600 /swapfile
-# Enable it on start
-echo "/swapfile swap swap defaults 0 0" >> /etc/fstab
-
-sed -i 's/SELINUX=enforcing/SELINUX=permissive/' /etc/selinux/config
-
-EOS
-
-sudo LIBGUESTFS_BACKEND=direct \
-virt-customize -m 16000 --smp 4 -a undercloud.qcow2 --run convert-overcloud-vc.sh \
---upload ./ironic-python-agent.initramfs:/home/stack/ironic-python-agent.initramfs \
---upload ./ironic-python-agent.kernel:/home/stack/ironic-python-agent.kernel \
---upload ./overcloud-full.qcow2:/home/stack/overcloud-full.qcow2 \
---upload ./overcloud-full.initrd:/home/stack/overcloud-full.initrd \
---upload ./overcloud-full.vmlinuz:/home/stack/overcloud-full.vmlinuz \
---run-command 'chown stack:stack /home/stack/*'
-
-# Sparsify the undercloud image
-sudo LIBGUESTFS_BACKEND=direct \
-virt-sparsify --tmp ./ \
---check-tmpdir fail \
---compress undercloud.qcow2 undercloud-compressed.qcow2
-
-rm -f undercloud.qcow2
-mv undercloud-compressed.qcow2 undercloud.qcow2
-
-# tar and create md5s for all images
-tar -cf ironic-python-agent.tar ironic-python-agent.initramfs ironic-python-agent.kernel
-tar -cf overcloud-full.tar overcloud-full.qcow2 overcloud-full.initrd overcloud-full.vmlinuz
-
-md5sum ironic-python-agent.tar > ironic-python-agent.tar.md5
-md5sum overcloud-full.tar > overcloud-full.tar.md5
-md5sum undercloud.qcow2 > undercloud.qcow2.md5
-
-
+if [ "$JOB_TYPE" = "gate" ] || [ "$JOB_TYPE" = "periodic" ]; then
+    bash tripleo-quickstart/quickstart.sh \
+        --tags all \
+        --no-clone \
+        --config $WORKSPACE/.quickstart/config/general_config/$CONFIG.yml \
+        --working-dir $WORKSPACE/.quickstart \
+        --release ${CI_ENV:+$CI_ENV/}$RELEASE \
+        --extra-vars undercloud_image_url="file:///var/lib/oooq-images/undercloud.qcow2" \
+        $VIRTHOST
+fi
