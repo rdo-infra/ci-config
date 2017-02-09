88a87a4eda181450a9d4aa62640877a554492e7f
John Trowbridge <trown@redhat.com>
2017-02-09 20:01:14 +0000

    Remove patches required for build-images role
    The checkout-patches.sh script is left in place to make it simple
    to add any "hotfix" patches we need in RDO CI later.
    
    Change-Id: I6336f8c43f0128139cbaa1cbe4c551bb5260012b

--- ci-scripts/checkout-patches.sh
+++ ci-scripts/checkout-patches.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
-# This is temporary, but needed to checki out the needed patches for the new image build role.
+# This can be used when we need some temporary quick fix for CI while we wait for the change
+# to get merged upstream.
 # We need it to happen before we get a node from cico so that the correct changes end up in
 # the quickstart venv
 set -eux
 
-# TODO(trown): Actually merge needed patches for new image building role.
-pushd tripleo-quickstart
-git fetch https://git.openstack.org/openstack/tripleo-quickstart refs/changes/19/424319/3 && git checkout FETCH_HEAD
+pushd $WORKSPACE/tripleo-quickstart
+# Add git checkouts for tripleo-quickstart here
 popd
 
-pushd tripleo-quickstart-extras
-git fetch https://git.openstack.org/openstack/tripleo-quickstart-extras refs/changes/36/414336/15 && git checkout FETCH_HEAD
+pushd $WORKSPACE/tripleo-quickstart-extras
+# Add git checkouts for tripleo-quickstart-extras here
 popd
 
 cp $WORKSPACE/tripleo-quickstart/requirements.txt $WORKSPACE/local-requires.txt
-echo "file://$WORKSPACE/tripleo-quickstart-extras/#egg=tripleo-quickstart-extras" >> local-requires.txt
+echo "file://$WORKSPACE/tripleo-quickstart-extras/#egg=tripleo-quickstart-extras" >> $WORKSPACE/local-requires.txt
 
 bash $WORKSPACE/tripleo-quickstart/quickstart.sh \
     --working-dir $WORKSPACE/.quickstart \
