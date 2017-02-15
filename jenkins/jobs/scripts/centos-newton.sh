ff40df56f3982d59d7c3082322f0d0da868e76f9
Alfredo Moralejo <amoralej@redhat.com>
2017-02-15 13:33:14 +0100

    Pin tempest version for RDO Trunk Newton jobs
    Tempest introduced a change not compatible with plugins
    in stable/newton branches after 14.0.0 release in:
    
    https://github.com/openstack/tempest/commit/152f49d0549cde9bea630614a64aefb1626358d7
    
    This patch pins tempest to 14.0.0 for jobs running
    in RDO Trunk Newton pipeline.
    
    Change-Id: I29829a731896b223b0922df596acfd494d1596d5

--- jenkins/jobs/scripts/centos-newton.sh
+++ jenkins/jobs/scripts/centos-newton.sh
@@ -12,5 +12,5 @@ export LOCATION='consistent'
 # The BUILD_SYS var stores what build system was used. It becomes part of the
 # path where images are stored.
 export BUILD_SYS='delorean'
-export TEMPEST_VERSION='origin/master'
+export TEMPEST_VERSION='14.0.0'
 export HASH_FILE='/tmp/delorean_newton_hash'
