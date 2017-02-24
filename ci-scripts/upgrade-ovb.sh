de8275d4ef10eba8de0fd1129de47cedb6bac165
Ronelle Landy <rlandy@redhat.com>
2017-02-24 10:36:08 -0500

    Update OVB updates ci-script to include delorean_hash
    This review updates the ci-script to include all
    options required by OVB upgrade jobs.
    
    Note that the gate and delorean gate options are
    disabled until companion reviews are merged in
    tripleo-quickstart.
    
    Change-Id: I0c70b66203a90f010b2289201e57fb6d78194bd6

--- ci-scripts/upgrade-ovb.sh
+++ ci-scripts/upgrade-ovb.sh
@@ -8,10 +8,12 @@
 #        <job_type> \
 #        <major_upgrade>  \
 #        <target_upgrade_version> \
+#        <upgrade_delorean_hash> \
 #        <hw-env-dir> \
 #        <network-isolation> \
 #        <ovb-settings-file> \
-#        <ovb-creds-file>
+#        <ovb-creds-file> \
+#        <playbook>
 
 set -eux
 
@@ -21,17 +23,39 @@ CONFIG=$3
 JOB_TYPE=$4
 MAJOR_UPGRADE=$5
 TARGET_VERSION=$6
-HW_ENV_DIR=$7
-NETWORK_ISOLATION=$8
-OVB_SETTINGS_FILE=$9
-OVB_CREDS_FILE=${10}
+UPGRADE_DELOREAN_HASH=$7
+HW_ENV_DIR=$8
+NETWORK_ISOLATION=$9
+OVB_SETTINGS_FILE=${10}
+OVB_CREDS_FILE=${11}
+PLAYBOOK=${12}
 
-if [ "$JOB_TYPE" = "gate" ] || [ "$JOB_TYPE" = "periodic" ]; then
+if [ "$JOB_TYPE" = "gate" ] || \
+   [ "$JOB_TYPE" = "periodic" ] || \
+   [ "$JOB_TYPE" = "dlrn-gate" ]; then
     unset REL_TYPE
+    if [ "$RELEASE" = "master-tripleo-ci" ]; then
+        # we don't have a local mirror for the tripleo-ci images
+        unset CI_ENV
+    fi
+elif [ "$JOB_TYPE" = "dlrn-gate-check" ]; then
+    # setup a test patch to be built
+    export ZUUL_HOST=review.openstack.org
+    export ZUUL_CHANGES=openstack/tripleo-ui:master:refs/changes/25/422025/2
+    unset REL_TYPE
+    if [ "$RELEASE" = "master-tripleo-ci" ]; then
+        # we don't have a local mirror for the tripleo-ci images
+        unset CI_ENV
+    fi
 elif [ "$JOB_TYPE" = "promote" ]; then
-    REL_TYPE=$LOCATION
+    export REL_TYPE=$LOCATION
 else
-    echo "Job type must be one of gate, periodic, or promote"
+    echo "Job type must be one of the following:"
+    echo " * gate - for gating changes on tripleo-quickstart or -extras"
+    echo " * promote - for running promotion jobs"
+    echo " * periodic - for running periodic jobs"
+    echo " * dlrn-gate - for gating upstream changes"
+    echo " * dlrn-gate-check - for gating upstream changes"
     exit 1
 fi
 
@@ -41,17 +65,20 @@ fi
 socketdir=$(mktemp -d /tmp/sockXXXXXX)
 export ANSIBLE_SSH_CONTROL_PATH=$socketdir/%%h-%%r
 
-### DO WE NEED THIS STEP? upgrade.sh does not include it
-if [ "$JOB_TYPE" = "gate" ]; then
-    bash quickstart.sh \
-        --working-dir $WORKSPACE/ \
-        --no-clone \
-        --bootstrap \
-        --playbook gate-quickstart.yml \
-        --release ${CI_ENV:+$CI_ENV/}$RELEASE${REL_TYPE:+-$REL_TYPE} \
-        $OPT_ADDITIONAL_PARAMETERS \
-        localhost
-fi
+# Disabling until reviews in tripleo-quickstart are merged
+# preparation steps to run with the gated roles
+#if [ "$JOB_TYPE" = "gate" ] || [ "$JOB_TYPE" = "dlrn-gate-check" ]; then
+#    bash quickstart.sh \
+#        --working-dir $WORKSPACE/ \
+#        --no-clone \
+#        --bootstrap \
+#        --playbook gate-quickstart.yml \
+#        --release ${CI_ENV:+$CI_ENV/}$RELEASE${REL_TYPE:+-$REL_TYPE} \
+#        $OPT_ADDITIONAL_PARAMETERS \
+#        $VIRTHOST
+#fi
+
+# TODO: Add dlrn gate check
 
 bash quickstart.sh \
     --working-dir $WORKSPACE/ \
@@ -63,8 +90,9 @@ bash quickstart.sh \
     --extra-vars @$OVB_SETTINGS_FILE \
     --extra-vars @$OVB_CREDS_FILE \
     --extra-vars @$WORKSPACE/$HW_ENV_DIR/network_configs/$NETWORK_ISOLATION/env_settings.yml \
-    --playbook upgrade-baremetal.yml \
+    --playbook $PLAYBOOK \
     --release ${CI_ENV:+$CI_ENV/}$RELEASE${REL_TYPE:+-$REL_TYPE} \
+    --extra-vars upgrade_delorean_hash=$UPGRADE_DELOREAN_HASH \
     --extra-vars major_upgrade=$MAJOR_UPGRADE \
     --extra-vars target_upgrade_version=$TARGET_VERSION \
 localhost
