19ac823bc8789c1558d8ba6eb5387cce2db31145
David Moreau-Simard <dms@redhat.com>
2017-03-02 11:46:01 -0500

    Move weirdo generic jobs from duffy to openstack virtual machines
    We're having a hard time with bare metal hardware capacity in ci.c.o.
    This patch introduces a builder and a publisher to respectively
    create and destroy virtual machines for the purpose of jobs.
    
    Since WeIRDO already expects an inventory file in $WORKSPACE/hosts,
    this makes no difference to the jobs at all, we're just changing
    how we generate the hosts file and the target is now a VM instead
    of a bare metal.
    
    Considerations:
    - ~/.config/openstack/clouds.yaml exists with the right credentials
      on every jenkins slave
    - The keypair is set up on the target cloud
    - The image is set up on the target cloud
    - There is a log server set up with pubkey authentication for
      the "uploader" user
    
    Change-Id: If4b350fd9eee852a91e0a3df3137b23009c7a2db

---
+++ jenkins/jobs/scripts/weirdo-destroy-vm.sh
@@ -0,0 +1,110 @@
+#!/bin/bash
+set -ex
+WORKSPACE=${WORKSPACE:-/tmp}
+JOB_NAME=${JOB_NAME:-rdo-ci}
+BUILD_NUMBER=${BUILD_NUMBER:-000}
+ANSIBLE_HOSTS=${ANSIBLE_HOSTS:-$WORKSPACE/hosts}
+CLOUD_CONFIG=${CLOUD_CONFIG:-~/.config/openstack/clouds.yaml}
+
+# Ansible config
+CLOUD=${CLOUD:-trystack-temp}
+NAME="${JOB_NAME}-${BUILD_NUMBER}"
+TIMEOUT="120"
+VM_INFO="${WORKSPACE}/vminfo.json"
+
+if [ ! -f "${CLOUD_CONFIG}" ]; then
+    echo "Configuration file does not exist: ${CLOUD_CONFIG}"
+    exit 1
+fi
+
+if [ ! -f "${VM_INFO}" ]; then
+    echo "Virtual machine information does not exist: ${VM_INFO}"
+    exit 1
+fi
+
+pushd $WORKSPACE
+
+# Install dependencies
+[[ ! -d provision_venv ]] && virtualenv provision_venv
+source provision_venv/bin/activate
+pip install ansible==2.2.1.0 ara shade
+
+ara_location=$(python -c "import os,ara; print(os.path.dirname(ara.__file__))")
+export ANSIBLE_HOST_KEY_CHECKING=False
+export ANSIBLE_CALLBACK_PLUGINS="${ara_location}/plugins/callbacks"
+export ARA_DATABASE="sqlite:///${WORKSPACE}/${NAME}.sqlite"
+
+cat <<EOF >destroy-vm.yml
+- name: Validate cloud authentication
+  hosts: localhost
+  gather_facts: false
+  tasks:
+    - name: Validate cloud authentication
+      os_auth:
+        cloud: "${CLOUD}"
+
+- name: Collect logs
+  hosts: logserver
+  gather_facts: false
+  tasks:
+    - block:
+        - name: Look up job virtual machine
+          set_fact:
+            vm: "{{ lookup('file', '${VM_INFO}') | from_json }}"
+
+        - name: Create log destination directory
+          file:
+            path: "/var/www/html/{{ vm.openstack.metadata.job_name }}/{{ vm.openstack.metadata.build_number }}"
+            state: "directory"
+            recurse: "yes"
+
+        - name: Upload logs
+          synchronize:
+            recursive: "yes"
+            src: "/var/log/weirdo"
+            dest: "/var/www/html/{{ vm.openstack.metadata.job_name }}/{{ vm.openstack.metadata.build_number }}"
+          delegate_to: "{{ item }}"
+          with_items: "{{ groups['openstack_nodes'] }}"
+          ignore_errors: "yes"
+
+- name: Destroy job virtual machine
+  hosts: localhost
+  gather_facts: false
+  tasks:
+    - block:
+        - name: Look up job virtual machine
+          set_fact:
+            vm: "{{ lookup('file', '${VM_INFO}') | from_json }}"
+
+        - name: Destroy job virtual machine
+          os_server:
+            state: "absent"
+            cloud: "${CLOUD}"
+            name: "{{ vm.openstack.id }}"
+            timeout: "${TIMEOUT}"
+            wait: "yes"
+EOF
+
+ansible-playbook -i "${ANSIBLE_HOSTS}" destroy-vm.yml
+
+# Generate and upload ARA report
+ara generate html "${WORKSPACE}/ara"
+cat <<EOF >ara.yml
+- name: Upload ARA report
+  hosts: logserver
+  gather_facts: false
+  tasks:
+    - name: Look up job virtual machine
+      set_fact:
+        vm: "{{ lookup('file', '${VM_INFO}') | from_json }}"
+
+    - name: Upload ARA report
+      synchronize:
+        src: "${WORKSPACE}/ara"
+        dest: "/var/www/html/{{ vm.openstack.metadata.job_name }}/{{ vm.openstack.metadata.build_number }}/"
+EOF
+
+ansible-playbook -i "${ANSIBLE_HOSTS}" ara.yml
+
+deactivate
+popd
