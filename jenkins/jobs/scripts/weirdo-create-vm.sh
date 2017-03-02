9db264ea6580a580da235dacc3beb048185ccf5f
David Moreau-Simard <dms@redhat.com>
2017-03-02 11:46:01 -0500

    Move weirdo generic jobs from duffy to openstack virtual machines
    We're having a hard time with bare metal hardware capacity in ci.c.o.
    This patch introduces a builder and a publisher to respectively
    create and destroy virtual machines for the purpose of jobs.
    
    Since WeIRDO already expects an inventory file in $WORKSPACE/hosts,
    this makes no difference to the jobs at all, we're just changing
    how we generate the hosts file and the target is now a VM instead
    of a bare metal.
    
    Considerations:
    - ~/.config/openstack/clouds.yaml exists with the right credentials
      on a given jenkins slave
    - A DIB image built with the "weirdo-dib.sh" script is set up on
      the target cloud with the name "template-centos7-weirdo"
    - There is a logserver "logs.rdoproject.org" available for pulling
      logs from the provisioned virtual machine and for pushing the ara
      report from the jenkins slave
    
    Change-Id: If4b350fd9eee852a91e0a3df3137b23009c7a2db

---
+++ jenkins/jobs/scripts/weirdo-create-vm.sh
@@ -0,0 +1,133 @@
+#!/bin/bash
+set -ex
+WORKSPACE=${WORKSPACE:-/tmp}
+JOB_NAME=${JOB_NAME:-rdo-ci}
+BUILD_NUMBER=${BUILD_NUMBER:-001}
+ANSIBLE_HOSTS=${ANSIBLE_HOSTS:-$WORKSPACE/hosts}
+CLOUD_CONFIG=${CLOUD_CONFIG:-~/.config/openstack/clouds.yaml}
+LOGSERVER="logs.rdoproject.org ansible_user=uploader"
+
+# Ansible config
+CLOUD=${CLOUD:-rdo-cloud}
+NETWORK=${NETWORK:-private}
+NAME="${JOB_NAME}-${BUILD_NUMBER}"
+IMAGE="template-centos7-weirdo"
+TIMEOUT="120"
+FLAVOR="nodepool.std"
+VM_INFO="${WORKSPACE}/vminfo.json"
+
+if [ ! -f "${CLOUD_CONFIG}" ]; then
+    echo "Configuration file does not exist: ${CLOUD_CONFIG}"
+    exit 1
+fi
+
+pushd $WORKSPACE
+
+# Install dependencies
+[[ ! -d provision_venv ]] && virtualenv provision_venv
+source provision_venv/bin/activate
+pip install ansible==2.2.2.0 ara shade
+
+ara_location=$(python -c "import os,ara; print(os.path.dirname(ara.__file__))")
+export ANSIBLE_HOST_KEY_CHECKING=False
+export ANSIBLE_CALLBACK_PLUGINS="${ara_location}/plugins/callbacks"
+export ARA_DATABASE="sqlite:///${WORKSPACE}/${JOB_NAME}.sqlite"
+
+# Write the header of the hosts file
+cat << EOF > ${ANSIBLE_HOSTS}
+[logserver]
+${LOGSERVER}
+
+[openstack_nodes]
+EOF
+
+cat <<EOF >create-vm.yml
+- name: Create job virtual machine
+  hosts: localhost
+  gather_facts: false
+  tasks:
+    - name: Validate cloud authentication
+      os_auth:
+        cloud: "${CLOUD}"
+
+    # Be a bit resilient to failures by trying at least twice
+    - block:
+        - name: Create job virtual machine
+          os_server:
+            state: "present"
+            cloud: "${CLOUD}"
+            name: "${NAME}"
+            image: "${IMAGE}"
+            flavor: "${FLAVOR}"
+            network: "${NETWORK}"
+            reuse_ips: "no"
+            timeout: "${TIMEOUT}"
+            boot_from_volume: "yes"
+            terminate_volume: "yes"
+            volume_size: 100
+            wait: "yes"
+            meta:
+              hostname: "${NAME}"
+              job_name: "${JOB_NAME}"
+              build_number: "${BUILD_NUMBER}"
+          register: vm
+      rescue:
+        - name: Handling virtual machine creation failure
+          debug:
+            msg: "The virtual machine creation failed, trying again ..."
+
+        - name: Create job virtual machine (second attempt)
+          os_server:
+            state: "present"
+            cloud: "${CLOUD}"
+            name: "${NAME}"
+            image: "${IMAGE}"
+            flavor: "${FLAVOR}"
+            network: "${NETWORK}"
+            reuse_ips: "no"
+            timeout: "${TIMEOUT}"
+            boot_from_volume: "yes"
+            terminate_volume: "yes"
+            volume_size: 100
+            wait: "yes"
+            meta:
+              hostname: "${NAME}"
+              job_name: "${JOB_NAME}"
+              build_number: "${BUILD_NUMBER}"
+          register: vm
+
+    - name: Dump complete virtual machine info
+      vars:
+        ansible_python_interpreter: "/usr/bin/python"
+      copy:
+        content: "{{ vm | to_nice_json }}"
+        dest: "${VM_INFO}"
+
+    - name: Wait until server is up and runnning
+      local_action:
+        module: "wait_for"
+        port: "22"
+        host: "{{ vm.openstack.accessIPv4 }}"
+        search_regex: "OpenSSH"
+        delay: "10"
+
+    - name: Write inventory
+      vars:
+        ansible_python_interpreter: "/usr/bin/python"
+      lineinfile:
+        dest: "${ANSIBLE_HOSTS}"
+        line: >-
+          {{ vm.openstack.name }}
+          ansible_host={{ vm.openstack.accessIPv4 }}
+          ansible_user=centos
+          ansible_become=yes
+          ansible_become_user=root
+EOF
+
+ansible-playbook -i 'localhost' create-vm.yml
+
+# Test VM connectivity
+ansible -i ${ANSIBLE_HOSTS} openstack_nodes -m ping
+
+deactivate
+popd
---
+++ jenkins/jobs/scripts/weirdo-destroy-vm.sh
@@ -0,0 +1,135 @@
+#!/bin/bash
+set -ex
+WORKSPACE=${WORKSPACE:-/tmp}
+JOB_NAME=${JOB_NAME:-rdo-ci}
+BUILD_NUMBER=${BUILD_NUMBER:-001}
+ANSIBLE_HOSTS=${ANSIBLE_HOSTS:-$WORKSPACE/hosts}
+CLOUD_CONFIG=${CLOUD_CONFIG:-~/.config/openstack/clouds.yaml}
+LOGSERVER="logs.rdoproject.org ansible_user=uploader"
+
+# Ansible config
+CLOUD=${CLOUD:-rdo-cloud}
+NAME="${JOB_NAME}-${BUILD_NUMBER}"
+TIMEOUT="120"
+VM_INFO="${WORKSPACE}/vminfo.json"
+
+if [ ! -f "${CLOUD_CONFIG}" ]; then
+    echo "ERROR: Configuration file does not exist: ${CLOUD_CONFIG}"
+    exit 1
+fi
+
+if [ ! -f "${VM_INFO}" ]; then
+    echo "WARNING: Virtual machine information does not exist: ${VM_INFO}"
+fi
+
+if [ ! -f "${ANSIBLE_HOSTS}" ]; then
+    echo "WARNING: Ansible hosts file does not exist: ${ANSIBLE_HOSTS}"
+    echo "WARNING: Creating an Ansible hosts file for log collection..."
+
+    # Write the header of the hosts file
+    cat << EOF > ${ANSIBLE_HOSTS}
+[logserver]
+${LOGSERVER}
+
+[openstack_nodes]
+EOF
+fi
+
+pushd $WORKSPACE
+
+# Install dependencies
+[[ ! -d provision_venv ]] && virtualenv provision_venv
+source provision_venv/bin/activate
+pip install ansible==2.2.2.0 ara shade
+
+ara_location=$(python -c "import os,ara; print(os.path.dirname(ara.__file__))")
+export ANSIBLE_HOST_KEY_CHECKING=False
+export ANSIBLE_CALLBACK_PLUGINS="${ara_location}/plugins/callbacks"
+export ARA_DATABASE="sqlite:///${WORKSPACE}/${JOB_NAME}.sqlite"
+
+cat <<EOF >collect-logs.yml
+- name: Collect logs
+  hosts: logserver
+  gather_facts: false
+  tasks:
+    - block:
+        - name: Create log destination directory
+          file:
+            path: "/var/www/html/${JOB_NAME}/${BUILD_NUMBER}"
+            state: "directory"
+            recurse: "yes"
+
+        - name: Look up job virtual machine
+          set_fact:
+            vm: "{{ lookup('file', '${VM_INFO}') | from_json }}"
+
+        # Synchronize doesn't prefix the username to the dest when using delegate_to
+        # https://github.com/ansible/ansible/issues/16215
+        - name: Pull logs from VM to logserver
+          vars:
+            ssh_opts: "ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no"
+            src: "/var/log/weirdo/./"
+            host: "{{ hostvars[item]['ansible_user'] }}@{{ vm.openstack.accessIPv4 }}"
+            path: "/var/www/html/${JOB_NAME}/${BUILD_NUMBER}"
+          shell: |
+            rsync -e "{{ ssh_opts }}" -avzR {{ host }}:{{ src }} {{ path }}
+          with_items: "{{ groups['openstack_nodes'] }}"
+          when: vm is defined
+      ignore_errors: "yes"
+EOF
+
+ansible-playbook -i "${ANSIBLE_HOSTS}" collect-logs.yml
+
+cat <<EOF >destroy-vm.yml
+- name: Destroy job virtual machine
+  hosts: localhost
+  gather_facts: false
+  tasks:
+    - name: Validate cloud authentication
+      os_auth:
+        cloud: "${CLOUD}"
+      no_log: "yes"
+
+    # Be a bit resilient to failures by trying at least twice
+    - block:
+        - name: Destroy job virtual machine
+          os_server:
+            state: "absent"
+            cloud: "${CLOUD}"
+            name: "${NAME}"
+            timeout: "${TIMEOUT}"
+            wait: "yes"
+      rescue:
+        - name: Handling virtual machine deletion failure
+          debug:
+            msg: "The virtual machine creation failed, trying again ..."
+
+        - name: Destroy job virtual machine
+          os_server:
+            state: "absent"
+            cloud: "${CLOUD}"
+            name: "${NAME}"
+            timeout: "${TIMEOUT}"
+            wait: "yes"
+            ignore_errors: "yes"
+EOF
+
+ansible-playbook -i "${ANSIBLE_HOSTS}" destroy-vm.yml
+
+# Generate and upload ARA report
+ara generate html "${WORKSPACE}/ara"
+cat <<EOF >ara.yml
+- name: Upload ARA report
+  hosts: logserver
+  gather_facts: false
+  tasks:
+    - name: Upload ARA report
+      synchronize:
+        src: "${WORKSPACE}/ara"
+        dest: "/var/www/html/${JOB_NAME}/${BUILD_NUMBER}/"
+EOF
+
+ansible-playbook -i "${ANSIBLE_HOSTS}" ara.yml
+
+deactivate
+popd
