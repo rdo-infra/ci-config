dc89d4d2472ab2aa889c69d8a904c82c5a7681e9
Attila Darazs <adarazs@redhat.com>
2017-07-18 14:32:50 +0200

    WIP: Add DLRN API reporting for quickstart promote jobs
    Change-Id: I8f6b9f3a9c29581fa92ed4c0dd277a3cc8534a85

---
+++ jenkins/jobs/scripts/dlrn-api-promote.sh
@@ -0,0 +1,16 @@
+curl -sLo $BUILD_TAG.yaml ${DELOREAN_URL/delorean.repo/commit.yaml}
+
+commit_hash=$(cat $BUILD_TAG.yaml| $WORKSPACE/bin/shyaml get-value commits.0.commit_hash)
+distro_hash=$(cat $BUILD_TAG.yaml| $WORKSPACE/bin/shyaml get-value commits.0.distro_hash)
+
+# DLRNAPI_PASSWD is provided by a credential binding
+$WORKSPACE/bin/dlrnapi report-result \
+    --url https://$DELOREAN_HOST/api-${RDO_VERSION} \
+    --username ciuser \
+    --password $DLRNAPI_PASSWD \
+    --job-id $JOB_NAME \
+    --commit-hash $commit_hash \
+    --distro-hash $distro_hash \
+    --timestamp $(date +%s) \
+    --info-url https://ci.centos.org/artifacts/rdo/$BUILD_TAG/console.txt.gz \
+    --success $JOB_SUCCESS
