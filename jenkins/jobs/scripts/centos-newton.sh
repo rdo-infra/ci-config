bcd70953fd69a2d375f81c301b6eca8b796becda
Alfredo Moralejo <amoralej@redhat.com>
2017-08-17 20:06:14 +0200

    Use https for trunk-primary.rdoproject.org and use redirect in curl
    https have been implemented in trunk-primary.rdoproject.org and every
    request to http is redirected to https. This patch changes urls to
    use https and makes curl to follow redirects in case we redirect it
    in future.
    
    Change-Id: Id8c7263c95835d32543c75e3c48d47993899e4c4

--- jenkins/jobs/scripts/centos-newton.sh
+++ jenkins/jobs/scripts/centos-newton.sh
@@ -2,9 +2,9 @@ set -e
 export RDO_VERSION='centos-newton'
 export DELOREAN_HOST='trunk-primary.rdoproject.org'
 export DELOREAN_PUBLIC_HOST='trunk.rdoproject.org'
-export DELOREAN_URL="http://$DELOREAN_HOST/centos7-newton/consistent/delorean.repo"
+export DELOREAN_URL="https://$DELOREAN_HOST/centos7-newton/consistent/delorean.repo"
 export LINKNAME='current-passed-ci'
-export LAST_PROMOTED_URL="http://$DELOREAN_HOST/centos7-newton/$LINKNAME/delorean.repo"
+export LAST_PROMOTED_URL="https://$DELOREAN_HOST/centos7-newton/$LINKNAME/delorean.repo"
 export RDO_VERSION_DIR='newton'
 # The LOCATION var stores what repo symlink was used. It becomes part of the
 # path where images are stored.
--- jenkins/jobs/scripts/promote-get-hash.sh
+++ jenkins/jobs/scripts/promote-get-hash.sh
@@ -1,5 +1,5 @@
-export NEW_HASH=`curl $DELOREAN_URL | grep baseurl | awk -F '/' '{ print $5"/"$6"/"$7 }'`
-export OLD_HASH=`curl $LAST_PROMOTED_URL | grep baseurl | awk -F '/' '{ print $5"/"$6"/"$7 }'`
+export NEW_HASH=`curl -L $DELOREAN_URL | grep baseurl | awk -F '/' '{ print $5"/"$6"/"$7 }'`
+export OLD_HASH=`curl -L $LAST_PROMOTED_URL | grep baseurl | awk -F '/' '{ print $5"/"$6"/"$7 }'`
 
 # No need to run the whole promote pipeline if there is nothing new to promote
 if [ $OLD_HASH == $NEW_HASH ]; then
