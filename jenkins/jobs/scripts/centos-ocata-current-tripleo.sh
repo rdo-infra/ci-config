7292164237259e1a92f50e36a2ff4348d10bbaae
Alfredo Moralejo <amoralej@redhat.com>
2017-08-17 20:06:14 +0200

    Use https for trunk-primary.rdoproject.org and use redirect in curl
    https have been implemented in trunk-primary.rdoproject.org and every
    request to http is redirected to https. This patch changes urls to
    use https and makes curl to follow redirects in case we redirect it
    in future.
    
    Change-Id: Id8c7263c95835d32543c75e3c48d47993899e4c4

--- jenkins/jobs/scripts/centos-ocata-current-tripleo.sh
+++ jenkins/jobs/scripts/centos-ocata-current-tripleo.sh
@@ -2,12 +2,12 @@ set -e
 export RDO_VERSION='centos-ocata'
 export DELOREAN_HOST='trunk-primary.rdoproject.org'
 export DELOREAN_PUBLIC_HOST='trunk.rdoproject.org'
-export DELOREAN_URL="http://$DELOREAN_HOST/centos7-ocata/current-tripleo/delorean.repo"
+export DELOREAN_URL="https://$DELOREAN_HOST/centos7-ocata/current-tripleo/delorean.repo"
 # The softlinks used in promotion should be cumulative. This job starts w/ current-tripleo
 # If the job passes at the rdo level, rdo is appended to the new softlink name.
 # End result would be two seperate softlinks e.g. current-tripleo, and current-tripleo-rdo
 export LINKNAME='current-tripleo-rdo'
-export LAST_PROMOTED_URL="http://$DELOREAN_HOST/centos7-ocata/$LINKNAME/delorean.repo"
+export LAST_PROMOTED_URL="https://$DELOREAN_HOST/centos7-ocata/$LINKNAME/delorean.repo"
 export RDO_VERSION_DIR='ocata'
 # The LOCATION var is handed off to the ansible-role-tripleo-image build (atrib) role to define where testing/staged images are uploaded
 export LOCATION='current-tripleo'  #update to current-tripleo
