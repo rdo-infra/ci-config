59883c1bde66ac583970d643d74a2860be48d730
John Trowbridge <trown@redhat.com>
2017-09-01 16:56:00 -0400

    Images are in the home dir not /var/lib
    Change-Id: I11629db572747f9a2d71878d63f00e67febbaa19

--- ci-scripts/tripleo-upstream/upload-cloud-images.sh
+++ ci-scripts/tripleo-upstream/upload-cloud-images.sh
@@ -3,9 +3,7 @@ echo ======== UPLOAD CLOUD IMAGES
 
 export FULL_HASH=$(grep -o -E '[0-9a-f]{40}_[0-9a-f]{8}' < /etc/yum.repos.d/delorean.repo)
 
-# If I try to build images as upstream ( in /home/jenkins ) something will go wrong
-# we leave quickstart build in the default location. it proved to work
-pushd /var/lib/oooq-images/$RELEASE
+pushd $HOME
 
 ls *.tar
 chmod 600 $SSH_KEY
