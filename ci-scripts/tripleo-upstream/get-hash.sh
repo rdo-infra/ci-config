b3f67f3e41c5ed6735f016fcfff62fa03621e44c
Attila Darazs <adarazs@redhat.com>
2017-09-01 16:44:54 +0200

    tripleo: improve get-hash.sh and add promote-hash.sh
    * remove the promotion part from the get-hash.sh script so that it can be
      used in all jobs
    * add promote-hash.sh that is used only to promote a previously acquired
      hash with a new name
    * improve the way the hashes are acquired, use commit.yaml and parse it
      with shyaml
    * move the hash_info.sh file to the workspace to avoid collisions
    * remove unused variables from the hash_info.sh file
    
    Change-Id: I889cac125eeb0e662ee590e28ab413ca75a42d5b

--- ci-scripts/tripleo-upstream/get-hash.sh
+++ ci-scripts/tripleo-upstream/get-hash.sh
@@ -1,17 +1,23 @@
 set -e
 
-echo ======== PREPARE HASH PROMOTION
+echo ======== PREPARE HASH INFO
 
-export PROMOTE_NAME=tripleo-ci-testing
-mkdir $WORKSPACE/logs
+# The script assumes that RELEASE and PROMOTE_NAME is set
 
-curl -o /tmp/delorean.repo https://trunk.rdoproject.org/centos7-$RELEASE/consistent/delorean.repo
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+source $SCRIPT_DIR/dlrnapi_venv.sh
 
-export FULL_HASH=$(grep -o -E '[0-9a-f]{40}_[0-9a-f]{8}' < /tmp/delorean.repo)
-export HASH_PATH=$(grep -o -E '[a-f0-9]{2}/[a-f0-9]{2}/[0-9a-f]{40}_[0-9a-f]{8}' < /tmp/delorean.repo)
-export COMMIT_HASH=$(awk -F_ '{print $1}' <<<$FULL_HASH)
-delorean_commit_url="https://trunk.rdoproject.org/centos7-$RELEASE/$HASH_PATH/commit.yaml"
-export DISTRO_HASH=$(curl $delorean_commit_url | awk -F": " '/distro_hash/ {print $2}')
+trap deactivate_dlrnapi_venv EXIT
+activate_dlrnapi_venv
+
+set -u
+
+curl -sLo $WORKSPACE/commit.yaml https://trunk.rdoproject.org/centos7-$RELEASE/$PROMOTE_NAME/commit.yaml
+
+COMMIT_HASH=$(shyaml get-value commits.0.commit_hash < $WORKSPACE/commit.yaml)
+DISTRO_HASH=$(shyaml get-value commits.0.distro_hash < $WORKSPACE/commit.yaml)
+FULL_HASH=${COMMIT_HASH}_${DISTRO_HASH:0:8}
+
 export DLRNAPI_URL="https://trunk.rdoproject.org/api-centos-$RELEASE"
 if [ "$RELEASE" = "master" ]; then
     # for master we have two DLRN builders, use the "upper constraint" one that
@@ -19,30 +25,15 @@ if [ "$RELEASE" = "master" ]; then
     export DLRNAPI_URL="${DLRNAPI_URL}-uc"
 fi
 
-cat > /tmp/hash_info.sh << EOF
+cat > $WORKSPACE/hash_info.sh << EOF
 export DLRNAPI_URL=$DLRNAPI_URL
 export RELEASE=$RELEASE
-export HASH_PATH=$HASH_PATH
 export FULL_HASH=$FULL_HASH
 export COMMIT_HASH=$COMMIT_HASH
 export DISTRO_HASH=$DISTRO_HASH
-export PROMOTE_NAME=$PROMOTE_NAME
 EOF
 
-mv /tmp/delorean.repo $WORKSPACE/logs
-cp /tmp/hash_info.sh $WORKSPACE/logs
+mkdir -p $WORKSPACE/logs
+cp $WORKSPACE/hash_info.sh $WORKSPACE/logs
 
-virtualenv --system-site-packages /tmp/delorean
-source /tmp/delorean/bin/activate
-pip install ansible==2.3.0.0 dlrnapi_client
-
-# Assign label to latest hash using the DLRN API
-dlrnapi --url $DLRNAPI_URL \
-    --username review_rdoproject_org \
-    repo-promote \
-    --commit-hash $COMMIT_HASH \
-    --distro-hash $DISTRO_HASH \
-    --promote-name $PROMOTE_NAME
-
-deactivate
-echo ======== PREPARE HASH PROMOTION COMPLETED
+echo ======== PREPARE HASH INFO COMPLETE
