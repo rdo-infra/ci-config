d0f43d7c4661a1f4171b238f9b5e924b2e634501
Attila Darazs <adarazs@redhat.com>
2018-04-03 15:23:41 +0200

    Add a role for hubbot
    This change moves the GateStatus Limnoria plugin from my private
    repository into ci-config, and adds an instance to our tripleo-infra
    tenant that hosts hubbot.
    
    Change-Id: I0102fb50978353804aa4ee1fecb75003bccaafab

---
+++ ci-scripts/infra-setup/roles/hubbot/files/GateStatus/__init__.py
@@ -0,0 +1,30 @@
+"""
+A module to report gate job status
+"""
+
+import supybot
+import supybot.world as world
+
+# Use this for the version of this plugin.  You may wish to put a CVS keyword
+# in here if you\'re keeping the plugin in CVS or some similar system.
+__version__ = "%%VERSION%%"
+
+__author__ = supybot.Author('Attila Darazs')
+
+# This is a dictionary mapping supybot.Author instances to lists of
+# contributions.
+__contributors__ = {}
+
+from . import config
+from . import plugin
+from imp import reload
+reload(plugin) # In case we're being reloaded.
+
+if world.testing:
+    from . import test
+
+Class = plugin.Class
+configure = config.configure
+
+
+# vim:set shiftwidth=4 softtabstop=4 expandtab textwidth=79:
