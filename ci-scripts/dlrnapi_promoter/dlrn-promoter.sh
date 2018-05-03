5ad2749fbfb8be022ffe715357c207195a552c34
Quique Llorente <ellorent@redhat.com>
2018-05-03 10:34:34 +0200

    Run promoter sequancially between releases
    Simplify the promoter removing the lock and running it
    sequancially one release after another.
    
    It also start it up as a systemd service unit.
    
    Change-Id: I5ac9daed9ed4a3957b8761bd05b647fb0244dc84

---
+++ ci-scripts/dlrnapi_promoter/dlrn-promoter.sh
@@ -0,0 +1,32 @@
+#!/bin/bash -x
+
+#Activate virtualenv
+source ~/promoter_venv/bin/activate
+
+while true; do
+
+    #fetch the latest ci-config
+    cd ~/ci-config; git pull >/dev/null
+
+    #fetch the latest dlrnapi_client and dependencies
+    pip install -U -r ~/ci-config/ci-scripts/dlrnapi_promoter/requirements.txt
+    
+    # Source secrets
+    source ~/registry_secret 
+    source ~/dlrnapi_secret 
+
+    # promoter script for the master branch
+    /usr/bin/timeout --preserve-status -k 120m 115m python ~/ci-config/ci-scripts/dlrnapi_promoter/dlrnapi_promoter.py ~/ci-config/ci-scripts/dlrnapi_promoter/config/master.ini
+
+    # promoter script for the pike branch
+    /usr/bin/timeout --preserve-status -k 120m 115m python ~/ci-config/ci-scripts/dlrnapi_promoter/dlrnapi_promoter.py ~/ci-config/ci-scripts/dlrnapi_promoter/config/pike.ini
+
+    # promoter script for the ocata branch
+    /usr/bin/timeout --preserve-status -k 120m 115m python ~/ci-config/ci-scripts/dlrnapi_promoter/dlrnapi_promoter.py ~/ci-config/ci-scripts/dlrnapi_promoter/config/ocata.ini
+
+    # promoter script for the queens branch
+    /usr/bin/timeout --preserve-status -k 120m 115m python ~/ci-config/ci-scripts/dlrnapi_promoter/dlrnapi_promoter.py ~/ci-config/ci-scripts/dlrnapi_promoter/config/queens.ini
+
+    # Sleep 10 minutes
+    sleep 600
+done
