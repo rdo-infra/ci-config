83991d651c50816255ea92897120155bd4cdb70c
Quique Llorente <ellorent@redhat.com>
2018-05-16 08:28:33 +0200

    [WIP] Ruck rover monitoring tool
    Change-Id: Idd2e008e6c9b0ab865e3546c4fbc1fdad490fb12

---
+++ ci-scripts/ruck-rover-cockpit/grafana/grafana-alert-notification-server.py
@@ -0,0 +1,38 @@
+#!/bin/env python
+
+from flask import Flask, request
+import thread
+import json
+import irc.client
+import logging
+import sys
+
+
+app = Flask(__name__)
+
+class GrafanaIRCAlert(irc.client.SimpleIRCClient):
+    def __init__(self):
+        irc.client.SimpleIRCClient.__init__(self)
+        self.channel = '#tripleo-ci'
+        self.connect('irc.freenode.net', 6667, 'ruck-rover-alert')
+        self.connection.set_keepalive(10)
+
+    def on_welcome(self, connection, event):
+        connection.join(self.channel)
+
+    def send(self, alert):
+        print(alert)
+        if alert['state'] is not 'ok':
+            alert_message = "{title}: {message}"
+            self.connection.privmsg(self.channel, alert_message.format(**alert))
+
+@app.route('/',methods=['POST'])
+def alert():
+   irc_alert.send(json.loads(request.data))
+   return "OK"
+
+if __name__ == '__main__':
+
+    irc_alert = GrafanaIRCAlert()
+    thread.start_new_thread(app.run, ())
+    irc_alert.start()
