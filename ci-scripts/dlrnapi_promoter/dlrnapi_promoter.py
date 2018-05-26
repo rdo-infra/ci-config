66a9c171ebbee0ff57b83ab0c534049e62bd84d5
Matt Young <myoung@redhat.com>
2018-05-25 23:04:09 -0400

    promoter: Add global exception handler default
    WIP
    
    Currently the promoter has a few places where we have a try
    except without any detail logged, and things silently fail.
    
    For example, if ansible isn't installed the call to invoke ansible
    just blows up with no error detail.  This patch turns this:
    
    promoter FAILED promoting current-tripleo-rdo as current-tripleo-rdo-internal $hashDetails
    
    into this:
    
    2018-05-25 20:26:27,567 15135 ERROR    promoter [Errno 2] No such file or directory
    Traceback (most recent call last):
      File "/home/cipromo/ci-config/ci-scripts/dlrnapi_promoter/dlrnapi_promoter.py", line 358, in <module>
        promoter(config)
      File "/home/cipromo/ci-config/ci-scripts/dlrnapi_promoter/dlrnapi_promoter.py", line 341, in promoter
        promote_all_links(api_instance, promote_from, job_reqs, dry_run, release, latest_hashes_count)
      File "/home/cipromo/ci-config/ci-scripts/dlrnapi_promoter/dlrnapi_promoter.py", line 266, in promote_all_links
        tag_containers(new_hashes, release, promote_name)
      File "/home/cipromo/ci-config/ci-scripts/dlrnapi_promoter/dlrnapi_promoter.py", line 163, in tag_containers
        env=env, stderr=subprocess.STDOUT).split("\n")
      File "/usr/lib64/python2.7/subprocess.py", line 216, in check_output
        process = Popen(stdout=PIPE, *popenargs, **kwargs)
      File "/usr/lib64/python2.7/subprocess.py", line 394, in __init__
        errread, errwrite)
      File "/usr/lib64/python2.7/subprocess.py", line 1047, in _execute_child
        raise child_exception
    OSError: [Errno 2] No such file or directory
    
    Change-Id: Ib06984b26154f42cb529d959a8ecc8805ab0334a

--- ci-scripts/dlrnapi_promoter/dlrnapi_promoter.py
+++ ci-scripts/dlrnapi_promoter/dlrnapi_promoter.py
@@ -11,8 +11,30 @@ import socket
 import subprocess
 import sys
 
+import os.path
+import traceback
+
+
 from dlrnapi_client.rest import ApiException
 import dlrnapi_client
+
+
+def handle_exception(exc_type, exc_value, exc_traceback):
+    """ handle all exceptions """
+
+    # KeyboardInterrupt is a special case.
+    if issubclass(exc_type, KeyboardInterrupt):
+        return
+
+    filename, line, dummy, dummy = traceback.extract_tb( exc_traceback ).pop()
+    
+    # TODO: log these
+    filename = os.path.basename( filename )
+    error    = "%s: %s" % ( exc_type.__name__, exc_value )
+
+    logger.error('Closed due to an error. This is the full error report:')
+    logger.error( "".join(traceback.format_exception(exc_type, exc_value, exc_traceback)))
+    sys.exit(1)
 
 
 def check_promoted(dlrn, link, hashes):
@@ -252,6 +274,7 @@ def promote_all_links(api, promote_from, job_reqs, dry_run, release, latest_hash
                 except:
                     logger.info('FAILED promoting %s as %s (%s)',
                                 current_name, promote_name, new_hashes)
+                    raise
 
 
 # Use atomic abstract socket creation as process lock
@@ -324,6 +347,10 @@ if __name__ == '__main__':
     if len(sys.argv) < 2:
         print("Usage: %s <config-file>" % sys.argv[0])
     else:
+
+        # install handler for exceptions
+        sys.excepthook = handle_exception
+
         config = ConfigParser.SafeConfigParser(allow_no_value=True)
         config.read(sys.argv[1])
         setup_logging(config.get('main', 'log_file'))
