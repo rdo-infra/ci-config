dd6aabc03b3c30260646e607a4744e2d7777a40a
Alfredo Moralejo <amoralej@redhat.com>
2018-05-30 12:50:47 +0200

    Pin the version of cmd2 when installing ansible
    md2>=0.9.0 now require python3.4 or greater[1]. We install ansible under
    python2 (for now) so we need to cap the cmd2 install to make the ansible
    installation function.
    
    [1] https://ci.centos.org/job/weirdo-generic-puppet-openstack-scenario002/5288/console
    
    Change-Id: Iad68b0340dbc27cbc494bd910b87c1ffca30f4de

--- jenkins/jobs/scripts/create-vm.sh
+++ jenkins/jobs/scripts/create-vm.sh
@@ -27,7 +27,7 @@ pushd $WORKSPACE
 # Install dependencies
 [[ ! -d provision_venv ]] && virtualenv provision_venv
 source provision_venv/bin/activate
-pip install ansible==2.5.2 ara shade
+pip install ansible==2.5.2 ara shade 'cmd2<0.9.0'
 
 # Is there a better way ?
 git clone https://github.com/rdo-infra/ci-config
