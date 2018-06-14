77033fedf8a1959b3ce276fd9920e7b017b68c7c
Quique Llorente <ellorent@redhat.com>
2018-06-14 12:46:52 +0200

    Change centos jenkins artifacts URL
    CentoOS CI jenkins URL are old, the patter is now job_name/build_id instead
    of job_name-build_id
    
    Change-Id: I02a81e7d4abc6b3b5bb782129720d91d23be416a

--- jenkins/jobs/scripts/dlrn-api-report.sh
+++ jenkins/jobs/scripts/dlrn-api-report.sh
@@ -15,5 +15,5 @@ dlrnapi --url https://$DELOREAN_HOST/api-$RDO_VERSION \
     --commit-hash $commit_hash \
     --distro-hash $distro_hash \
     --timestamp $(date +%s) \
-    --info-url https://ci.centos.org/artifacts/rdo/$BUILD_TAG/console.txt.gz \
+    --info-url https://ci.centos.org/artifacts/rdo/$JOB_NAME/$BUILD_ID/console.txt.gz \
     --success $JOB_SUCCESS
