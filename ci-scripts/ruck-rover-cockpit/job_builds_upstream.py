a626be087c5bfc923661d32a3b84b04437a4bcd8
Quique Llorente <ellorent@redhat.com>
2018-06-25 15:54:42 +0200

    [RR Rockpit] Reduce to 8h noop change gauge
    Also add cloud provider and region to upstream jobs
    
    Change-Id: I10005abd2d673e0d6367023785697327e5908372

--- ci-scripts/ruck-rover-cockpit/job_builds_upstream.py
+++ ci-scripts/ruck-rover-cockpit/job_builds_upstream.py
@@ -2,14 +2,14 @@
 import datetime
 import time
 import requests
+import yaml
 
-
+NOOP_CHANGES = ['560445', '567224', '564285', '564291']
 ADDITIONAL_JOBS = []
 ZUUL_URL = 'http://zuul.openstack.org/api/'
 JOBS_API = ZUUL_URL + 'jobs'
 BUILDS_API = ZUUL_URL + 'builds'
 PAGES = 1
-
 
 # Convert datetime to timestamp
 def to_ts(d, seconds=False):
@@ -19,6 +19,7 @@ def to_ts(d, seconds=False):
 
 
 def get(url, query={}, timeout=20, json_view=True):
+
     try:
         response = requests.get(url, params=query, timeout=timeout)
     except Exception as e:
@@ -37,8 +38,7 @@ def get_jobs_list():
     return ooo_jobs + ADDITIONAL_JOBS
 
 
-def get_builds_info(job_name, pages):
-    query = {'job_name': job_name}
+def get_builds_info(query, pages=PAGES):
     result = []
     for p in range(pages):
         if p > 0:
@@ -50,10 +50,23 @@ def get_builds_info(job_name, pages):
             result += response
     return result
 
+def add_inventory_info(build, hosts):
+    if 'log_url' in build:
+        try:
+            r = requests.get(build['log_url'] + "/zuul-info/inventory.yaml")
+            if r.ok:
+                inventory = yaml.load(r.content)
+                # FIXME: Primary is enough ?
+                if all(host in inventory['all']['hosts'] for host in hosts):
+                    build['inventory'] = inventory
+        except Exception:
+            pass
 
 def influx(build):
     if build['start_time'] == None:
         build['start_time'] = build['end_time']
+
+    # Get the nodename
     return (
         'build,'
         'type=upstream,'
@@ -72,7 +85,10 @@ def influx(build):
         'log_link="%s",'
         'duration=%s,'
         'start=%s,'
-        'end=%s'
+        'end=%s,'
+        'primary_node_cloud="%s",'
+        'primary_node_region="%s"'
+        ''
         ' '
         '%s' % (
             build['pipeline'],
@@ -92,21 +108,37 @@ def influx(build):
             build.get('duration', 0),
             to_ts(build['start_time'], seconds=True),
             to_ts(build['end_time'], seconds=True),
-
+            'null' if 'inventory' not in build else build['inventory']
+                ['all']['hosts']['primary']['nodepool']['cloud'],
+            'null' if 'inventory' not in build else build['inventory']
+                ['all']['hosts']['primary']['nodepool']['region'],
             to_ts(build['end_time'])
                 )
     )
 
 
+def print_influx(builds):
+    if builds:
+        for build in builds:
+            if build['result'] != 'SUCCESS':
+                add_inventory_info(build, hosts=['primary'])
+            print(influx(build))
+
 def main():
     jobs = get_jobs_list()
+    builds = []
     if jobs:
         for job in jobs:
-            builds = get_builds_info(job, pages=PAGES)
-            if builds:
-                for build in builds:
-                    print(influx(build))
+            for build in get_builds_info({'job_name': job}):
+                # Filter by noop, we are going to print them later
+                # so we don't exclude any job for them
+                if build['change'] not in NOOP_CHANGES:
+                    builds.append(build)
 
+    for noop_change in NOOP_CHANGES:
+        builds += get_builds_info({'change': noop_change})
+
+    print_influx(builds)
 
 if __name__ == '__main__':
     main()
