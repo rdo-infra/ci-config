9bc4ade0582fb8294ca5bf5a51720d51e12548dc
Quique Llorente <ellorent@redhat.com>
2018-06-28 09:14:08 +0200

    [RR Cockpit] Add cloud info to jobs
    It uses a cache so we don't git log server too much
    
    Change-Id: Ifec47aacadcf72b9cc1763e6dd440d3a195f59a2

--- ci-scripts/ruck-rover-cockpit/job_builds_upstream.py
+++ ci-scripts/ruck-rover-cockpit/job_builds_upstream.py
@@ -5,12 +5,24 @@ import requests
 import yaml
 import json
 
-ADDITIONAL_PROJECTS = []
+from diskcache import Cache
+
+OOO_PROJECTS = [
+        'openstack/puppet-triple',
+        'openstack/python-tripleoclient',
+        'openstack/tripleo-upgrade',
+        'openstack/tripleo-quickstart-extras',
+        'openstack/tripleo-common',
+        'openstack-infra/tripleo-ci',
+        'openstack/tripleo-quickstart',
+        'openstack/tripleo-heat-templates']
+
 ZUUL_URL = 'http://zuul.openstack.org/api/'
-GERRIT_URL = 'https://review.openstack.org/'
-PROJECTS_API = GERRIT_URL + 'projects/'
 BUILDS_API = ZUUL_URL + 'builds'
 PAGES = 1
+
+cache = Cache('/tmp/ruck_rover_cache')
+cache.expire()
 
 # Convert datetime to timestamp
 def to_ts(d, seconds=False):
@@ -34,17 +46,8 @@ def get(url, query={}, timeout=20, json_view=True):
     return None
 
 
-def get_projects_list():
-    project_list_text = get(PROJECTS_API, json_view=False)
-    # Gerrit is returning garbage in the first line
-    project_list_text = '\n'.join(project_list_text.split('\n')[1:])
-    project_list = json.loads(project_list_text)
-    ooo_projects = [p for p in project_list if 'tripleo' in p]
-    return ooo_projects + ADDITIONAL_PROJECTS
-
-
 def get_builds_info(query, pages=PAGES):
-    result = []
+    builds = []
     for p in range(pages):
         if p > 0:
             query['skip'] = ((pages - 1) * 50)
@@ -52,23 +55,36 @@ def get_builds_info(query, pages=PAGES):
             time.sleep(2)
         response = get(BUILDS_API, query)
         if response is not None:
-            result += response
-    return result
+            builds += response
+    return builds
 
-def add_inventory_info(build, hosts):
+def add_inventory_info(build):
 
     if 'log_url' in build:
+        inventory_path = build['log_url'] + "/zuul-info/inventory.yaml"
         try:
-            r = requests.get(build['log_url'] + "/zuul-info/inventory.yaml")
-            if r.ok:
-                inventory = yaml.load(r.content)
-                # FIXME: Primary is enough ?
-                if all(host in inventory['all']['hosts'] for host in hosts):
-                    build['inventory'] = inventory
+            if inventory_path not in cache:
+                r = requests.get(inventory_path)
+                if r.ok:
+                    cache.add(inventory_path, yaml.load(r.content),
+                            expire=259200) # expire is 3 days
+
+            inventory = cache[inventory_path]
+            hosts = inventory['all']['hosts']
+            host = hosts[hosts.keys()[0]]
+            if 'nodepool' in host:
+                nodepool = host['nodepool']
+                build['cloud'] = nodepool['cloud']
+                build['region'] = nodepool['region']
+                build['provider'] = nodepool['provider']
+
         except Exception:
             pass
 
 def influx(build):
+
+    add_inventory_info(build)
+
     if build['start_time'] == None:
         build['start_time'] = build['end_time']
 
@@ -83,7 +99,10 @@ def influx(build):
         'voting=%s,'
         'change=%s,'
         'patchset=%s,'
-        'passed=%s'
+        'passed=%s,'
+        'cloud=%s,'
+        'region=%s'
+        'provider=%s'
         ' '
         'result="%s",'
         'result_num=%s,'
@@ -91,7 +110,10 @@ def influx(build):
         'log_link="%s",'
         'duration=%s,'
         'start=%s,'
-        'end=%s'
+        'end=%s,'
+        'cloud="%s",'
+        'region="%s"'
+        'provider="%s"'
         ' '
         '%s' % (
             build['pipeline'],
@@ -101,8 +123,10 @@ def influx(build):
             build['voting'],
             build['change'],
             build['patchset'],
-
             'True' if build['result'] == 'SUCCESS' else 'False',
+            build.get('cloud', 'null'),
+            build.get('region', 'null'),
+            build.get('provider', 'null'),
 
             'SUCCESS' if build['result'] == 'SUCCESS' else 'FAILURE',
             1 if build['result'] == 'SUCCESS' else 0,
@@ -111,6 +135,9 @@ def influx(build):
             build.get('duration', 0),
             to_ts(build['start_time'], seconds=True),
             to_ts(build['end_time'], seconds=True),
+            build.get('cloud', 'null'),
+            build.get('region', 'null'),
+            build.get('provider', 'null'),
             to_ts(build['end_time'])
                 )
     )
@@ -119,20 +146,11 @@ def influx(build):
 def print_influx(builds):
     if builds:
         for build in builds:
-
-            # FIXME: We don't want to hit log.ooo server too much
-            #        let's find another way to access inventory
-            #if build['result'] != 'SUCCESS':
-            #    add_inventory_info(build, hosts=['primary'])
-
             print(influx(build))
 
 def main():
-    projects = get_projects_list()
-
-    if projects:
-        for project in projects:
-            print_influx(get_builds_info({'project': project}))
+    for project in OOO_PROJECTS:
+        print_influx(get_builds_info({'project': project}))
 
 if __name__ == '__main__':
     main()
