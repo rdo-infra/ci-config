27860fd1765628a9947026296cbc133b80daaf8f
Quique Llorente <ellorent@redhat.com>
2018-06-29 13:04:26 +0200

    [RR Cockpit] Add logs to skipped promotions
    Change-Id: I7fb82d1354ed00eba49cd4ce4b9ec7805641ac21

--- ci-scripts/ruck-rover-cockpit/job_builds_upstream.py
+++ ci-scripts/ruck-rover-cockpit/job_builds_upstream.py
@@ -19,7 +19,7 @@ OOO_PROJECTS = [
 
 ZUUL_URL = 'http://zuul.openstack.org/api/'
 BUILDS_API = ZUUL_URL + 'builds'
-PAGES = 1
+PAGES = 10
 
 cache = Cache('/tmp/ruck_rover_cache')
 cache.expire()
@@ -101,7 +101,7 @@ def influx(build):
         'patchset=%s,'
         'passed=%s,'
         'cloud=%s,'
-        'region=%s'
+        'region=%s,'
         'provider=%s'
         ' '
         'result="%s",'
@@ -112,7 +112,7 @@ def influx(build):
         'start=%s,'
         'end=%s,'
         'cloud="%s",'
-        'region="%s"'
+        'region="%s",'
         'provider="%s"'
         ' '
         '%s' % (
