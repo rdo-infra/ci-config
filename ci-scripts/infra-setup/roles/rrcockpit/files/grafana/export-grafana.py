b785d2c76456e5da9bb49583f3b973754fdca435
Quique Llorente <ellorent@redhat.com>
2018-07-05 15:30:26 +0200

    [WIP] Add role to deploy ruck-rover-cockpit
    Change-Id: Ie621e734324b4b8482105e0633457d5ced8ac6f4

---
+++ ci-scripts/infra-setup/roles/rrcockpit/files/grafana/export-grafana.py
@@ -0,0 +1,83 @@
+#!/usr/bin/env python
+
+import argparse
+import requests
+import json
+
+def main():
+
+    json_options = {
+        "sort_keys": True,
+        "indent": 4,
+        "separators": (',', ':')
+    }
+
+    parser = argparse.ArgumentParser(
+        description="Export grafana as json files")
+
+    parser.add_argument('--host', default='http://localhost')
+    parser.add_argument('--key', default='grafana.key')
+
+    args = parser.parse_args()
+
+    with open(args.key) as key_file:
+        key = key_file.read().splitlines()[0]
+        authorization_header = "Bearer {}".format(key)
+        headers = {'Authorization': authorization_header}
+
+        dashboards_list = json.loads(
+            requests.get(
+                "{}/api/search?query=&".format(args.host),
+                headers=headers).content)
+        for dashboard_id in dashboards_list:
+            print("Exporting dashboard {title}".format(**dashboard_id))
+            url = "{host}/api/dashboards/uid/{uid}".format(
+                host=args.host, **dashboard_id)
+            dashboard = json.loads(requests.get(url, headers=headers).content)
+            dashboard_name = dashboard_id['uri'].split('/')[1]
+            with file("{}.dashboard.json".format(dashboard_name),
+                      'w') as json_file:
+                dashboard.pop('meta', None)
+                dashboard['dashboard'].pop('version', None)
+                dashboard['dashboard'].pop('id', None)
+                dashboard['dashboard'].pop('uid', None)
+                json.dump(dashboard, json_file, **json_options)
+
+        datasources_list = json.loads(
+            requests.get("{}/api/datasources".format(args.host),
+                         headers=headers).content)
+        for datasource_id in datasources_list:
+            print("Exporting datasource {name}".format(**datasource_id))
+            url = "{host}/api/datasources/{id}".format(
+                host=args.host, **datasource_id)
+            datasource = json.loads(requests.get(url, headers=headers).content)
+            with file("{name}.datasource.json".format(**datasource_id),
+                      'w') as json_file:
+                datasource.pop('id', None)
+                datasource.pop('version', None)
+                json.dump(datasource, json_file, **json_options)
+
+        alert_notifications_list = json.loads(
+            requests.get(
+                "{}/api/alert-notifications".format(args.host),
+                headers=headers).content)
+        for alert_notification_id in alert_notifications_list:
+            print("Exporting alert-notification {name}".format(
+                **alert_notification_id))
+            url = "{host}/api/alert-notifications/{id}".format(
+                host=args.host, **alert_notification_id)
+            alert_notification = json.loads(
+                requests.get(url, headers=headers).content)
+            with file(
+                    "{name}.alert-notification.json".format(
+                        **alert_notification_id), 'w') as json_file:
+                alert_notification.pop('id', None)
+                alert_notification.pop('version', None)
+                alert_notification.pop('created', None)
+                alert_notification.pop('updates', None)
+                alert_notification.pop('updated', None)
+                json.dump(alert_notification, json_file, **json_options)
+
+
+if __name__ == '__main__':
+    main()
---
+++ ci-scripts/infra-setup/roles/rrcockpit/files/grafana/grafana-alert-notification-server.py
@@ -0,0 +1,97 @@
+#!/usr/bin/env python
+
+import argparse
+import thread
+import json
+import irc.bot
+
+from flask import Flask, request
+from get_alerts import get_alerts
+
+app = Flask(__name__)
+
+
+class GrafanaIRCAlertBot(irc.bot.SingleServerIRCBot):
+    def __init__(self, grafana_host, grafana_key):
+        irc.bot.SingleServerIRCBot.__init__(
+            self, [('chat.freenode.net', 6667)], 'ruck-rover-alert',
+            'Openstack triplo ci alert bot')
+        self.channel = '#tripleo-ci'
+        self.grafana_host = grafana_host
+        self.grafana_key = grafana_key
+
+    def on_welcome(self, connection, event):
+        connection.join(self.channel)
+
+    def on_privmsg(self, c, e):
+        self.do_command(e, e.arguments[0])
+
+    def on_pubmsg(self, c, e):
+        line = e.arguments[0]
+        a = line.split(":", 1)
+        if len(a) <= 1:
+            a = line.split(",", 1)
+        if len(a) > 1 and irc.strings.lower(a[0]) == irc.strings.lower(
+                self.connection.get_nickname()):
+            self.do_command(e, a[1].strip())
+        return
+
+    def send_message(self, message):
+        self.connection.privmsg(self.channel, message)
+
+    def send_alert(self, alert):
+        if alert['state'] != 'ok':
+            self.send_message("[{title}] {message}".format(**alert))
+
+    def filter_alerts(self, alerts, command):
+        filtered_alerts = []
+
+        if len(command) <= 1:
+            filtered_alerts = alerts
+        else:
+            for alert in alerts:
+                filters = command[1:]
+                name = alert['name'].lower()
+                message = alert['Message'].lower()
+                if any(filter.lower() in name + message for filter in filters):
+                    filtered_alerts.append(alert)
+        return filtered_alerts
+
+    def do_command(self, e, cmd):
+        splitted_cmd = cmd.split()
+        action = splitted_cmd[0]
+        if action == "alerts":
+            alerts = self.filter_alerts(
+                get_alerts(self.grafana_host, self.grafana_key), splitted_cmd)
+            if alerts:
+                for alert in alerts:
+                    self.send_alert({
+                        'title': alert['name'],
+                        'message': alert['Message'],
+                        'state': 'alerting'
+                    })
+            else:
+                self.send_message("No alerts")
+        else:
+            self.send_message("Not understood: " + cmd)
+
+
+@app.route('/', methods=['POST'])
+def alert():
+    irc_alert.send_alert(json.loads(request.data))
+    return "OK"
+
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser(
+        description="Export grafana as json files")
+
+    parser.add_argument('--grafana-host', required=True)
+    parser.add_argument('--grafana-key', required=True)
+
+    args = parser.parse_args()
+
+    irc_alert = GrafanaIRCAlertBot(args.grafana_host, args.grafana_key)
+    thread.start_new_thread(app.run, ())
+    irc_alert.start()
