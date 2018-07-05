9710febb05144f221ac52c0deab47c7c77cace9d
Quique Llorente <ellorent@redhat.com>
2018-07-05 15:30:26 +0200

    [WIP] Add role to deploy ruck-rover-cockpit
    Change-Id: Ie621e734324b4b8482105e0633457d5ced8ac6f4

---
+++ ci-scripts/infra-setup/roles/rrcockpit/files/grafana/get_alerts.py
@@ -0,0 +1,40 @@
+#!/bin/env python
+
+import argparse
+import requests
+import json
+
+
+def call_alerts_api(host, key, suffix):
+    authorization_header = "Bearer {}".format(key)
+    headers = {'Authorization': authorization_header}
+    return json.loads(
+        requests.get("{}/api/alerts{}".format(host, suffix),
+                     headers=headers).content)
+
+
+def get_alerts(host, key):
+    alerts = []
+    for alert_meta in call_alerts_api(host, key, "?state=alerting"):
+        alert = call_alerts_api(host, key, "/{}".format(alert_meta['id']))
+        alert.update(alert_meta)
+        alerts.append(alert)
+    return alerts
+
+
+def main():
+
+    parser = argparse.ArgumentParser(
+        description="Export grafana as json files")
+
+    parser.add_argument('--host', required=True)
+    parser.add_argument('--key', required=True)
+
+    args = parser.parse_args()
+
+    for alert in get_alerts(args.host, args.key):
+        print(alert)
+
+
+if __name__ == '__main__':
+    main()
