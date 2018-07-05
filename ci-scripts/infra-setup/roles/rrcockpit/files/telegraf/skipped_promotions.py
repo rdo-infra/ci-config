8f0c4d70e6a7acdd7a5599518327a07fe16b9cc3
Quique Llorente <ellorent@redhat.com>
2018-07-05 15:30:26 +0200

    [WIP] Add role to deploy ruck-rover-cockpit
    Change-Id: Ie621e734324b4b8482105e0633457d5ced8ac6f4

---
+++ ci-scripts/infra-setup/roles/rrcockpit/files/telegraf/skipped_promotions.py
@@ -0,0 +1,127 @@
+#!/usr/bin/env python
+import requests
+import re
+import sys
+import dlrnapi_client
+import influxdb_utils
+
+from promoter_utils import get_dlrn_instance_for_release
+from diskcache import Cache
+
+cache = Cache('/tmp/skipped_promotions_cache')
+cache.expire()
+
+promoter_skipping_regex = re.compile(
+    ('.*promoter Skipping promotion of (.*) from (.*) to (.*), '
+     'missing successful jobs: (.*)')
+)
+
+
+def get_failing_jobs_html(dlrn_hashes, release_name):
+    failing_jobs_html = ""
+
+    # If any of the jobs is still in progress
+    in_progress = False
+
+    try:
+        dlrn = get_dlrn_instance_for_release(release_name)
+        if dlrn:
+            params = dlrnapi_client.Params2()
+            params.commit_hash = dlrn_hashes['commit_hash']
+            params.distro_hash = dlrn_hashes['distro_hash']
+            params.success = str(False)
+
+            failing_jobs = dlrn.api_repo_status_get(params)
+            for i, failing_job in enumerate(failing_jobs):
+                if failing_job.in_progress:
+                    in_progress = True
+                failing_job_ln = "<a href='{}' target='_blank' >{}</a>".format(
+                        failing_job.url, failing_job.job_id)
+
+                if i > 0:
+                    failing_job_ln += "<br>"
+                failing_jobs_html += failing_job_ln
+
+    except Exception as e:
+        print(e)
+        pass
+    return (in_progress, failing_jobs_html)
+
+
+# FIXME: Use a decorator ?
+def get_cached_failing_jobs_html(dlrn_hashes, release_name):
+    cache_key = "failing_jobs_html_{timestamp}_{repo_hash}".format(
+        **dlrn_hashes)
+    if cache_key not in cache:
+        in_progress, failing_jobs_html = get_failing_jobs_html(
+            dlrn_hashes, release_name)
+
+        # Only chache if jobs have finished
+        if not in_progress:
+            cache.add(cache_key, failing_jobs_html, expire=259200)
+
+    return cache[cache_key]
+
+
+def parse_skipped_promotions(release_name):
+    skipped_promotions = []
+    promoter_logs = requests.get(
+        "http://38.145.34.55/{}.log".format(release_name))
+
+    def get_log_time(log_line):
+        log_line_splitted = log_line.split()
+        log_time = "{} {}".format(log_line_splitted[0], log_line_splitted[1])
+        log_time = log_time.split(',')[0]
+        return log_time
+
+    for log_line in promoter_logs.iter_lines():
+        matched_regex = promoter_skipping_regex.match(log_line)
+        if matched_regex:
+
+            promotion = matched_regex.group(1)
+            try:
+                promotion = eval(matched_regex.group(1))
+                repo_hash = promotion['repo_hash']
+                failing_jobs = get_cached_failing_jobs_html(
+                    promotion, release_name)
+            except Exception:
+                repo_hash = promotion
+                failing_jobs = matched_regex.group(3)
+
+            skipped_promotion = {
+                'repo_hash': repo_hash,
+                'from_name': matched_regex.group(2),
+                'to_name': matched_regex.group(3),
+                'failing_jobs': failing_jobs,
+                'timestamp': get_log_time(log_line),
+                'release': release_name
+            }
+            skipped_promotions.append(skipped_promotion)
+
+    return skipped_promotions
+
+
+def to_influxdb(skipped_promotions):
+    influxdb_lines = []
+    influxdb_format = (
+        "skipped-promotions,repo_hash={repo_hash}"
+        ",release={release},from_name={from_name},"
+        "to_name={to_name} failing_jobs=\"{failing_jobs}\" "
+        "{timestamp}")
+
+    for skipped_promotion in skipped_promotions:
+        skipped_promotion['timestamp'] = influxdb_utils.format_ts_from_str(
+            skipped_promotion['timestamp'])
+        influxdb_lines.append(influxdb_format.format(**skipped_promotion))
+
+    return influxdb_lines
+
+
+def main():
+    release = sys.argv[1]
+    influxdb_lines = to_influxdb(parse_skipped_promotions(release))
+    print('\n'.join(influxdb_lines))
+
+
+if __name__ == '__main__':
+    main()
---
+++ ci-scripts/infra-setup/roles/rrcockpit/files/telegraf/zuulv3_queues_status.py
@@ -0,0 +1,109 @@
+#!/usr/bin/env python
+
+import argparse
+import requests
+import json
+from time import time
+
+
+def find_zuul_queues(zuul_status_url, pipeline_name, queue_name):
+    queues = []
+    zuul_status = json.loads(requests.get(zuul_status_url).content)
+
+    found_pipeline = next(pipeline['change_queues']
+                          for pipeline in zuul_status['pipelines']
+                          if pipeline['name'] == pipeline_name)
+
+    found_queues = (queue for queue in found_pipeline
+                    if queue_name is None or queue['name'] == queue_name)
+
+    for queue in found_queues:
+        refspecs = []
+        queue_heads = queue['heads']
+        if queue_heads:
+            refspecs = queue_heads[0]
+            queues.append({
+                'url': zuul_status_url,
+                'pipeline': pipeline_name,
+                'queue': queue['name'],
+                'refspecs': refspecs
+            })
+
+    return queues
+
+
+def calculate_minutes_enqueued(enqueue_time):
+    # TODO: Do we have to use start time instead ?
+    current_time = int(time() * 1000)
+    minutes_enqueued = ((current_time - enqueue_time) / 60000)
+    return minutes_enqueued
+
+
+def convert_builds_as_influxdb(queues):
+    # Le's express failures as negative numbers, is easier for alarms
+
+    result_mapping = {
+        'NODE_FAILURE': -1,
+        'FAILURE': -1,
+        'POST_FAILURE': -1,
+        'MERGER_FAILURE': -1,
+        'ONGOING': 0,
+        'SUCCESS': 1,
+        'SKIPPED': 1,
+    }
+    influxdb_lines = []
+    influxdb_line = ("zuul-queue-status,url={url},pipeline={pipeline}"
+                     ",queue={queue},job={name},review={review}"
+                     ",patch_set={patch_set} result=\"{result}\""
+                     ",enqueue_time={enqueue_time},"
+                     "enqueued_time={enqueued_time},result_code={result_code}")
+    for queue in queues:
+        for refspec in queue['refspecs']:
+            for job in refspec['jobs']:
+                # FIXME: Not very performant
+                values = {}
+                values.update(job)
+                values.update(refspec)
+                values.update(queue)
+
+                result = values['result']
+                if result is not None:
+                    result_code = result_mapping[result]
+                else:
+                    values['result'] = 'ONGOING'
+                    result_code = result_mapping[values['result']]
+
+                values['result_code'] = result_code
+                values['enqueued_time'] = calculate_minutes_enqueued(
+                    values['enqueue_time'])
+
+                # influxdb line protocol uses commas to split stuf,
+                # let's escape it
+                id = values['id'].split(',')
+                values['review'] = id[0]
+                values['patch_set'] = id[1]
+
+                influxdb_lines.append(influxdb_line.format(**values))
+    return influxdb_lines
+
+
+def main():
+
+    parser = argparse.ArgumentParser(
+        description="Print zuul status as influxdb lines")
+
+    parser.add_argument('--url', required=True)
+    parser.add_argument('--pipeline', required=True)
+    parser.add_argument('--queue')
+
+    args = parser.parse_args()
+
+    queues = find_zuul_queues(args.url, args.pipeline, args.queue)
+
+    influxdb_lines = convert_builds_as_influxdb(queues)
+
+    print('\n'.join(influxdb_lines))
+
+
+if __name__ == '__main__':
+    main()
