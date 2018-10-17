28c90fd0df67d16183195bf21750740f19f3b16d
Adam Kimball <baha@astyr.net>
2018-10-17 14:17:00 +0000

    Add cico flavor specification to cico-node-get-to-ansible.sh
    Add a CICO_FLAVOR variable used to query specific node flavors
    from cicoclient. This will allow multiarch jobs to request larger
    cico nodes to prevent errors. Currently defaults to small, as per
    https://wiki.centos.org/QaWiki/CI/Multiarch
    
    Change-Id: I6fead5507bf93850c6d08a46e60d6f2ac0aee9c2

--- jenkins/jobs/scripts/cico-node-get-to-ansible.sh
+++ jenkins/jobs/scripts/cico-node-get-to-ansible.sh
@@ -5,6 +5,7 @@ ANSIBLE_HOSTS=${ANSIBLE_HOSTS:-$WORKSPACE/hosts}
 SSID_FILE=${SSID_FILE:-$WORKSPACE/cico-ssid}
 
 CPU_ARCH=${CPU_ARCH:-x86_64}
+CICO_FLAVOR=${CICO_FLAVOR:-small}
 
 # Write the header of the hosts file
 cat << EOF > ${ANSIBLE_HOSTS}
@@ -14,7 +15,7 @@ localhost ansible_connection=local
 EOF
 
 # Get nodes
-nodes=$(cico -q node get --arch $CPU_ARCH --retry-count 6 --retry-interval 60  --count ${NODE_COUNT} --column hostname --column ip_address --column comment -f value)
+nodes=$(cico -q node get --arch $CPU_ARCH --flavor $CICO_FLAVOR --retry-count 6 --retry-interval 60  --count ${NODE_COUNT} --column hostname --column ip_address --column comment -f value)
 
 # Write nodes to inventory file and persist the SSID separately for simplicity
 touch ${SSID_FILE}
