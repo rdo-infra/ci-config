a5ab0f3f313e94ece37c3f8789c4f72b6a3abb34
Javier Pena <jpena@redhat.com>
2018-10-23 13:05:01 +0200

    Remove Newton jobs
    Newton is being phased out in RDO, since it is already EOL upstream.
    Removing all Newton promotion jobs, and other jobs related to the
    Newton release.
    
    Change-Id: I98cc8765f7fb6f485bf18e18a66a715d213bbcc7

--- jenkins/jobs/scripts/centos-cloudsig-stable-newton.sh
+++
@@ -1,9 +0,0 @@
-set -e
-export RDO_VERSION_DIR='newton'
-# The LOCATION var stores what repo symlink was used. It becomes part of the
-# path where images are stored.
-export LOCATION='cloudsig-stable'
-# The BUILD_SYS var stores what build system was used. It becomes part of the
-# path where images are stored.
-export BUILD_SYS='cbs'
-export HASH_FILE='/tmp/cloudsig-stable_newton_hash'
