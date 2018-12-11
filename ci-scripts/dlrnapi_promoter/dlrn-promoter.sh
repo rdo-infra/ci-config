d1a9c24d79dd70f58fa50cd87fe5d0da2d5fdfcb
Ronelle Landy <rlandy@redhat.com>
2018-12-11 13:28:51 -0500

    Separate the Centos-7 and Fedora-28 promotion ini files
    This review adds more descriptive job names to the promotion
    criteria in place of the job_id - which would have resulted in
    duplicate jobs being considered in the case of a mixed-distro
    pipeline.
    The old ini files are not deleted.
    
    Change-Id: I59d1deaa4e8d8b12cd1f561da2bbfa1280e89c00

--- ci-scripts/dlrnapi_promoter/dlrn-promoter.sh
+++ ci-scripts/dlrnapi_promoter/dlrn-promoter.sh
@@ -15,6 +15,8 @@ while true; do
     source ~/registry_secret
     source ~/dlrnapi_secret
 
+    # Temp retain old promoter config reporting
+
     # promoter script for the master branch
     /usr/bin/timeout --preserve-status -k 120m 115m python ~/ci-config/ci-scripts/dlrnapi_promoter/dlrnapi_promoter.py ~/ci-config/ci-scripts/dlrnapi_promoter/config/master.ini
 
@@ -29,6 +31,26 @@ while true; do
 
     # promoter script for the rocky branch
     /usr/bin/timeout --preserve-status -k 120m 115m python ~/ci-config/ci-scripts/dlrnapi_promoter/dlrnapi_promoter.py ~/ci-config/ci-scripts/dlrnapi_promoter/config/rocky.ini
+
+    # Start using the new promoter configs
+
+    # promoter script for the CentOS-7 master branch
+    /usr/bin/timeout --preserve-status -k 120m 115m python ~/ci-config/ci-scripts/dlrnapi_promoter/dlrnapi_promoter.py ~/ci-config/ci-scripts/dlrnapi_promoter/config/CentOS-7/master.ini
+
+    # promoter script for the CentOS-7 pike branch
+    /usr/bin/timeout --preserve-status -k 120m 115m python ~/ci-config/ci-scripts/dlrnapi_promoter/dlrnapi_promoter.py ~/ci-config/ci-scripts/dlrnapi_promoter/config/CentOS-7/pike.ini
+
+    # promoter script for the CentOS-7 ocata branch
+    /usr/bin/timeout --preserve-status -k 120m 115m python ~/ci-config/ci-scripts/dlrnapi_promoter/dlrnapi_promoter.py ~/ci-config/ci-scripts/dlrnapi_promoter/config/CentOS-7/ocata.ini
+
+    # promoter script for the CentOS-7 queens branch
+    /usr/bin/timeout --preserve-status -k 120m 115m python ~/ci-config/ci-scripts/dlrnapi_promoter/dlrnapi_promoter.py ~/ci-config/ci-scripts/dlrnapi_promoter/config/CentOS-7/queens.ini
+
+    # promoter script for the CentOS-7 rocky branch
+    /usr/bin/timeout --preserve-status -k 120m 115m python ~/ci-config/ci-scripts/dlrnapi_promoter/dlrnapi_promoter.py ~/ci-config/ci-scripts/dlrnapi_promoter/config/CentOS-7/rocky.ini
+
+    # promoter script for the Fedora-28 master branch
+    #/usr/bin/timeout --preserve-status -k 120m 115m python ~/ci-config/ci-scripts/dlrnapi_promoter/dlrnapi_promoter.py ~/ci-config/ci-scripts/dlrnapi_promoter/config/Fedora-28/master.ini
 
     # Sleep 10 minutes
     sleep 600
