36a2f104a584c8b5ec78724ec5bcd1802035d6cd
Wes Hayutin <weshayutin@gmail.com>
2019-01-21 12:06:22 -0700

    WIP, load lp data into mariadb
    Create a new script to export lp data in csv
    format.
    Create the script to import the data into the
    mariadb container
    
    Question: how does one make grafana and telegraph
    continue to load data into the database?
    
    Change-Id: Iaebcd75f5db45651b0fe3ea187b93ece8ebf9a9b

---
+++ ci-scripts/infra-setup/roles/rrcockpit/files/telegraf/load_lp_mysql.sql
@@ -0,0 +1,14 @@
+USE lpbug;
+CREATE TABLE IF NOT EXISTS rr_bugs (
+    id INT,
+    status VARCHAR(255) NOT NULL,
+    tag VARCHAR(255) NOT NULL,
+    link VARCHAR(255) NOT NULL,
+    description TEXT NOT NULL,
+    PRIMARY KEY (id)
+);
+
+LOAD DATA LOCAL INFILE '/tmp/test.csv' \n
+INTO TABLE rr_bugs \n
+FIELDS TERMINATED BY ',' \n
+LINES TERMINATED BY '\n';
