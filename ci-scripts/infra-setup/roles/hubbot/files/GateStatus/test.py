234e2497801714c44112c5be9d74dacb485bd71a
Sorin Sbarnea <ssbarnea@redhat.com>
2019-01-30 16:18:52 +0000

    Remove flake8 excludes and ignores
    Adds tox-pep8 job that runs only flake8 for developer convenience,
    even if this is part of tox-linters which is run on CI.
    
    Change-Id: I004fca8344ee9b91b146a2441eded814d9819a10

--- ci-scripts/infra-setup/roles/hubbot/files/GateStatus/test.py
+++ ci-scripts/infra-setup/roles/hubbot/files/GateStatus/test.py
@@ -1,11 +1,8 @@
-from supybot.test import *
+from supybot.test import PluginTestCase
 
 import json
 import subprocess
-import sys
 import time
-
-import supybot.world as world
 
 from . import plugin as GateStatus
 
@@ -16,11 +13,11 @@ mocked_data = [
         'url': 'http://review.example.com/1',
         'comments': [
             {
-                'timestamp': int(time.time()) - 25*60*60,
+                'timestamp': int(time.time()) - 25 * 60 * 60,
                 'reviewer': {'username': 'ci-bot'},
                 'message': 'Old timestamp from a ci-bot, gets filtered\n'
-                    '- passing-job http://example.com/1 : SUCCESS in 1m 00s\n'
-                    '- failing-job http://example.com/2 : FAILURE in 1h 01m 00s'
+                '- passing-job http://example.com/1 : SUCCESS in 1m 00s\n'
+                '- failing-job http://example.com/2 : FAILURE in 1h 01m 00s'
             },
             {
                 'timestamp': int(time.time()) - 90,
@@ -31,19 +28,19 @@ mocked_data = [
                 'timestamp': int(time.time()) - 60,
                 'reviewer': {'username': 'good-bot'},
                 'message': 'Comment from ci-bot with fresh timestamp:\n'
-                    '- passing-job http://example.com/1 : SUCCESS in 1m 01s\n'
-                    '- failing-job http://example.com/2 : FAILURE in 1h 01m 01s\n'
-                    '- pass-and-fail-job http://example.com/3 : SUCCESS in 01s\n'
-                    '- filtered-job-nv http://example.com/4 : FAILURE in 02m 01s'
+                '- passing-job http://example.com/1 : SUCCESS in 1m 01s\n'
+                '- failing-job http://example.com/2 : FAILURE in 1h 01m 01s\n'
+                '- pass-and-fail-job http://example.com/3 : SUCCESS in 01s\n'
+                '- filtered-job-nv http://example.com/4 : FAILURE in 02m 01s'
             },
             {
                 'timestamp': int(time.time()) - 30,
                 'reviewer': {'username': 'good-bot'},
                 'message': 'Second comment from ci-bot with fresh timestamp:\n'
-                    '* passing-job http://example.com/1 : SUCCESS in 1m 02s\n'
-                    '* failing-job http://example.com/2 : FAILURE in 1h 01m 02s\n'
-                    '* pass-and-fail-job http://example.com/3 : FAILURE in 01s\n'
-                    '* filtered-job-nv http://example.com/4 : FAILURE in 02m 01s'
+                '* passing-job http://example.com/1 : SUCCESS in 1m 02s\n'
+                '* failing-job http://example.com/2 : FAILURE in 1h 01m 02s\n'
+                '* pass-and-fail-job http://example.com/3 : FAILURE in 01s\n'
+                '* filtered-job-nv http://example.com/4 : FAILURE in 02m 01s'
             },
         ],
     },
@@ -56,17 +53,20 @@ mocked_data = [
                 'timestamp': int(time.time()) - 60,
                 'reviewer': {'username': 'good-bot'},
                 'message': 'Comment from ci-bot with fresh timestamp:\n'
-                    '- failing-job-queens http://example.com/2 : FAILURE in 1h 01m 01s\n'
+                '- failing-job-queens http://example.com/2 : '
+                'FAILURE in 1h 01m 01s\n'
             },
             {
                 'timestamp': int(time.time()) - 30,
                 'reviewer': {'username': 'good-bot'},
                 'message': 'Second comment from ci-bot with fresh timestamp:\n'
-                    '* failing-job-queens http://example.com/2 : FAILURE in 1h 01m 02s\n'
+                '* failing-job-queens http://example.com/2 : '
+                'FAILURE in 1h 01m 02s\n'
             },
         ],
     }
 ]
+
 
 def mock_response(*args, **kwargs):
     # TODO: make this return values according to the IDs in changeIDs
@@ -74,13 +74,13 @@ def mock_response(*args, **kwargs):
     response += "\n{'footer': 'doesntmatter'}"
     return response
 
+
 class GateStatusTestCase(PluginTestCase):
     plugins = ('GateStatus',)
     config = {'supybot.plugins.GateStatus.userFilter': ['good-bot'],
               'supybot.plugins.GateStatus.timeLimit': 24,
               'supybot.plugins.GateStatus.changeIDs': ['I100000', 'Ideadbeef'],
-              'supybot.plugins.GateStatus.sshCommand': 'ssh foo@bar',
-             }
+              'supybot.plugins.GateStatus.sshCommand': 'ssh foo@bar'}
 
     def testFetchData(self):
         subprocess.check_output = mock_response
@@ -91,8 +91,8 @@ class GateStatusTestCase(PluginTestCase):
     def testFilterComments(self):
         gs = GateStatus.GateStatus(self.irc)
         output = gs.filter_comments(mocked_data[0]['comments'])
-        #from pprint import pprint
-        #pprint(output)
+        # from pprint import pprint
+        # pprint(output)
         self.assertEqual(len(output), 2)
 
     def testParseComments(self):
@@ -113,7 +113,8 @@ class GateStatusTestCase(PluginTestCase):
         subprocess.check_output = mock_response
         gs = GateStatus.GateStatus(self.irc)
         output = gs.job_report()
-        self.assertEqual(output,
+        self.assertEqual(
+            output,
             'FAILING CHECK JOBS on stable/queens: failing-job-queens @ '
             'http://review.example.com/2, master: failing-job @ '
             'http://review.example.com/1')
@@ -121,7 +122,8 @@ class GateStatusTestCase(PluginTestCase):
     def testGateStatus(self):
         subprocess.check_output = mock_response
         self.assertNotError('gatestatus')
-        self.assertResponse('gatestatus',
+        self.assertResponse(
+            'gatestatus',
             'FAILING CHECK JOBS on stable/queens: failing-job-queens @ '
             'http://review.example.com/2, master: failing-job @ '
             'http://review.example.com/1')
@@ -130,16 +132,16 @@ class GateStatusTestCase(PluginTestCase):
         subprocess.check_output = mock_response
         gs = GateStatus.GateStatus(self.irc)
         output = gs.user_report()
-        self.assertEqual(output,
+        self.assertEqual(
+            output,
             "Current userFilter: ['good-bot']; all users and comments "
             "within the timeLimit: foouser (1), good-bot (4)")
 
     def testPrintUsers(self):
         subprocess.check_output = mock_response
-        self.assertResponse('printusers',
+        self.assertResponse(
+            'printusers',
             "Current userFilter: ['good-bot']; all users and comments "
             "within the timeLimit: foouser (1), good-bot (4)")
-
-
 
 # vim:set shiftwidth=4 softtabstop=4 expandtab textwidth=79:
