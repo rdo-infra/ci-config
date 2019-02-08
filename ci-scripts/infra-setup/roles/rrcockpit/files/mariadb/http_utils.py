2ae482d7f3df4a901906a124ce198827aeec4831
Quique Llorente <ellorent@redhat.com>
2019-02-08 12:26:07 +0100

    cockpit-ci: use mariadb for noop
    Change-Id: I91bb43c59bae7498b0510c7030e07ec2a6e5954e

---
+++ ci-scripts/infra-setup/roles/rrcockpit/files/mariadb/http_utils.py
@@ -0,0 +1,17 @@
+import requests
+import logging
+
+
+def get(url, query={}, timeout=20, json_view=True):
+
+    try:
+        response = requests.get(url, params=query, timeout=timeout)
+    except Exception:
+        logging.exception("Cannot get {}".format(url))
+        pass
+    else:
+        if response and response.ok:
+            if json_view:
+                return response.json()
+            return response.text
+    return None
---
+++ ci-scripts/infra-setup/roles/rrcockpit/files/mariadb/load_noop_mysql.sql
@@ -0,0 +1,20 @@
+USE rrcockpit;
+
+CREATE TABLE IF NOT EXISTS noop_builds (
+    branch VARCHAR(255) NOT NULL,
+    type VARCHAR(255) NOT NULL,
+    job VARCHAR(255) NOT NULL,
+    log_url VARCHAR(255) NOT NULL,
+    result VARCHAR(255) NOT NULL,
+    success INT,
+    ts TIMESTAMP,
+    description TEXT NOT NULL,
+    PRIMARY KEY (branch, type, job)
+);
+
+TRUNCATE TABLE noop_builds;
+
+LOAD DATA LOCAL INFILE '/tmp/noop.csv' \n
+INTO TABLE noop_builds\n
+FIELDS TERMINATED BY ',' \n
+LINES TERMINATED BY '\n';
