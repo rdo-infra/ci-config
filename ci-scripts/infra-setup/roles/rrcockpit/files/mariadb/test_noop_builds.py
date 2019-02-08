9e934740cd447fab201ee73a36c06f55ab9dfd15
Quique Llorente <ellorent@redhat.com>
2019-02-08 12:26:07 +0100

    cockpit-ci: use mariadb for noop
    Change-Id: I91bb43c59bae7498b0510c7030e07ec2a6e5954e

---
+++ ci-scripts/infra-setup/roles/rrcockpit/files/mariadb/test_noop_builds.py
@@ -0,0 +1,19 @@
+import pytest
+import noop_build
+
+# We get all the combinations and attack gerrit
+@pytest.mark.parametrize("release", ['master', 'rocky', 'queens', 'pike'])
+@pytest.mark.parametrize("type", ["upstream", "rdo"])
+def test_infra(release, type):
+    messages = noop_build.get_messages(release)
+    assert(isinstance(messages, list))
+    assert(len(messages) > 0)
+    last_message = noop_build.get_last_message(messages, type)
+    assert(last_message)
+    builds = noop_build.get_builds(last_message)
+    assert(builds)
+    assert(isinstance(builds, list))
+    assert(len(builds) > 0)
+    csv = noop_build.compose_as_csv(release, type, builds)
+    assert(csv)
+    assert(len(csv) > 0)
