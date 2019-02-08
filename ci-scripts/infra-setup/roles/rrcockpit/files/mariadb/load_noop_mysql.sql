19aaa00955fcaa5c83073c21ce290b2cd987b909
Quique Llorente <ellorent@redhat.com>
2019-02-08 12:26:07 +0100

    cockpit-ci: use mariadb for noop
    Change-Id: I91bb43c59bae7498b0510c7030e07ec2a6e5954e

---
+++ ci-scripts/infra-setup/roles/rrcockpit/files/mariadb/load_noop_mysql.sql
@@ -0,0 +1,19 @@
+USE rrcockpit;
+
+CREATE TABLE IF NOT EXISTS noop_builds (
+    branch VARCHAR(255) NOT NULL,
+    type VARCHAR(255) NOT NULL,
+    job VARCHAR(255) NOT NULL,
+    log_url VARCHAR(255) NOT NULL,
+    result INT,
+    ts VARCHAR(255) NOT NULL,
+    description TEXT NOT NULL,
+    PRIMARY KEY (branch, type, job)
+);
+
+TRUNCATE TABLE noop_builds;
+
+LOAD DATA LOCAL INFILE '/tmp/noop.csv' \n
+INTO TABLE noop_builds\n
+FIELDS TERMINATED BY ',' \n
+LINES TERMINATED BY '\n';
---
+++ ci-scripts/infra-setup/roles/rrcockpit/files/mariadb/noop_build.py
@@ -0,0 +1,85 @@
+#!/usr/bin/env python
+
+import argparse
+import http_utils
+import json
+import re
+
+GERRIT_URL = 'https://review.openstack.org'
+
+CI_NAME = {
+    'upstream': 'zuul',
+    'rdo': 'rdothirdparty'
+}
+
+REVIEWS = {
+    'master': 560445,
+    'rocky': 604293,
+    'queens': 567224,
+    'pike': 602248
+}
+
+BUILD_REGEX = re.compile('^[*-] (?P<job>.*?) (?P<url>.*?) : (?P<result>[^ ]+) '
+                                  '?(?P<comment>.*)$')
+
+def get_messages(release):
+    review = REVIEWS[release]
+    detail_url = "{}/changes/{}/detail".format(GERRIT_URL, review)
+    response = http_utils.get(url=detail_url, json_view=False)
+    sanitized_content = "\n".join(response.split("\n")[1:])
+    detail = json.loads(sanitized_content)
+    return detail['messages']
+
+def get_last_message(messages, type):
+    for message in reversed(messages):
+        if message['author']['username'] == CI_NAME[type]:
+            return message
+    return None
+
+def get_builds(message):
+    builds = []
+    for line in message['message'].splitlines():
+        match = BUILD_REGEX.match(line)
+        if match:
+            build = {}
+            build['job'] = match.group('job')
+            build['url'] = match.group('url')
+            build['result'] = match.group('result')
+            build['comment'] = match.group('comment')
+            build['date'] = message['date']
+            builds.append(build)
+    return builds
+
+def compose_as_csv(release, type, builds):
+    csv = ""
+    for build in builds:
+        csv += ('{},{},{},{},{},{},{}\n').format(
+            release,
+            type,
+            build['job'],
+            build['url'],
+            1 if build['result'] == 'SUCCESS' else 0,
+            build['date'],
+            build['comment'])
+    return csv
+
+def main():
+    parser = argparse.ArgumentParser(
+        description="Retrieve as influxdb zuul builds")
+
+    parser.add_argument(
+        '--type',
+        default="upstream",
+        help="(default: %(default)s)")
+    parser.add_argument(
+        '--release', default="master", help="(default: %(default)s)")
+
+    args = parser.parse_args()
+
+    messages = get_messages(args.release)
+    last_message = get_last_message(messages, args.type)
+    builds = get_builds(last_message)
+    print(compose_as_csv(args.release, args.type, builds))
+
+if __name__ == '__main__':
+    main()
