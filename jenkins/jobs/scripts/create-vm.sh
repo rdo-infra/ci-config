38dc30392ad552d5bc48ba571b355f3175062ac8
Alfredo Moralejo <amoralej@redhat.com>
2019-03-06 16:03:40 +0100

    Move weirdo jobs to ci aggretate temporarily
    RDO aggregate is currently broken in RDO Cloud. Until fixed, let's move
    weirdo jobs to the CI aggregate.
    
    Change-Id: I618406388ee8ca1a09515b6cc1b3202eead98f0d

--- jenkins/jobs/scripts/create-vm.sh
+++ jenkins/jobs/scripts/create-vm.sh
@@ -20,7 +20,7 @@ NETWORK=${NETWORK:-private}
 NAME="${JOB_NAME_SIMPLIFIED}-${BUILD_NUMBER}"
 IMAGE=${IMAGE:-template-rdo-centos-7}
 TIMEOUT=${TIMEOUT:-120}
-FLAVOR=${FLAVOR:-rdo.m1.nodepool}
+FLAVOR=${FLAVOR:-ci.m1.nodepool}
 VM_INFO="${WORKSPACE}/vminfo.json"
 ANSIBLE_PYTHON_INTERPRETER=${ANSIBLE_PYTHON_INTERPRETER:-/usr/bin/python}
 
