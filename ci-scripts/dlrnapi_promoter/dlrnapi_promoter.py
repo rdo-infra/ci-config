785ddc63436f7c2c1e9ed7430d33a4227c6aa71f
Gabriele Cerami <gcerami@redhat.com>
2019-03-12 13:56:41 +0000

    Container-push: make the playbook distro aware
    Prepare the playbook to handle containers built for a different
    distribution and version.
    This change just removes the hardcoding from the playbook. Doesn't deal
    yet with the passing of the correct distro/version to the playbook from
    the dlrnapi_promoter (harder and more time consuming task that needs
    design)
    
    Change-Id: If1b4e129a0c6e244af12f0be1eca5c6c7db0fb23

--- ci-scripts/dlrnapi_promoter/dlrnapi_promoter.py
+++ ci-scripts/dlrnapi_promoter/dlrnapi_promoter.py
@@ -133,6 +133,8 @@ def tag_containers(new_hashes, release, promote_name):
     env['FULL_HASH'] = new_hashes['full_hash']
     env['PROMOTE_NAME'] = promote_name
     env['SCRIPT_ROOT'] = script_root
+    env['DISTRO_NAME'] = "centos"
+    env['DISTRO_VERSION'] = "7"
     promote_playbook = (
         script_root + 'ci-scripts/container-push/container-push.yml'
     )
