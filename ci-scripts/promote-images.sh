08ccaca40715444c92e5dad562485b5b60dc2373
Sorin Sbarnea <ssbarnea@redhat.com>
2019-03-20 11:42:34 +0000

    Make promote-images.sh fail on sftp failures
    * Avoids annoying ssh command warnings.
    * Keeps sftp command return code.
    * Display location of images at the end.
    
    Change-Id: If65633c2358ba739b17f60020f01918beaf32d6f
    Related-Bug: https://bugs.launchpad.net/tripleo/+bug/1823173

--- ci-scripts/promote-images.sh
+++ ci-scripts/promote-images.sh
@@ -10,11 +10,16 @@ PROMOTED_HASH=$2
 LINK_NAME=$3
 
 function sftp_command {
-    echo "$1" | sftp \
-        -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null \
-        uploader@images.rdoproject.org
+    # "-b -" assures that sftp command exit code is returned
+    sftp -b - \
+        -o LogLevel=error -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null \
+        uploader@images.rdoproject.org <<EOF
+$1
+EOF
 }
 
+# needed for new releases, keep it:
+sftp_command "mkdir -p /var/www/html/images/$RELEASE/rdo_trunk/"
 # sftp rename oldpath newpath
 # remove n-1 image soft link
 sftp_command "rm /var/www/html/images/$RELEASE/rdo_trunk/previous-${LINK_NAME}"
@@ -22,3 +27,5 @@ sftp_command "rm /var/www/html/images/$RELEASE/rdo_trunk/previous-${LINK_NAME}"
 sftp_command "rename /var/www/html/images/$RELEASE/rdo_trunk/$LINK_NAME /var/www/html/images/$RELEASE/rdo_trunk/previous-${LINK_NAME}"
 # promote new hash with link
 sftp_command "ln -s /var/www/html/images/$RELEASE/rdo_trunk/$PROMOTED_HASH /var/www/html/images/$RELEASE/rdo_trunk/$LINK_NAME"
+
+echo "INFO: New images should be accessible at https://images.rdoproject.com/$RELEASE/rdo_trunk/$LINK_NAME"
