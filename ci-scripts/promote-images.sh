df9199fa94f2b253bc9daf6ebc21f7b63c8c6229
Sorin Sbarnea <ssbarnea@redhat.com>
2019-03-20 11:42:34 +0000

    [POC] Ease usage of promote-images.sh
    Check parameters numbers and display help text if needed.
    
    Allows it to be used for making initial promotion of new releases, so
    it will create required folders and be able to symlink to master
    images.
    
    Makes link-name optional, defaulting to tripleo-current when not
    specified.
    
    Avoids annoying ssh command warnings.
    
    Keeps sftp command return code.
    
    Display location of images at the end.
    
    Change-Id: If65633c2358ba739b17f60020f01918beaf32d6f
    TBD: detect promoted-hash value to be used.

--- ci-scripts/promote-images.sh
+++ ci-scripts/promote-images.sh
@@ -1,24 +1,47 @@
 #!/bin/bash
 # Script to promote qcow images on rdoproject (images.rdoproject.org)
-# Usage: promote-images.sh <release> <promoted_hash> <link_name>
-# Example: promote-images.sh master f442a3aa35981c3d6d7e312599dde2a1b1d202c9_0468cca4 current-tripleo
 
 set -eux
 
+if [ "$#" -lt 3 ]; then
+  echo """
+  Usage: promote-images.sh <release> <promoted_hash> <link_name> [source-release]
+
+  Source release param is optional and is used only to symlink files from other
+  release, useful only when manually doing a first pre-release.
+
+  Example: promote-images.sh master f442a3aa35981c3d6d7e312599dde2a1b1d202c9_0468cca4 current-tripleo
+  """
+  exit 1
+fi
 RELEASE=$1
 PROMOTED_HASH=$2
-LINK_NAME=$3
+LINK_NAME=${3:-current-tripleo}
+SOURCE_RELEASE=${4:-$RELEASE}
+
+# TODO(ssbarnea): find a way to latest promoted_hash programately and make param optional
+# or allow use of special "latest" tag.
+# dlrnapi --url https://trunk.rdoproject.org/api-centos-master-uc promotion-get
 
 function sftp_command {
-    echo "$1" | sftp \
-        -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null \
-        uploader@images.rdoproject.org
+    # "-b -" assures that sftp command exit code is returned
+    sftp -b - \
+        -o LogLevel=error -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null \
+        uploader@images.rdoproject.org <<EOF
+$1
+EOF
 }
 
+# needed for new releases, keep it:
+sftp_command "mkdir /var/www/html/images/$RELEASE"
+sftp_command "mkdir /var/www/html/images/$RELEASE/rdo_trunk/"
 # sftp rename oldpath newpath
 # remove n-1 image soft link
-sftp_command "rm /var/www/html/images/$RELEASE/rdo_trunk/previous-${LINK_NAME}"
+
+sftp_command "!rm -f /var/www/html/images/$RELEASE/rdo_trunk/previous-${LINK_NAME}"
 # rename the current link to previous-$LINK_NAME to keep n-1 copy of image
-sftp_command "rename /var/www/html/images/$RELEASE/rdo_trunk/$LINK_NAME /var/www/html/images/$RELEASE/rdo_trunk/previous-${LINK_NAME}"
+sftp_command "!mv /var/www/html/images/$RELEASE/rdo_trunk/$LINK_NAME /var/www/html/images/$RELEASE/rdo_trunk/previous-${LINK_NAME}"
 # promote new hash with link
-sftp_command "ln -s /var/www/html/images/$RELEASE/rdo_trunk/$PROMOTED_HASH /var/www/html/images/$RELEASE/rdo_trunk/$LINK_NAME"
+sftp_command "ln -s /var/www/html/images/$SOURCE_RELEASE/rdo_trunk/$PROMOTED_HASH /var/www/html/images/$RELEASE/rdo_trunk/$LINK_NAME"
+
+echo "INFO: New images should be accessible at https://images.rdoproject.com/$RELEASE/rdo_trunk/$LINK_NAME"
