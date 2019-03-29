6e66b0fbc1fdea14e862041398d1b184408d211d
Quique Llorente <ellorent@redhat.com>
2019-03-29 11:31:09 +0100

    [WIP] Promote different distros
    Change-Id: Ib0d3462db7c066130438d7f7925bc5a79e05ea2e
    Story: https://tree.taiga.io/project/tripleo-ci-board/task/860?kanban-status=1447274

--- ci-scripts/dlrnapi_promoter/dlrn-promoter.sh
+++ ci-scripts/dlrnapi_promoter/dlrn-promoter.sh
@@ -52,7 +52,7 @@ while true; do
     /usr/bin/timeout --preserve-status -k 120m 115m python ~/ci-config/ci-scripts/dlrnapi_promoter/dlrnapi_promoter.py ~/ci-config/ci-scripts/dlrnapi_promoter/config/CentOS-7/rocky.ini
 
     # promoter script for the Fedora-28 master branch
-    #/usr/bin/timeout --preserve-status -k 120m 115m python ~/ci-config/ci-scripts/dlrnapi_promoter/dlrnapi_promoter.py ~/ci-config/ci-scripts/dlrnapi_promoter/config/Fedora-28/master.ini
+    /usr/bin/timeout --preserve-status -k 120m 115m python ~/ci-config/ci-scripts/dlrnapi_promoter/dlrnapi_promoter.py ~/ci-config/ci-scripts/dlrnapi_promoter/config/Fedora-28/master.ini
 
     # Sleep 10 minutes
     sleep 600
