9fd878b05e1de78a8ad3f7630317151a872ec458
Sorin Sbarnea <ssbarnea@redhat.com>
2019-04-04 16:59:52 +0100

    Refactor image promoting to be distro aware
    Adds ability to mention --distro and --distro-version while still
    keeping backwards compatibility (defaults to centos 7).
    
    Improved usage and documentation.
    
    Assure that the script output is easy to read and provides enough
    information in order to debug potential failures. Includes two fail-safe
    checks: source exists and target exists.
    
    Due to current symlinks on the webserver, there is no impact on
    consumers of the old urls. New image promotions made on centos7 are
    still going to be accesible at their old location (the ones that do
    not use /centos7/ suffix).
    
    Change-Id: I4c4f91118e3b5ad4407a8dad707c94502d02bd0b
    Closes-Bug: https://bugs.launchpad.net/tripleo/+bug/1823173

--- ci-scripts/promote-images.sh
+++ ci-scripts/promote-images.sh
@@ -1,15 +1,64 @@
 #!/bin/bash
-# Script to promote qcow images on rdoproject (images.rdoproject.org)
-# Usage: promote-images.sh <release> <promoted_hash> <link_name>
-# Example: promote-images.sh master f442a3aa35981c3d6d7e312599dde2a1b1d202c9_0468cca4 current-tripleo
+set -euo pipefail
 
-set -euxo pipefail
+# defaults used for backwards compatibility
+: ${OPT_DISTRO:=centos}
+: ${OPT_DISTRO_VERSION:=7}
 : ${OPT_WEBROOT:=/var/www/html/images}
 : ${OPT_WEBSITE:=https://images.rdoproject.org}
+
+function usage {
+    cat <<-EOF
+    Script to promote qcow images on ${OPT_WEBSITE}
+    Usage: promote-images.sh [--distro centos] [--distro-version 7] <release> <promoted_hash> <link_name>
+    Example: promote-images.sh master f442a3aa35981c3d6d7e312599dde2a1b1d202c9_0468cca4 current-tripleo
+EOF
+}
+
+function finish {
+    rc=$?
+    if [[ $rc -eq 0 ]]; then
+        echo "INFO: $0 succeded updating ${TARGET_URL:-} to point to ${SOURCE_URL:-} " >&2
+    else
+        echo "ERROR: $0 failed with $rc exit code" >&2
+    fi
+    exit $rc
+}
+trap finish EXIT
+
+while [ "x${1:-}" != "x" ]; do
+    case "$1" in
+        --distro)
+            # we lowercase distro string, to avoid accidents
+            OPT_DISTRO=$(echo $2| tr '[:upper:]' '[:lower:]')
+            shift
+            ;;
+        --distro-version)
+            OPT_DISTRO_VERSION=$2
+            shift
+            ;;
+        -*) echo "ERROR: unknown option: $1" >&2
+            usage >&2
+            exit 2
+            ;;
+        *)
+            break
+            ;;
+    esac
+    shift
+done
+
+if [ "$#" -ne 3 ]; then
+    usage >&2
+    echo "ERROR: invalid number of parameters" >&2
+    exit 2
+fi
 
 RELEASE=$1
 PROMOTED_HASH=$2
 LINK_NAME=$3
+
+DISTRO_AND_VERSION="${OPT_DISTRO}${OPT_DISTRO_VERSION}"
 
 function sftp_command {
     # "-b -" assures that sftp command exit code is returned
@@ -20,21 +69,30 @@ $1
 EOF
 }
 
+# check if target url exists and fail-fast if it doesn't
+SOURCE_URL=${OPT_WEBSITE}/$DISTRO_AND_VERSION/$RELEASE/rdo_trunk/$PROMOTED_HASH
+curl -L --silent --head --fail $SOURCE_URL >/dev/null || {
+    echo "ERROR: The promoted hash was not found: $SOURCE_URL" >&2
+    exit 3
+}
+# needed for new releases, keep it:
 # very important for new releases, where some folders may not even exist
 # "mkdir: Invalid flag -p" may be returned in some cases, do not use it.
 # "-" prefix tells it to ignore errors (if folder already exists)
-sftp_command "-mkdir ${OPT_WEBROOT}/$RELEASE/"
-sftp_command "-mkdir ${OPT_WEBROOT}/$RELEASE/rdo_trunk/"
+sftp_command "-mkdir ${OPT_WEBROOT}/$DISTRO_AND_VERSION/"
+sftp_command "-mkdir ${OPT_WEBROOT}/$DISTRO_AND_VERSION/$RELEASE/"
+sftp_command "-mkdir ${OPT_WEBROOT}/$DISTRO_AND_VERSION/$RELEASE/rdo_trunk/"
 
 # sftp rename oldpath newpath
 # remove n-1 image soft link
-sftp_command "-rm ${OPT_WEBROOT}/$RELEASE/rdo_trunk/previous-${LINK_NAME}"
+sftp_command "-rm ${OPT_WEBROOT}/$DISTRO_AND_VERSION/$RELEASE/rdo_trunk/previous-${LINK_NAME}"
 # rename the current link to previous-$LINK_NAME to keep n-1 copy of image
-sftp_command "-rename ${OPT_WEBROOT}/$RELEASE/rdo_trunk/$LINK_NAME ${OPT_WEBROOT}/$RELEASE/rdo_trunk/previous-${LINK_NAME}"
+sftp_command "-rename ${OPT_WEBROOT}/$DISTRO_AND_VERSION/$RELEASE/rdo_trunk/$LINK_NAME ${OPT_WEBROOT}/$DISTRO_AND_VERSION/$RELEASE/rdo_trunk/previous-${LINK_NAME}"
 # promote new hash with link
-sftp_command "ln -s ${OPT_WEBROOT}/$RELEASE/rdo_trunk/$PROMOTED_HASH ${OPT_WEBROOT}/$RELEASE/rdo_trunk/$LINK_NAME"
+sftp_command "ln -s ${OPT_WEBROOT}/$DISTRO_AND_VERSION/$RELEASE/rdo_trunk/$PROMOTED_HASH ${OPT_WEBROOT}/$DISTRO_AND_VERSION/$RELEASE/rdo_trunk/$LINK_NAME"
 
-curl -L --silent --head --fail ${OPT_WEBSITE}/$RELEASE/rdo_trunk/$LINK_NAME >/dev/null || {
-    echo "ERROR: The target is invalid: ${OPT_WEBSITE}/$RELEASE/rdo_trunk/$LINK_NAME" >&2
+TARGET_URL=${OPT_WEBSITE}/$DISTRO_AND_VERSION/$RELEASE/rdo_trunk/$LINK_NAME
+curl -L --silent --head --fail $TARGET_URL >/dev/null || {
+    echo "ERROR: The target is invalid: $TARGET_URL" >&2
     exit 3
 }
