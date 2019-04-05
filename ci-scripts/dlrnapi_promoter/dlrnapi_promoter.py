d31c595be9761d0786ab04165e6b92b43261e575
Quique Llorente <ellorent@redhat.com>
2019-04-05 11:07:48 +0200

    promoter: Skip qcow tags for fedora
    For fedora we just run standalone no need for qcows this review just
    skip qcows tagging for fedora (there are stuff to fix there to be distro
    aware).
    
    Change-Id: I48ff97652813a90a58f524068b0d1723266ade57
    Closes-Bug: #1823173

--- ci-scripts/dlrnapi_promoter/dlrnapi_promoter.py
+++ ci-scripts/dlrnapi_promoter/dlrnapi_promoter.py
@@ -268,7 +268,12 @@ def promote_all_links(api, promote_from, job_reqs, dry_run, distro, release, lat
                     # this can be removed once ocata is EOL
                     if release not in ['ocata']:
                         tag_containers(new_hashes, distro, release, promote_name)
-                    tag_qcow_images(new_hashes, release, promote_name)
+
+                    # For fedora we just run standalone let's not tag images
+                    distro_name, _ = distro
+                    if distro_name.lower() != 'fedora':
+                        tag_qcow_images(new_hashes, release, promote_name)
+
                     promote_link(api, new_hashes, promote_name)
                     logger.info('SUCCESS promoting %s as %s (%s)',
                                 current_name, promote_name, new_hashes)
