fbce3589e04d2364c1e9ade7ce6975325fa9cf34
Wes Hayutin <weshayutin@gmail.com>
2019-04-17 15:58:05 -0600

    use port 8080 to create key and export
    just making the scripts slightly more user
    friendly.
    
    Change-Id: Ifc3f4838a74e93c79f7e32272a79c81dd008faff

--- ci-scripts/infra-setup/roles/rrcockpit/files/grafana/create-api-key.py
+++ ci-scripts/infra-setup/roles/rrcockpit/files/grafana/create-api-key.py
@@ -7,7 +7,7 @@ if __name__ == '__main__':
 
     parser = argparse.ArgumentParser(description="Print created api key")
 
-    parser.add_argument('--url', default="http://admin:admin@localhost")
+    parser.add_argument('--url', default="http://admin:admin@localhost:8080")
     parser.add_argument('--key-name', required=True)
 
     args = parser.parse_args()
