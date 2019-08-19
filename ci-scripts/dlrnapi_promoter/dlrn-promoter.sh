ebeecc5a99c9ef01f941f11a7fd99dec6b7b873f
Gabriele Cerami <gcerami@redhat.com>
2019-08-19 12:52:54 +0100

    [WIP] invoke promoter script w/ staging criteria
    The promoter script needs to emulate a fake criteria for staging
    env.
    
    Co-Authored-By: Rafael Folco <rfolco@redhat.com>
    
    Change-Id: Ic51ae1d86198b6c48475e361f72295da5393ce70

--- ci-scripts/dlrnapi_promoter/dlrn-promoter.sh
+++ ci-scripts/dlrnapi_promoter/dlrn-promoter.sh
@@ -1,5 +1,9 @@
 #!/bin/bash -x
 
+usage(){
+    echo "$0 Usage: $ dlrn-promoter.sh [-t 115m] [-k 120m] [-s] [-h]"
+}
+
 # Source secrets
 source ~/registry_secret
 source ~/dlrnapi_secret
@@ -10,6 +14,26 @@ KILLTIME=120m
 
 RELEASES=( "CentOS-7/master" "CentOS-7/stein" "CentOS-7/rocky" \
            "CentOS-7/queens" "CentOS-7/ocata" "RedHat-8/master" )
+
+while getopts "t:k:sh" arg; do
+    case $arg in
+    t)
+        TIMEOUT="${OPTARG}"
+        ;;
+    k)
+        KILLTIME="${OPTARG}"
+        ;;
+    s)
+        echo "Staging promoter mode enabled"
+        RELEASES=( "CentOS-7/staging" )
+        export IMAGE_SERVER_USER_HOST="test@localhost"
+        ;;
+    h)
+        usage
+        exit 0
+        ;;
+    esac
+done
 
 for r in "${RELEASES[@]}"; do
     /usr/bin/timeout --preserve-status -k $KILLTIME $TIMEOUT \
