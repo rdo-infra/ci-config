3fbfa94beb13d70fac526fd1f51fbe3f600d0c8b
Gabriele Cerami <gcerami@redhat.com>
2019-08-20 15:14:19 +0100

    [WIP] setup staging environment
    status:
    - injects fixtures in dlrnapi database (commits, user, votes)
    - setup and teardown overcloud images hierarchy
    - setup and teardown containers locally
    
    missing:
    - setup and teardown container remotely
    
    Change-Id: I6cde100d630657c8051521ac9b15b8c7b549a649

---
+++ ci-scripts/dlrnapi_promoter/tests/staging-setup/test_staging_environment.py
@@ -0,0 +1,7 @@
+# This is the expected structure under root-dir
+
+# Asserts
+# - tree in root-dir conforms to sample/tree.txt
+# - docker images conforms to sample/docker_images.txt
+# - meta.yaml is equal to sample/meta.yaml
+
