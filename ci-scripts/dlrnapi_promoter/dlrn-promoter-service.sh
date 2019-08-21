563cab1c5f4fd2591f168f2bd0067038167beb96
Rafael Folco <rfolco@redhat.com>
2019-08-21 12:55:38 +0000

    [WIP] Staging promoter provisioning
    Configure a staging promoter using infra-setup role. This setup
    will be used to run a staging promotion to test changes to the
    promotion workflow and artifacts.
    
    Change-Id: I816a1170ee13defe89c35dea3f041ea3dd9ebbb8
    Story: https://tree.taiga.io/project/tripleo-ci-board/us/1105

--- ci-scripts/dlrnapi_promoter/dlrn-promoter-service.sh
+++ ci-scripts/dlrnapi_promoter/dlrn-promoter-service.sh
@@ -10,10 +10,14 @@ source ~/promoter_venv/bin/activate
 while true; do
 
     #fetch the latest ci-config
-    cd ~/ci-config; git reset --hard origin/master && git pull >/dev/null
+    #TODO(rfolco): convert this in a condition in j2 template,
+    # skip if setup_staging is true
+    #cd ~/ci-config; git reset --hard origin/master && git pull >/dev/null
 
     #fetch the latest dlrnapi_client and dependencies
-    pip install -U -r ~/ci-config/ci-scripts/dlrnapi_promoter/requirements.txt
+    #TODO(rfolco): convert this in a condition in j2 template,
+    # skip if setup_staging is true
+    #pip install -U -r ~/ci-config/ci-scripts/dlrnapi_promoter/requirements.txt
 
     # call dlrn-promoter.sh for multiple releases/distros
     /bin/bash ~/ci-config/ci-scripts/dlrnapi_promoter/dlrn-promoter.sh
