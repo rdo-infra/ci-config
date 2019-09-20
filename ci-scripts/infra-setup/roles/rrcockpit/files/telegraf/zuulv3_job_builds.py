d94e4e29da8e4ecfc1b1726313754ebb396ad902
Wes Hayutin <weshayutin@gmail.com>
2019-09-19 21:53:03 -0600

    WIP2: DNM, test container-prep-time
    Change-Id: I76fff78514d9eac6ea3ce96dfdd6d024914a28f3

--- ci-scripts/infra-setup/roles/rrcockpit/files/telegraf/zuulv3_job_builds.py
+++ ci-scripts/infra-setup/roles/rrcockpit/files/telegraf/zuulv3_job_builds.py
@@ -1,11 +1,18 @@
-#!/usr/bin/env python
+#!/usr/bin/python
 
+from __future__ import print_function
 import argparse
-import datetime
+from datetime import datetime, timedelta
 import time
 import re
 import requests
 import yaml
+import urllib
+import urllib2
+
+import pdb
+
+import logging
 
 try:
     from urlparse import urljoin
@@ -51,7 +58,7 @@ cache.expire()
 
 
 def to_ts(d, seconds=False, pattern=TIMESTAMP_PATTERN):
-    return datetime.datetime.strptime(
+    return datetime.strptime(
         d, pattern).strftime('%s') + ('' if seconds else "000000000")
 
 
@@ -61,7 +68,7 @@ def to_seconds(duration):
         hours=x.tm_hour, minutes=x.tm_min, seconds=x.tm_sec).total_seconds()
 
 
-def get(url, query=None, timeout=20, json_view=True):
+def get(url, json_view, query=None, timeout=20):
     query = query or {}
     try:
         response = requests.get(url, params=query, timeout=timeout)
@@ -69,10 +76,13 @@ def get(url, query=None, timeout=20, json_view=True):
         # add later log file
         pass
     else:
+
         if response and response.ok:
             if json_view:
                 return response.json()
-            return response.text
+            else:
+                response.encoding = 'utf-8'
+                return response.text
     return None
 
 
@@ -86,25 +96,31 @@ def get_builds_info(url, query, pages, offset):
         else:
             query['skip'] = offset
         builds_api = url + "builds"
-        response = get(builds_api, query)
+        response = get(builds_api, True, query)
         if response is not None:
             builds += response
     return builds
 
 
-def get_file_from_build(build, file_relative_path):
+def get_file_from_build(build, file_relative_path, json_view):
     if 'log_url' in build:
         if build['log_url'].endswith("/html/"):
             build['log_url'] = build['log_url'].replace('html/', '')
         if build['log_url'].endswith("/cover/"):
             build['log_url'] = build['log_url'].replace('cover/', '')
         file_path = urljoin(build['log_url'], file_relative_path)
+
         if file_path not in cache:
-            r = requests.get(file_path)
-            if r.ok:
-                cache.add(
-                    file_path, yaml.safe_load(r.content),
-                    expire=259200)  # expire is 3 days
+            resp = get(file_path, json_view)
+
+            if resp is not None:
+                if json_view == True:
+                    cache.add(
+                        file_path, yaml.safe_load(resp),
+                        expire=259200)  # expire is 3 days
+                else:
+                    cache.add(
+                        file_path, resp)
             # Add negative cache
             else:
                 cache[file_path] = None
@@ -112,9 +128,10 @@ def get_file_from_build(build, file_relative_path):
         return cache[file_path]
 
 
-def add_inventory_info(build):
+def add_inventory_info(build, json_view=False):
     try:
-        inventory = get_file_from_build(build, "zuul-info/inventory.yaml")
+        inventory = get_file_from_build(build, "zuul-info/inventory.yaml", json_view)
+        inventory = yaml.safe_load(inventory)
         hosts = inventory['all']['hosts']
         host = hosts[hosts.keys()[0]]
         if 'nodepool' in host:
@@ -127,6 +144,70 @@ def add_inventory_info(build):
         pass
 
 
+def add_container_prep_time(build):
+    if build['log_url'] is not None:
+        job_terms = ['featureset', 'oooq', 'multinode']
+        skip_terms = ['update', 'upgrade', 'rocky']
+        skip_branch = ['stable/rocky', 'stable/queens']
+
+        if any(x in build['job_name'] for x in skip_terms):
+            build['container_prep_time'] = 0
+            return build
+        elif any(x in build['branch'] for x in skip_branch):
+            build['container_prep_time'] = 0
+            return build
+        elif any(x in build['job_name'] for x in job_terms):
+            file_relative_path = "logs/undercloud/home/zuul/install-undercloud.log.txt.gz"
+            respData = get_file_from_build(build, file_relative_path, json_view=False)
+        elif 'standalone' in build['job_name']:
+            file_relative_path = "logs/undercloud/home/zuul/standalone_deploy.log.txt.gz"
+
+            respData = get_file_from_build(build, file_relative_path, json_view=False)
+        else:
+            build['container_prep_time'] = 0
+            return build
+
+        if respData is None:
+            build['container_prep_time'] = -1
+            return build
+
+        # loop through the file looking for the container prep
+        # signature
+        count = 0
+        container_prep_begin = ""
+        container_prep_end = ""
+        container_prep_line = 'tripleo-container-image-prepare.log'
+        match=""
+        for line in respData.splitlines():
+            count += 1
+            try:
+                match = re.search(r'.*'+container_prep_line+'.*',str(line))
+            except Exception:
+                build['container_prep_time'] = -1
+                return build
+            if match:
+                container_prep_begin = \
+                    "-".join(line.decode("utf-8").split(" ")[0:2])
+                count = count + 2
+                end_line = respData.splitlines()[count]
+                container_prep_end = \
+                    "-".join(end_line.decode("utf-8").split(" ")[0:2])
+                break
+        try:
+            begin = datetime.strptime(container_prep_begin, '%Y-%m-%d-%H:%M:%S.%f' )
+            end = datetime.strptime(container_prep_end, '%Y-%m-%d-%H:%M:%S.%f' )
+            prep_container_time = end - begin
+            build['container_prep_time'] = prep_container_time
+        except ValueError as err:
+            try:
+                begin = datetime.strptime(container_prep_begin, '%Y-%m-%d-%H:%M:%S' )
+                end = datetime.strptime(container_prep_end, '%Y-%m-%d-%H:%M:%S' )
+                prep_container_time = end - begin
+            except ValueError as err:
+                build['container_prep_time'] = -1
+                return build
+
+
 def fix_task_name(task_name):
     return re.sub(r'/tmp/tripleo-modify-image.*', '/tmp/tripleo-modify-image',
                   task_name).replace(',', '_')
@@ -136,7 +217,7 @@ def print_influx_ara_tasks(build, ara_json_file):
     if build['job_name'] not in JOBS_FOR_ARA:
         return
     try:
-        tasks = get_file_from_build(build, ara_json_file)
+        tasks = get_file_from_build(build, ara_json_file, json_view=True)
         if tasks is None:
             return
         for task in tasks:
@@ -160,6 +241,10 @@ def print_influx_ara_tasks(build, ara_json_file):
 def influx(build):
 
     add_inventory_info(build)
+    add_container_prep_time(build)
+
+    if 'container_prep_time' not in build:
+        build['container_prep_time'] = 0
 
     if build['end_time'] is None:
         build['end_time'] = datetime.datetime.fromtimestamp(
@@ -172,6 +257,7 @@ def influx(build):
         duration = 0
     # Get the nodename
     return ('build,'
+            'container_prep_time=%s,'
             'type=%s,'
             'pipeline=%s,'
             'branch=%s,'
@@ -198,7 +284,8 @@ def influx(build):
             'provider="%s"'
             ' '
             '%s' %
-            (build['type'], build['pipeline'], 'none' if not build['branch']
+            (build['container_prep_time'], build['type'],
+             build['pipeline'], 'none' if not build['branch']
              else build['branch'], build['project'], build['job_name'],
              build['voting'], build['change'], build['patchset'], 'True'
              if build['result'] == 'SUCCESS' else 'False',
@@ -215,13 +302,16 @@ def influx(build):
 
 
 def print_influx(build_type, builds):
+    logging.basicConfig(level=logging.INFO, format='%(message)s')
+    logger = logging.getLogger()
+    logger.addHandler(logging.FileHandler('/tmp/ruck_rover_cache/zuulv3_build.log', 'a'))
+    print = logger.info
     if builds:
         for build in builds:
             build['type'] = build_type
             for ara_json in ARA_JSONS:
                 print_influx_ara_tasks(build, ara_json)
             print(influx(build))
-
 
 def main():
 
