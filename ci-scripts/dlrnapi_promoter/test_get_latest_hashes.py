86c34744f6a434df37aa29ffa50ec460ad1c0efa
Marios Andreou <marios@redhat.com>
2019-10-09 14:55:12 +0300

    Ensure named hashes are unchanged during a promotion run
    Logs error + raise Exception to abort promotion if named
    hashes change from when the promotion process starts.
    Tracked by TripleO CI squad with [1]
    
    [1] https://tree.taiga.io/project/tripleo-ci-board/task/1325
    Change-Id: Ia1fd360cc424be30ce0ec58403c8687c13f6c204

--- ci-scripts/dlrnapi_promoter/test_get_latest_hashes.py
+++ ci-scripts/dlrnapi_promoter/test_get_latest_hashes.py
@@ -143,3 +143,34 @@ def test_old_hashes_get_filtered_from_candidates(fetch_hashes_mock):
         mock.call('dlrn_api', 'promote_name', count=-1)])
 
     assert(obtained_hashes == expected_hashes)
+
+
+@mock.patch('dlrnapi_promoter.fetch_hashes')
+def test_named_hashes_unchanged(mock_fetch_hashes):
+    dlrn_start_hash = {
+            'timestamp': '1528085427',
+            'commit_hash': 'd1c5379369b24effdccfe5dde3e93bd21884ed27',
+            'distro_hash': 'cd4fb616ac3065794b8a9156bbe70ede3d77ef27'
+    }
+    dlrn_changed_hash = {
+            'timestamp': '1528085529',
+            'commit_hash': 'd1c5372341a61effdccfe5dde3e93bd21884ed27',
+            'distro_hash': 'cd4fb616ac30625a51ba9156bbe70ede3d7e1921'
+    }
+    promote_from = {'current-tripleo': 'foo', 'current-tripleo-rdo': 'bar'}
+    release = ('centos', '7')
+
+    # positive test for hashes_unchanged
+    mock_fetch_hashes.side_effect = [dlrn_start_hash, dlrn_start_hash]
+    start_named_hashes = dlrnapi_promoter.fetch_current_named_hashes(
+        release, promote_from, 'dlrn')
+    dlrnapi_promoter.start_named_hashes = start_named_hashes
+    mock_fetch_hashes.side_effect = [dlrn_start_hash, dlrn_start_hash]
+    dlrnapi_promoter.check_named_hashes_unchanged(release, promote_from,
+                                                  'dlrn')
+
+    # negative test
+    mock_fetch_hashes.side_effect = [dlrn_start_hash, dlrn_changed_hash]
+    with pytest.raises(Exception):
+        dlrnapi_promoter.check_named_hashes_unchanged(
+                                                release, promote_from, 'dlrn')
