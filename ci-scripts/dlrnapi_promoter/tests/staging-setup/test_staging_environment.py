0b8ed9cc562b303ad0cad4871eecef4ed0f1580b
Gabriele Cerami <gcerami@redhat.com>
2019-11-15 18:25:15 +0000

    Add support and test for secured registries
    multi-arch manifest commands don't work well with local secured
    self-signed registries, but we still need to test the roles with them,
    especially to ensure that docker logins work.
    This change adds support for secure registries for the staging
    environment, using self-signed certificate.
    
    Additional changes in tests
    - splits container-push tests into two categories, to get the
    coverage we want:
      - unsecure registries + multi-arch manifests push
      - secure registries + login (multi-arch manifests are disabled)
    - in container-push test registries variable are taken directly from the
      staging environment
    
    Additional changes in staging environment:
    - added ability to specify the config file to build stage from
    
    Change-Id: Id697322cfa917b7da8c04c3f9d80ff8d079b1ed6

--- ci-scripts/dlrnapi_promoter/tests/staging-setup/test_staging_environment.py
+++ ci-scripts/dlrnapi_promoter/tests/staging-setup/test_staging_environment.py
@@ -114,6 +114,21 @@ def test_registries(staged_env):
             for target in stage_info['registries']['targets']:
                 if registry['name'] == target['name']:
                     found = True
+                    if registry['secure']:
+                        # Check that registry marked as secure
+                        # have a auth_url defined
+                        assert "auth_url" in target
+                        # And we can log in with info provided
+                        try:
+                            docker_client.login(
+                                registry=target['auth_url'],
+                                username=target['username'],
+                                password=target['password'],
+                                dockercfg_path="/dev/null",
+                                reauth=True
+                            )
+                        except docker.errors.APIError:
+                            assert False, "Login failed"
             assert found
         # Check that the registries are up and running
         assert docker_client.containers.get(registry['name'])
