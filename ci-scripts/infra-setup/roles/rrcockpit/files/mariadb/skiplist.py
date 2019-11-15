72f369bd0ef32a6cb67996b759ef7073381d482a
Soniya Vyas <svyas@redhat.com>
2019-11-15 17:32:50 +0530

    Added script to parse html data to json
    This scirpt takes the html.gz file and returns the
    test_names and their respective status.
    
    Change-Id: I89af8cdd237461c4d0c420d08facbaa461291135
    Signed-off-by: Soniya Vyas<svyas@redhat.com>

---
+++ ci-scripts/infra-setup/roles/rrcockpit/files/mariadb/skiplist.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python
+# Copyright 2019 Red Hat, Inc.
+# All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+import tempest_file_downloader
+import tempest_html_json
+import glob
+import os
+import json
+
+
+parse_result = []
+previously_processed_files = [
+    'tempest.master.html',
+    'tempest.stein.html']
+recent_files = []
+
+
+def get_diff():
+    """
+    This function gives the difference of previously proccessed files and
+    recent files
+    """
+    list_of_files = glob.glob('/tmp/skip/*.html')
+    recent_files = max(list_of_files, key=os.path.getctime)
+    return list(set(previously_processed_files) - set(recent_files))
+
+
+def get_files():
+    """
+    This function gives the result of all the files parsed from html to json
+    """
+    tempest_file_downloader.get_last_build()
+    previously_processed_files.extend(get_diff())
+    for file in get_diff():
+        parse_result.append(tempest_html_json.main())
+    return parse_result
+
+
+def get_output_file():
+    json.dumps(get_files(), sort_keys=True, indent=2)
+
+
+def print_as_csv():
+    for file in get_files():
+        for result in file.values():
+            for release_name, test in result.iteritems():
+                for testname, status in test.iteritems():
+                    print(('{},{},{}').format(release_name, testname, status))
+
+
+if __name__ == "__main__":
+    print_as_csv()
