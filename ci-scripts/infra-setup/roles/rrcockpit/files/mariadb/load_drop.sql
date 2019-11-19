e61e29ea903991b0d8a271d8668567f562b64a82
Soniya Vyas <svyas@redhat.com>
2019-11-19 11:21:16 +0530

    Added processing and parsing functionality for skiplist tool
    This patch contains everything required for the tool to
    process the log files of different releases viz.
    [master, train, stein, rocky, queens] and dump the data to
    stdout.
    
    Here skiplist.py works like the proccessor and gets all
    possible files from get_last_build API of
    tempest_file_downloader.py and processes those files
    using tempest_html_json.py script.
    
    load_pass_mysql.sql is the sql script for defining
    schema required for the tool.
    
    Link of locally saved snapshot:-
    http://localhost:3000/dashboard/snapshot/J52oGFhbKBfz202fAc5HbboUG0qtGDO8
    
    Link of snapshot.raintank.io:-
    https://snapshot.raintank.io/dashboard/snapshot/sgpBmf1y4ASHwuINLS5IvCImtK6xOqyP
    
    Change-Id: I89af8cdd237461c4d0c420d08facbaa461291135
    Signed-off-by: Soniya Vyas<svyas@redhat.com>

--- ci-scripts/infra-setup/roles/rrcockpit/files/mariadb/load_drop.sql
+++
@@ -1,2 +0,0 @@
-DROP DATABASE IF EXISTS rrcockpit;
-CREATE DATABASE rrcockpit;
---
+++ ci-scripts/infra-setup/roles/rrcockpit/files/mariadb/skiplist.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python
+# Copyright 2019 Red Hat, Inc.
+# All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+import json
+import tempest_file_downloader
+import tempest_html_json
+
+from datetime import datetime
+
+
+parse_result = []
+
+
+def get_files():
+    """
+    This function gives the result of all the files parsed
+    """
+    release_names = ['master', 'train', 'stein', 'rocky', 'queens']
+    for release_name in release_names:
+        if release_name in ['master', 'train']:
+            file_name = "stestr_results.html"
+        else:
+            file_name = "tempest.html.gz"
+        try:
+            log_url, release = tempest_file_downloader.get_last_build(
+                    "periodic-tripleo-ci-centos-7-ovb-1ctlr_2comp-"
+                    "featureset021-" + release_name, file_name)
+        except Exception:
+            pass
+        parse_result.append(tempest_html_json.output(log_url, release))
+    return parse_result
+
+
+def get_output_file():
+    """
+    This function returns json serialized data
+    """
+    json.dumps(get_files(), sort_keys=True, indent=2)
+
+
+def print_as_csv():
+    """
+    This function print the result in csv format
+    """
+    timestamp = datetime.now()
+    for file in get_files():
+        for result in file.values():
+            for release_name, test in result.iteritems():
+                for testname, status in test.iteritems():
+                    print(('{},{},{},{},{}').format(
+                        0,
+                        release_name,
+                        testname.split('(')[-1].replace("']", ""),
+                        str(timestamp),
+                        status))
+
+
+if __name__ == "__main__":
+    print_as_csv()
