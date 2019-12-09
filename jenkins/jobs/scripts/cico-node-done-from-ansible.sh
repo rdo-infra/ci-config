26523d9ce04776bf82c52acd5d2d0e1e8e4e1571
Adam Kimball <baha@astyr.net>
2019-12-09 21:21:27 +0000

    ppc64le: Revert Container Build Job from Holding Nodes
    This patch undoes the patch that deliberately sets cico nodes to
    failed states. When merged, the job will run as normal.
    
    Change-Id: I0772f456b8c2c933cab7ab1cae158b2b12aac8a8

--- jenkins/jobs/scripts/cico-node-done-from-ansible.sh
+++ jenkins/jobs/scripts/cico-node-done-from-ansible.sh
@@ -3,6 +3,5 @@
 SSID_FILE=${SSID_FILE:-$WORKSPACE/cico-ssid}
 
 for ssid in $(cat ${SSID_FILE}); do
-    #cico -q node done $ssid
-    curl -v http://admin.ci.centos.org:8080/Node/fail?key=$CICO_API_KEY&ssid=$ssid
+    cico -q node done $ssid
 done
