1bde550835531a6c9c67f57ac029d28d1b458788
Adam Kimball <baha@astyr.net>
2019-12-09 19:50:08 +0000

    ppc64le: Adjust Containers Job to Hold Cico Node
    This patch adjusts the ppc64le container build job to set the cico
    node held by it to a failed state, which prompts it to linger
    after a build for 12 hours. It also adjusts the timing to happen
    twice a day, during the work hours of myself and Mike Turek, so
    that the job won't consume all cico nodes.
    
    Change-Id: I7e51b673b15d7da9fcec750a64b381b56f854bcf

--- jenkins/jobs/scripts/cico-node-done-from-ansible.sh
+++ jenkins/jobs/scripts/cico-node-done-from-ansible.sh
@@ -3,5 +3,6 @@
 SSID_FILE=${SSID_FILE:-$WORKSPACE/cico-ssid}
 
 for ssid in $(cat ${SSID_FILE}); do
-    cico -q node done $ssid
+    #cico -q node done $ssid
+    curl -v http://admin.ci.centos.org:8080/Node/fail?key=$CICO_API_KEY&ssid=$ssid
 done
