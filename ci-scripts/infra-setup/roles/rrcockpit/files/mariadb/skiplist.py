6c7ff3f8d73481bbb512cca6706f17c4d2e385b2
SurajP <surpatil@redhat.com>
2019-12-12 18:25:46 +0530

    Correctly populate skiplist panel for all releases
    Currently wrong data is populated for all releases
    in skiplist panel, refer lp#1857334. This was due
    to same dictionary('result') reference was added to
    'parse_result' list, this is fixed by creating
    new dictionary using deepcopy method and adding that
    to the list instead of referencing same dictionary.
    
    In addition to fixing the above issue this patch
    also does following:-
    - Remove unnecessary timestamp field from table load_pass_mysql.sql
    - Remove unused imports
    - Fix some indentation
    
    Closes-Bug: lp#1857334
    Change-Id: Ifa556d26ebcd8fd49a24e306951ce3b795f9c1fd
    Signed-off-by: SurajP <surpatil@redhat.com>

--- ci-scripts/infra-setup/roles/rrcockpit/files/mariadb/skiplist.py
+++ ci-scripts/infra-setup/roles/rrcockpit/files/mariadb/skiplist.py
@@ -18,16 +18,12 @@ import json
 import tempest_file_downloader
 import tempest_html_json
 
-from datetime import datetime
-
-
-parse_result = []
-
 
 def get_files():
     """
     This function gives the result of all the files parsed
     """
+    parse_result = []
     release_names = ['master', 'train', 'stein', 'rocky', 'queens']
     for release_name in release_names:
         if release_name in ['master', 'train']:
@@ -36,8 +32,8 @@ def get_files():
             file_name = "tempest.html.gz"
         try:
             log_url, release = tempest_file_downloader.get_last_build(
-                    "periodic-tripleo-ci-centos-7-ovb-1ctlr_2comp-"
-                    "featureset021-" + release_name, file_name)
+                "periodic-tripleo-ci-centos-7-ovb-1ctlr_2comp-"
+                "featureset021-" + release_name, file_name)
             global log
             log = log_url
         except Exception:
@@ -57,16 +53,14 @@ def print_as_csv():
     """
     This function print the result in csv format
     """
-    timestamp = datetime.now()
     for file in get_files():
         for result in file.values():
-            for release_name, test in result.iteritems():
-                for testname, status in test.iteritems():
-                    print(('{},{},{},{},{}').format(
+            for release_name, test in result.items():
+                for testname, status in test.items():
+                    print(('{},{},{},{}').format(
                         0,
                         release_name,
                         testname.split('(')[-1].replace("']", ""),
-                        str(timestamp),
                         status))
 
 
