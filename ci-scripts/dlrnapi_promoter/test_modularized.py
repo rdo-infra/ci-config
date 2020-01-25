5987f12dda25473681d9b80bdff12e7188da8de2
Gabriele Cerami <gcerami@redhat.com>
2020-01-25 17:01:25 +0000

    Modularize Promoter: replace legacy promoter function
    Legacy promoter function goal was mainly gathering variable from
    configuration file. This is now implemented by the PromoterConfig class
    This change expands the class to extract all information previously
    extracted by the promoter function, and feed the promote_all_link
    properly.
    New test cases were added and old improved for the Config class.
    
    Change-Id: I872b01944cd2691e8d09b08fe785bc1e7cbd373f

--- ci-scripts/dlrnapi_promoter/test_modularized.py
+++ ci-scripts/dlrnapi_promoter/test_modularized.py
@@ -4,10 +4,44 @@ import tempfile
 import unittest
 
 from config import PromoterConfig, ConfigError
+from six import string_types
 
 test_ini_configurations = dict(
     not_ini='''
     I am not a ini file
+    ''',
+    missing_parameters='''
+    [main]
+    distro_version: 7
+    release: master
+    api_url: https://trunk.rdoproject.org/api-centos-master-uc
+    username: ciuser
+    dry_run: no
+    log_file: ~/promoter_logs/centos7_master.log
+    latest_hashes_count: 10
+    manifest_push: true
+
+    [promote_from]
+    current-tripleo: tripleo-ci-testing
+
+    [current-tripleo]
+    periodic-tripleo-centos-7-master-containers-build-push
+    ''',
+    missing_section='''
+    [main]
+    # missing mandatory parameters and sections
+    distro_name: centos
+    distro_version: 7
+    release: master
+    api_url: https://trunk.rdoproject.org/api-centos-master-uc
+    username: ciuser
+    dry_run: no
+    log_file: ~/promoter_logs/centos7_master.log
+    latest_hashes_count: 10
+    manifest_push: true
+
+    [promote_from]
+    current-tripleo: tripleo-ci-testing
     ''',
     correct='''
     [main]
@@ -29,6 +63,11 @@ test_ini_configurations = dict(
     ''',
 )
 
+promotion_criteria_map = {
+    "current-tripleo": ["periodic-tripleo-centos-7-master-containers-build"
+                        "-push"]
+}
+
 
 class TestConfig(unittest.TestCase):
 
@@ -48,8 +87,34 @@ class TestConfig(unittest.TestCase):
         with self.assertRaises(ConfigError):
             PromoterConfig(self.filepaths['not_ini'])
 
+    def test_load_defective_ini_file(self):
+        with self.assertRaises(ConfigError):
+            PromoterConfig(self.filepaths['missing_parameters'])
+
+    def test_load_ini_file_no_criteria(self):
+        with self.assertRaises(ConfigError):
+            PromoterConfig(self.filepaths['missing_section'])
+
+    def test_load_ini_file_no_pass(self):
+        try:
+            del(os.environ["DLRNAPI_PASSWORD"])
+        except KeyError:
+            pass
+        with self.assertRaises(ConfigError):
+            PromoterConfig(self.filepaths['correct'])
+
     def test_load_ini_file(self):
         # Test for load correctness
+        os.environ["DLRNAPI_PASSWORD"] = "test"
         config = PromoterConfig(self.filepaths['correct'])
+        # Test if config keys are there and have a value
+        assert hasattr(config, "release"), "Missing mandatory argument"
+        assert hasattr(config, "distro_name"), "Missing mandatory argument"
+        self.assertIsInstance(config.distro_name, string_types)
+        self.assertEqual(config.release, "master")
+        self.assertEqual(config.target_registries_push, True)
         # Test if legacy config has been correctly created
         self.assertIsInstance(config.legacy_config, configparser.ConfigParser)
+        self.assertDictEqual(promotion_criteria_map,
+                             config.promotion_criteria_map)
+        self.assertEqual(config.latest_hashes_count, 10)
