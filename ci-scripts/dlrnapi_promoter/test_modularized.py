1132e14a16198ab1e5eeab75ffae4575b6b5032b
Gabriele Cerami <gcerami@redhat.com>
2020-01-25 17:01:25 +0000

    Modularize Promoter: replace legacy promoter function
    Legacy promoter function goal was mainly gathering variable from
    configuration file. This is now implemented by the PromoterConfig class
    This change expands the class to extract all information previously
    extracted by the promoter function, and feed the promote_all_link
    properly.
    New test cases were added and old improved for the Config class.
    
    Change-Id: I872b01944cd2691e8d09b08fe785bc1e7cbd373f

--- ci-scripts/dlrnapi_promoter/test_modularized.py
+++ ci-scripts/dlrnapi_promoter/test_modularized.py
@@ -3,11 +3,49 @@ import os
 import tempfile
 import unittest
 
+try:
+    import mock
+except ImportError:
+    from unittest import mock
+
 from config import PromoterConfig, ConfigError
 
 test_ini_configurations = dict(
     not_ini='''
     I am not a ini file
+    ''',
+    missing_parameters='''
+    [main]
+    distro_version: 7
+    release: master
+    api_url: https://trunk.rdoproject.org/api-centos-master-uc
+    username: ciuser
+    dry_run: no
+    log_file: ~/promoter_logs/centos7_master.log
+    latest_hashes_count: 10
+    manifest_push: true
+
+    [promote_from]
+    current-tripleo: tripleo-ci-testing
+
+    [current-tripleo]
+    periodic-tripleo-centos-7-master-containers-build-push
+    ''',
+    missing_section='''
+    [main]
+    # missing mandatory parameters and sections
+    distro_name: centos
+    distro_version: 7
+    release: master
+    api_url: https://trunk.rdoproject.org/api-centos-master-uc
+    username: ciuser
+    dry_run: no
+    log_file: ~/promoter_logs/centos7_master.log
+    latest_hashes_count: 10
+    manifest_push: true
+
+    [promote_from]
+    current-tripleo: tripleo-ci-testing
     ''',
     correct='''
     [main]
@@ -29,6 +67,11 @@ test_ini_configurations = dict(
     ''',
 )
 
+promotion_criteria_map = {
+    "current-tripleo": ["periodic-tripleo-centos-7-master-containers-build"
+                        "-push"]
+}
+
 
 class TestConfig(unittest.TestCase):
 
@@ -44,12 +87,40 @@ class TestConfig(unittest.TestCase):
         for filepath in self.filepaths.values():
             os.unlink(filepath)
 
-    def test_load_notini_config(self):
+    @mock.patch('config.fetch_current_named_hashes')
+    def test_load_notini_config(self, fetch_current_named_hashes_mock):
         with self.assertRaises(ConfigError):
             PromoterConfig(self.filepaths['not_ini'])
 
-    def test_load_ini_file(self):
+    @mock.patch('config.fetch_current_named_hashes')
+    def test_load_defective_ini_file(self, fetch_current_named_hashes_mock):
+        with self.assertRaises(ConfigError):
+            PromoterConfig(self.filepaths['missing_parameters'])
+
+    @mock.patch('config.fetch_current_named_hashes')
+    def test_load_ini_file_no_criteria(self, fetch_current_named_hashes_mock):
+        with self.assertRaises(ConfigError):
+            PromoterConfig(self.filepaths['missing_section'])
+
+    @mock.patch('config.fetch_current_named_hashes')
+    def test_load_ini_file_no_pass(self, fetch_current_named_hashes_mock):
+        try:
+            del(os.environ["DLRNAPI_PASSWORD"])
+        except KeyError:
+            pass
+        with self.assertRaises(ConfigError):
+            PromoterConfig(self.filepaths['correct'])
+
+    @mock.patch('config.fetch_current_named_hashes')
+    def test_load_ini_file(self, fetch_current_named_hashes_mock):
         # Test for load correctness
+        os.environ["DLRNAPI_PASSWORD"] = "test"
         config = PromoterConfig(self.filepaths['correct'])
+        # Test if config keys are there and have a value
+        assert hasattr(config, "release"), "Missing mandatory argument"
+        self.assertEqual(config.release, "master")
+        self.assertEqual(config.target_registries_push, True)
         # Test if legacy config has been correctly created
         self.assertIsInstance(config.legacy_config, configparser.ConfigParser)
+        self.assertDictEqual(promotion_criteria_map,
+                             config.promotion_criteria_map)
