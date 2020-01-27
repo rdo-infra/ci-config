1fd2ece8736a01b82cd36cb88ab7bb53b9e025aa
Gabriele Cerami <gcerami@redhat.com>
2020-01-27 23:12:04 +0000

    Modularize Promoter: replace get_latest_hashes
    This change moves the single most important and complicated function of
    the entire promoter to the new reorganization
    
    Change-Id: I976191f6be717bfed084cbe9a9fa7a878eb62923

--- ci-scripts/dlrnapi_promoter/dlrn_interface.py
+++ ci-scripts/dlrnapi_promoter/dlrn_interface.py
@@ -32,38 +32,44 @@ class DlrnHash(dict):
 
     log = logging.getLogger("promoter")
 
-    def __init__(self, commit=None, distro=None, from_api=None, from_dict=None):
+    def __init__(self, commit=None, distro=None, timestamp=None, from_api=None,
+                 from_dict=None):
         """
         Dlrn Hash can be initialized either by direct kwargs value, from a
         dictionary, or from a dlrn api response object
         :param commit: the direct commit hash
         :param distro:  the direct distro hash
+        :param timstamp: the direct timestamp value, must be float
         :param from_api: A valid dlrn api response object
         :param from_dict:  A dictionary that needs to contain commit_hash and
         distro_hash as keys
         """
         self.commit_hash = ""
         self.distro_hash = ""
+        self.timestamp = timestamp
         if from_api is not None:
             try:
-                self.log.debug("Using values from a Promotion object")
                 self.commit_hash = from_api.commit_hash
                 self.distro_hash = from_api.distro_hash
+                if hasattr(from_api.timestamp):
+                    self.timestamp = from_api.timestamp
             except AttributeError:
-                raise AttributeError("Cannot create hash,"
+                raise AttributeError("Error while building DlrnHash:"
                                      " invalid source API object")
         elif from_dict is not None:
             try:
-                self.log.debug("Using values from a Promotion object")
                 self.commit_hash = from_dict['commit_hash']
                 self.distro_hash = from_dict['distro_hash']
+                if "timestamp" in from_dict:
+                    self.timestamp = from_dict['timestamp']
             except KeyError:
-                raise KeyError("Cannot create hash:"
+                raise KeyError("Error while building DlrnHash:"
                                " invalid source dict")
-
-        else:
+        elif commit is not None and distro is not None:
             self.commit_hash = commit
             self.distro_hash = distro
+        else:
+            self.log.debug("Creating empty DlrnHash")
 
         # TODO(gcerami) strict dlrn validation
         # check that the hashes are valid hashes with correct size
@@ -90,6 +96,10 @@ class DlrnHash(dict):
     def __repr__(self):
         return "<DlrnHash object commit: %s, distro: %s>" % (self.commit_hash,
                                                              self.distro_hash)
+
+    @property
+    def id(self):
+        return self.full_hash
 
     @property
     def full_hash(self):
