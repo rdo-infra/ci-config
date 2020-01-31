6b4b669249626ad001d8d2eb62f9729c96de791e
Gabriele Cerami <gcerami@redhat.com>
2020-01-31 16:22:27 +0000

    [WIP] Promoter overhaul for integration pipeline
    Promoter is currently working with a single definition of dlrn hash.
    With the coming of the component pipeline, the basic assumptions for all
    dlrn hashes are not true anymore.
    The component pipeline introduced the concept of aggregate hash, which
    does adds an attribute to the existing hash, which is computed from a
    repo file through md5sum.
    Known pipelines will still promote the dlrn hashes
    Component pipeline will need to promote aggregated hashes
    
    The workflow needs to take this difference into consideration to promote
    for the component pipeline.
    The difference in dlrnapi is rendered somewhat transparent as the api
    calls for this type of hash will be the same, but overloaded with
    different attributes.
    To render the difference completely transparent to the whole workflow
    and not repeat e.g. a check "Is this single or component pipeline?"
    everywhere in the workflow, and having to test multiple execution paths
    for every single answer to this question, this patch moves all the
    complexity to the DlrnHash class and its tests.
    The workflow should never need to address the type of pipeline directly,
    and work only with the DlrnHash object in all cases.
    Staging environment has been redesigned to accomodate the testing in
    multiple scenarios.
    
    This patch is quite the jump, this is a summary of the changes included
    in this patch.
    PROMOTER:
    
    STAGING ENVIRONMENT:
    Stage environment setup now is able to spawn and kill dlrn server from
    the stage config file, and we can pass alternative fixture files.
    
    TESTS:
    
    Change-Id: I221f4b33cf2763875fc6394902f7923108a34d2a

---
+++ ci-scripts/dlrnapi_promoter/test_stage_qcows_unit.py
@@ -0,0 +1,41 @@
+import pytest
+import unittest
+
+
+class TestQcowStagingServer(unittest.TestCase):
+
+    @pytest.mark.xfail(reason="Not implemented")
+    def test_create_hierarchy_dir_exist(self):
+        assert False
+
+    @pytest.mark.xfail(reason="Not implemented")
+    def test_create_hierarchy_success(self):
+        assert False
+
+    @pytest.mark.xfail(reason="Not implemented")
+    def test_teardown_success(self):
+        assert False
+
+    @pytest.mark.xfail(reason="Not implemented")
+    def test_teardown_no_dir(self):
+        assert False
+
+    @pytest.mark.xfail(reason="Not implemented")
+    def test_stage_info(self):
+        assert False
+
+    @pytest.mark.xfail(reason="Not implemented")
+    def test_promote_overcloud_images_success(self):
+        assert False
+
+    @pytest.mark.xfail(reason="Not implemented")
+    def test_promote_overcloud_images_link_exist(self):
+        assert False
+
+    @pytest.mark.xfail(reason="Not implemented")
+    def test_setup_dir_exist(self):
+        assert False
+
+    @pytest.mark.xfail(reason="Not implemented")
+    def test_setup_success(self):
+        assert False
