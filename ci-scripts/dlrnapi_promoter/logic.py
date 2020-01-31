7087f5cba3368297f2189e900195bf21c23ac16b
Gabriele Cerami <gcerami@redhat.com>
2020-01-31 16:22:27 +0000

    [WIP] Implement promotion for integration pipeline
    Promoter is currently working with a single definition of dlrn hash.
    With the coming of the component pipeline, the basic assumptions for all
    dlrn hashes are not true anymore.
    The component pipeline introduced the concept of aggregate hash, which
    does adds an attribute to the existing hash, which is computed from a
    repo file through md5sum.
    Known pipelines will still promote the dlrn hahses
    Component pipeline will need to promote aggregated hashes
    
    The workflow needs to take this difference into consideration to promote
    for the component pipeline.
    The difference in dlrnapi is rendered somewhat transparent as the api
    calls for this type of hash will be the same, but overloaded with
    different attributes.
    To render the difference completely transparent to the whole workflow
    and not repeat e.g. a check "Is this single or component pipeline?"
    everywhere in the workflow, and having to test multiple execution paths
    for every single answer to this question, this patch moves all the
    complexity to the DlrnHash class and its tests.
    The workflow should never need to address the type of pipeline directly,
    and work only with the DlrnHash object in all cases.
    Stage environment setup now is able to spawn and kill dlrn server from
    the stage config file, and we can pass alternative fixture files.
    
    Change-Id: I221f4b33cf2763875fc6394902f7923108a34d2a

--- ci-scripts/dlrnapi_promoter/logic.py
+++ ci-scripts/dlrnapi_promoter/logic.py
@@ -3,17 +3,16 @@ This file contains classes and function for high level logic of the promoter
 workflow
 """
 import logging
+import itertools
 
-from dlrn_interface import DlrnClient
-from registry import RegistryClient
-from qcow import QcowClient
+from dlrn_client import DlrnClient
+from registries_client import RegistriesClient
+from qcow_client import QcowClient
+from common import PromotionError
+from config import PromoterConfig
 
 
-class PromotionError(Exception):
-    pass
-
-
-class PromoterLogic(object):
+class Promoter(object):
     """
     This class embeds the promoter logic and its methods implement all the
     high level decisions around promotion
@@ -21,10 +20,15 @@ class PromoterLogic(object):
 
     log = logging.getLogger('promoter')
 
-    def __init__(self, config):
-        self.config = config
+    def __init__(self, config_file=None, overrides=None):
+        self.config = PromoterConfig(config_file=config_file,
+                                     overrides=overrides)
+        # This message is also used by some test to understand if the
+        # promoter is running the new or the legacy code by looking at the logs
+        self.log.warning("This workflow is using the new modularized "
+                         "code")
         self.dlrn_client = DlrnClient(self.config)
-        self.registry_client = RegistryClient(self.config)
+        self.registries_client = RegistriesClient(self.config)
         self.qcow_client = QcowClient(self.config)
 
     def select_candidates(self, candidate_label, target_label):
@@ -39,86 +43,109 @@ class PromoterLogic(object):
         :param target_label:  The label to which the candidate would be promoted
         :return: A list of candidate hashes
         """
-        candidate_hashes_list = self.dlrn_client.fetch_hashes(
-            candidate_label, count=self.config.latest_hashes_count,
-            sort="timestamp", reverse=True)
+        candidate_hashes_list = self.dlrn_client.fetch_promotions(
+            candidate_label, count=self.config.latest_hashes_count)
 
-        if candidate_hashes_list is None:
-            self.log.error(
-                'Failed to fetch any hashes for %s, skipping promotion',
-                candidate_label)
-            return []
+        if not candidate_hashes_list:
+            self.log.warning("Candidate label '{}': No hashes fetched"
+                             "".format(candidate_label))
+            return candidate_hashes_list
+        else:
+            self.log.info("Candidate label '{}': Fetched {} hashes"
+                          "".format(candidate_label,
+                                    len(candidate_hashes_list)))
 
         # This will be a map of recent hashes candidate for
-        # promotion. We'll map
-        # here the timestamp for each promotion to promote name, if any
+        # promotion. We'll map here the timestamp for each promotion to
+        # promote name, if any
         candidate_hashes = {}
-        for hash in candidate_hashes_list:
-            candidate_hashes[hash.id] = {}
-            candidate_hashes[hash.id][candidate_label] = hash.timestamp
+        for dlrn_hash in candidate_hashes_list:
+            candidate_hashes[dlrn_hash.full_hash] = {}
+            candidate_hashes[dlrn_hash.full_hash][candidate_label] = \
+                dlrn_hash.timestamp
 
-        old_hashes = self.dlrn_client.fetch_hashes(target_label)
-        if old_hashes is None:
-            self.log.warning('Failed to fetch hashes for %s, no previous '
-                             'promotion or typo in the link name',
-                             target_label)
+        old_hashes = self.dlrn_client.fetch_promotions(target_label)
+        if not old_hashes:
+            self.log.warning("Target label '{}': No hashes fetched."
+                             " This could mean that the target label is new"
+                             " or it's the wrong label"
+                             "".format(target_label))
         else:
-            for hash in old_hashes:
+            self.log.info("Target label '{}': Fetched {} hashes"
+                          "".format(target_label, len(old_hashes)))
+            for dlrn_hash in old_hashes:
                 # it may happen that an hash appears in this list,
-                # but it's not from
-                # our list of candindates. If this happens we're just
-                # ignoring it
-                if hash.id in candidate_hashes:
-                    candidate_hashes[hash.id][target_label] = \
-                        hash.timestamp
+                # but it's not from our list of candindates. If this happens
+                # we're just ignoring it
+                if dlrn_hash.full_hash in candidate_hashes:
+                    candidate_hashes[dlrn_hash.full_hash][target_label] = \
+                        dlrn_hash.timestamp
 
         # returning only the hashes younger than the latest promoted
         # this list is already in reverse time order
-        for index, hash in enumerate(candidate_hashes_list):
-            if target_label in candidate_hashes[hash.id]:
-                self.log.info(
-                    'Current "%s" hash is %s' % (target_label, hash))
+        for index, dlrn_hash in enumerate(candidate_hashes_list):
+            if target_label in candidate_hashes[dlrn_hash.full_hash]:
+                self.log.info("Target label '{}': current hash is {}"
+                              "".format(target_label, dlrn_hash))
                 candidate_hashes_list = candidate_hashes_list[:index]
                 break
 
         if candidate_hashes_list:
-            self.log.debug(
-                'Remaining hashes after removing ones older than the '
-                'currently promoted: %s', candidate_hashes_list)
+            self.log.info("Candidate hashes younger than target label current"
+                          ": {}".format(candidate_hashes_list))
         else:
-            self.log.debug(
-                'No remaining hashes after removing ones older than the '
-                'currently promoted')
+            self.log.info("Candidate hashes: none found younger than target "
+                          "label current")
 
         return candidate_hashes_list
 
-    def promote(self, candidate, target_label):
+    def promote(self, candidate_hash, candidate_label, target_label,
+                allowed_clients=None):
         """
         This method drives the effective promotion of all the single component
-        :param candidate: The candidate element to be promoted
+        :param candidate_hash: The candidate element to be promoted
+        :param candidate_label: the label to which the element was previously
+        promoted to
         :param target_label: The label to which promote the candidate
+        :param allowed_clients: A list of client we are going to use for the
+        promotion. The default is calling all the client, but when testing,
+        we can decide to launch a single client or group of clients
         :return: None
         """
-        # replaces promote_all_links - noop promotion
+        promoted_pair = ()
+
         if self.config.dry_run:
             return
 
-        # replaces promote_all_links -effective promotion
-        # replaces promote_all_links - containers promotion
-        self.dlrn_client.check_named_hashes_unchanged()
-        if self.config.allow_containers_promotion:
-            self.registry_client.promote_containers(candidate,
-                                                    target_label)
-        # replaces promote_all_links - qcow promotion
-        self.dlrn_client.check_named_hashes_unchanged()
-        if self.config.allow_qcows_promotion:
-            self.qcow_client.promote_images(candidate, target_label)
-        # replaces promote_all_links - dlrn promotion
-        self.dlrn_client.check_named_hashes_unchanged()
-        if self.config.allow_dlrn_promotion:
-            self.dlrn_client.promote_hash(candidate, target_label)
+        if allowed_clients is None:
+            allowed_clients = self.config.allowed_clients
 
-        self.dlrn_client.update_current_named_hashes(candidate, target_label)
+        # DLRN client should always be called last,
+        # This ensures the order of the called clients, it uses alphabetical
+        # sorting, which is quite weak but works. If it becomes inconvenient,
+        # we can just remove the loop here and act on clients singularly
+        allowed_clients.sort(reverse=True)
+
+        self.log.debug("Candidate hash '{}': clients allowed to promote: {}"
+                       "".format(candidate_hash, allowed_clients))
+
+        # FIXME: In python2 itertools.repeat needs a length parameter or it
+        #  will just repeat self ad libitum. Python3 does not need it.
+        for client in list(map(getattr,
+                               itertools.repeat(self, len(allowed_clients)),
+                               allowed_clients)):
+            self.dlrn_client.check_named_hashes_unchanged()
+            self.log.info("Candidate hash '{}': client {} attempting promotion"
+                          "".format(candidate_hash, client))
+            try:
+                client.promote(candidate_hash, target_label,
+                               candidate_label=candidate_label)
+                promoted_pair = (candidate_hash, target_label)
+            except PromotionError as e:
+                self.log.error("Candidate hash '{}': Failed Promotion attempt")
+                self.log.error(e)
+
+        return promoted_pair
 
     def promote_label_to_label(self, candidate_label, target_label):
         """
@@ -131,70 +158,95 @@ class PromoterLogic(object):
         promoted
         :return: None
         """
-        # replaces promote_all_links - candidate hashes selection
-        for selected_candidate in self.select_candidates(candidate_label,
-                                                         target_label):
-            self.log.info('Checking hash %s from %s for promotion criteria',
-                          selected_candidate, candidate_label)
-            successful_jobs = set(self.dlrn_client.fetch_jobs(
-                selected_candidate))
+        promoted_pair = ()
+        selected_candidates = self.select_candidates(candidate_label,
+                                                     target_label)
+        if not selected_candidates:
+            self.log.warning("Candidate label '{}': No candidate hashes"
+                             "".format(candidate_label))
+            return promoted_pair
+        else:
+            self.log.info("Candidate label '{}': {} candidates"
+                          "".format(candidate_label, len(selected_candidates)))
+
+        self.log.info("Candidate label '{}': Checking candidates that meet "
+                      "promotion criteria for target label '{}'"
+                      "".format(candidate_label, target_label))
+        for candidate_hash in selected_candidates:
             required_jobs = self.config.promotion_criteria_map[target_label]
-            # The label reject condition is moved as config time check
-            # replaces promote_all_links - hashes reject condition
-            missing_jobs = list(required_jobs - successful_jobs)
-            if not missing_jobs:
-                try:
-                    self.promote(selected_candidate, target_label)
-                    self.log.info('SUCCESS promoting %s-%s %s as %s (%s)',
-                                  self.config.distro, self.config.release,
-                                  candidate_label, target_label,
-                                  selected_candidate)
-                    self.log.info(
-                        '%s \n%s/api/civotes_detail.html?\
-                        commit_hash=%s&distro_hash=%s'.replace(" ", ""),
-                        'DETAILED SUCCESSFUL STATUS: ',
-                        self.config.api_url,
-                        selected_candidate.commit_hash,
-                        selected_candidate.distro_hash)
-                    # stop here, don't try to promote other hashes
-                    break
-                except Exception:
-                    # replaces promot_all_links - effective promotion failure
-                    self.log.info('FAILED promoting %s-%s %s as %s (%s)',
-                                  self.config.distro, self.config.release,
-                                  candidate_label, target_label,
-                                  selected_candidate)
-                    civotes_info = self.dlrn_client.get_civotes_info(
-                        selected_candidate)
-                    self.log.info(civotes_info)
-                    raise
+            self.log.debug("Candidate hash '{}': required jobs {}"
+                           "".format(candidate_hash, required_jobs))
+            successful_jobs = \
+                set(self.dlrn_client.fetch_jobs(candidate_hash))
+            if successful_jobs:
+                self.log.info("Candidate hash '{}': successful jobs {}"
+                              "".format(candidate_hash, successful_jobs))
             else:
-                self.log.info(
-                    'Skipping promotion of %s-%s from %s to %s, missing '
-                    'successful jobs: %s',
-                    self.config.distro, self.config.release,
-                    candidate_label, target_label, missing_jobs)
-                self.log.info('Check Results at:')
-                self.log.info(
-                    '%s \n%s/api/civotes_detail.html?'
-                    'commit_hash=%s&distro_hash=%s'.replace(" ", ""),
-                    'DETAILED MISSING JOBS: ',
-                    self.config.api_url,
-                    selected_candidate.commit_hash,
-                    selected_candidate.distro_hash)
+                self.log.warning("Candidate hash '{}': NO successful jobs"
+                                 "".format(candidate_hash))
 
-        self.log.info("No more candidates")
+            missing_jobs = list(required_jobs - successful_jobs)
+            if missing_jobs:
+                self.log.warning("Candidate hash '{}': missing jobs {}"
+                                 "".format(candidate_hash, missing_jobs))
+                self.log.warning("Candidate hash '{}': criteria NOT met "
+                                 "for promotion to {}"
+                                 "".format(candidate_hash, target_label))
+                self.log.warning("Candidate hash '{}': {}"
+                                 "".format(candidate_hash,
+                                           self.dlrn_client.get_civotes_info()))
+                continue
 
-    def promote_all_links(self):
+            self.log.info("Candidate hash '{}': criteria met, attempting "
+                          "promotion to {}"
+                          "".format(candidate_hash, target_label))
+            promoted_pair = self.promote(candidate_hash,
+                                         candidate_label,
+                                         target_label)
+            if promoted_pair:
+                self.log.info("Candidate hash '{}': SUCCESSFUL promotion to {}"
+                              "".format(candidate_hash, target_label))
+                self.log.info("Candidate hash '{}': {}"
+                              "".format(candidate_hash,
+                                        self.dlrn_client.get_civotes_info(
+                                            candidate_hash)))
+                # stop here, don't try to promote other hashes
+                break
+            else:
+                self.log.warning("Candidate hash '{}': FAILED promotion "
+                                 "attempt to {}"
+                                 "".format(candidate_hash, target_label))
+                self.log.warning("Candidate hash '{}': {}"
+                                 "".format(candidate_hash,
+                                           self.dlrn_client.get_civotes_info(
+                                               candidate_hash)))
+
+        return promoted_pair
+
+    def promote_all(self):
         """
         This method loops over all the labels specified in the config file
         and attempt to find and promote suitable candidates
-        This method replaces part of the legacy promote_all_links function
-        :return: None
+        :return: a list of promoted (candidate, target) tuples
         """
-        # replaces promote_all_links - labels loop
         self.dlrn_client.fetch_current_named_hashes(store=True)
 
+        promoted_pairs = []
+        self.log.info("Starting promotion attempts for all labels")
         for target_label, candidate_label in \
                 self.config.promotion_steps_map.items():
-            self.promote_label_to_label(candidate_label, target_label)
+            self.log.info("Candidate label '{}': Attempting promotion to '{}'"
+                          "".format(candidate_label, target_label))
+            promoted_pair = self.promote_label_to_label(candidate_label,
+                                                        target_label)
+            if promoted_pair:
+                promoted_pairs.append(promoted_pair)
+            else:
+                self.log.warning("Candidate label '{}': NO candidate "
+                                 "hash promoted to {}"
+                                 "".format(candidate_label, target_label))
+
+        self.log.info("Summary: Promoted {} hashes this round"
+                      "".format(len(promoted_pairs)))
+        self.log.info("------- -------- Promoter terminated normally")
+        return promoted_pairs
