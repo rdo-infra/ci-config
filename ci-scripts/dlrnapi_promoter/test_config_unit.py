d01f09f670249423dd5c58d2b7d2a107cf643fc6
Gabriele Cerami <gcerami@redhat.com>
2020-01-31 16:22:27 +0000

    [WIP] Promoter overhaul for integration pipeline
    Promoter is currently working with a single definition of dlrn hash.
    With the coming of the component pipeline, the basic assumptions for all
    dlrn hashes are not true anymore.
    The component pipeline introduced the concept of aggregate hash, which
    does adds an attribute to the existing hash, which is computed from a
    repo file through md5sum.
    Known pipelines will still promote the dlrn hashes
    Component pipeline will need to promote aggregated hashes
    
    The workflow needs to take this difference into consideration to promote
    for the component pipeline.
    The difference in dlrnapi is rendered somewhat transparent as the api
    calls for this type of hash will be the same, but overloaded with
    different attributes.
    To render the difference completely transparent to the whole workflow
    and not repeat e.g. a check "Is this single or component pipeline?"
    everywhere in the workflow, and having to test multiple execution paths
    for every single answer to this question, this patch moves all the
    complexity to the DlrnHash class and its tests.
    The workflow should never need to address the type of pipeline directly,
    and work only with the DlrnHash object in all cases.
    Staging environment has been redesigned to accomodate the testing in
    multiple scenarios.
    
    This patch is quite the jump, this is a summary of the changes included
    in this patch.
    PROMOTER:
    
    STAGING ENVIRONMENT:
    Stage environment setup now is able to spawn and kill dlrn server from
    the stage config file, and we can pass alternative fixture files.
    
    TESTS:
    
    Change-Id: I221f4b33cf2763875fc6394902f7923108a34d2a

---
+++ ci-scripts/dlrnapi_promoter/test_config_unit.py
@@ -0,0 +1,208 @@
+import os
+import tempfile
+import unittest
+try:
+    # Python3 imports
+    from unittest.mock import Mock, patch
+    import unittest.mock as mock
+except ImportError:
+    # Python2 imports
+    from mock import Mock, patch
+    import mock
+
+import pytest
+from six import string_types
+
+from config import ConfigError, PromoterConfigBase, PromoterConfig
+from test_unit_fixtures import test_ini_configurations
+
+
+class ConfigBase(unittest.TestCase):
+
+    def setUp(self):
+        self.filepaths = {}
+        for case, content in test_ini_configurations.items():
+            fp, filepath = tempfile.mkstemp(prefix="ini_conf_test")
+            with os.fdopen(fp, "w") as test_file:
+                test_file.write(content)
+            self.filepaths[case] = filepath
+
+    def tearDown(self):
+        for filepath in self.filepaths.values():
+            os.unlink(filepath)
+
+
+# Config in general is difficult to test singularly, as the init already calls
+# all the  methods involved. Maybe we can add an option to defer calling other
+# methods. but for now the best way is to just verify the final artifact
+class TestConfigBase(ConfigBase):
+
+    def test_config_none(self):
+        with self.assertRaises(ConfigError):
+            PromoterConfigBase(None)
+
+    @patch('logging.Logger.error')
+    def test_config_not_found(self, mock_log_error):
+        with self.assertRaises(OSError):
+            PromoterConfigBase("/does/not/exist")
+        mock_log_error.assert_has_calls([
+            mock.call("Configuration file not found")
+        ])
+
+    def test_load_notini_config(self):
+        with self.assertRaises(ConfigError):
+            PromoterConfigBase(self.filepaths['not_ini'])
+
+    @patch('logging.Logger.error')
+    def test_load_ini_file_no_criteria(self, mock_log_error):
+        with self.assertRaises(ConfigError):
+            PromoterConfigBase(self.filepaths['missing_criteria_section'])
+        mock_log_error.assert_has_calls([
+            mock.call("Missing criteria section for target %s",
+                      'current-tripleo')
+        ])
+
+    @patch('logging.Logger.error')
+    def test_load_config_missing_main(self, mock_log_error):
+        with self.assertRaises(ConfigError):
+            PromoterConfigBase(self.filepaths['missing_main'])
+        mock_log_error.assert_has_calls([
+            mock.call("Config file: %s Missing main section",
+                      self.filepaths['missing_main'])
+        ])
+
+    @patch('logging.Logger.error')
+    def test_load_config_missing_promotions(self, mock_log_error):
+        with self.assertRaises(ConfigError):
+            PromoterConfigBase(self.filepaths['missing_promotions_section'])
+        mock_log_error.assert_has_calls([
+            mock.call("Missing promotion_from section")
+        ])
+
+    def test_load_correct_ini_file_verify_params(self):
+        self.maxDiff = None
+        # Test for load correctness
+        os.environ["DLRNAPI_PASSWORD"] = "test"
+        config = PromoterConfigBase(self.filepaths['correct'])
+        # Test if config keys are there and have a value
+        assert hasattr(config, "release"), "Missing mandatory argument"
+        assert hasattr(config, "distro_name"), "Missing mandatory argument"
+        self.assertIsInstance(config.distro_name, string_types)
+        self.assertEqual(config.release, "master")
+        # TODO(gcerami) we should also check that ~ in log_file are expanded
+        #  to user home, but it's not easy as setting log_file to something
+        #  different from /dev/null will need a valid file to write logs to,
+        #  and it cannot be in the user home
+        # This is tricky, here we verified that the code correctly loaded the
+        # value, and value from ini is text. In PromoterConfig we verify if
+        # this value has been correctly handled and converted to int
+        self.assertEqual(config.latest_hashes_count, '10')
+
+
+class TestConfig(ConfigBase):
+
+    def test_load_correct_verify_extended_params(self):
+        self.maxDiff = None
+        # Test for load correctness
+        os.environ["DLRNAPI_PASSWORD"] = "test"
+        config = PromoterConfig(self.filepaths['correct'])
+        self.assertEqual(config.target_registries_push, True)
+        promotion_criteria_map = {
+            "current-tripleo": {
+                "periodic-tripleo-centos-7-master-containers-build-push",
+                "periodic-tripleo-centos-7-master-standalone"
+            }
+        }
+        self.assertDictEqual(promotion_criteria_map,
+                             config.promotion_criteria_map)
+
+        # This is tricky, here we verified that the code correctly
+        # converted this value to int In PromoterConfigBase we verify if
+        # this value has just been correctly loaded as str from ini config
+        self.assertEqual(config.latest_hashes_count, 10)
+
+    # FIXME: python2 has no unittest.assertLogs
+    @patch('logging.Logger.warning')
+    @patch('logging.Logger.error')
+    def test_load_defective_ini_file(self, mock_error, mock_warning):
+        os.environ["DLRNAPI_PASSWORD"] = "test"
+        ini_config = self.filepaths['missing_parameters']
+        with self.assertRaises(ConfigError):
+            PromoterConfig(ini_config)
+        calls = [
+            mock.call('Missing parameter in configuration file: %s. Using '
+                      'default value: %s', 'distro_name', 'centos')
+        ]
+        mock_warning.assert_has_calls(calls)
+        calls = [
+            mock.call('Invalid Log file: %s', '/dev/nul'),
+            mock.call('Error in configuration file %s' % str(ini_config))
+        ]
+        mock_error.assert_has_calls(calls, any_order=True)
+
+    @patch('logging.Logger.error')
+    def test_load_ini_file_no_pass(self, mock_error):
+        try:
+            del(os.environ["DLRNAPI_PASSWORD"])
+        except KeyError:
+            pass
+        with self.assertRaises(ConfigError):
+            PromoterConfig(self.filepaths['correct'])
+        calls = [
+            mock.call('No dlrnapi password found in env'),
+        ]
+        mock_error.assert_has_calls(calls, any_order=True)
+
+    @patch('logging.Logger.error')
+    def test_load_empty_criteria(self, mock_error):
+        os.environ["DLRNAPI_PASSWORD"] = "test"
+        with self.assertRaises(ConfigError):
+            PromoterConfig(self.filepaths['criteria_empty'])
+        calls = [
+            mock.call('No jobs in criteria for target %s', 'current-tripleo'),
+            mock.call('Error in configuration file {}'
+                      ''.format(self.filepaths['criteria_empty']))
+        ]
+        mock_error.assert_has_calls(calls, any_order=True)
+
+    @pytest.mark.xfail(reason="Not implemented")
+    def test_get_dlrn_api_url_none(self):
+        assert False
+
+    @pytest.mark.xfail(reason="Not implemented")
+    def test_get_dlrn_api_url_local(self):
+        assert False
+
+    @pytest.mark.xfail(reason="Not implemented")
+    def test_get_dlrn_api_url_remote(self):
+        assert False
+
+    @pytest.mark.xfail(reason="Not implemented")
+    def test_experimental_config(self):
+        assert False
+
+    @pytest.mark.xfail(reason="Not implemented")
+    def test_sanity_check(self):
+        assert False
+
+    @pytest.mark.xfail(reason="Not implemented")
+    def test_get_expand_config_complete(self):
+        # All fields already present
+        assert False
+
+    @pytest.mark.xfail(reason="Not implemented")
+    def test_get_expand_config_incomplete(self):
+        # All optional fields missing
+        assert False
+
+    @pytest.mark.xfail(reason="Not implemented")
+    def test_handle_overrides_overrides_exists_but_empty(self):
+        assert False
+
+    @pytest.mark.xfail(reason="Not implemented")
+    def test_handle_overrides_overrides_nonexisting(self):
+        assert False
+
+    @pytest.mark.xfail(reason="Not implemented")
+    def test_handle_overrides_ok(self):
+        assert False
---
+++ ci-scripts/dlrnapi_promoter/test_registries_client_unit.py
@@ -0,0 +1,49 @@
+import pytest
+import subprocess
+try:
+    # Python3 imports
+    from unittest.mock import Mock, patch
+    import unittest.mock as mock
+except ImportError:
+    # Python2 imports
+    from mock import Mock, patch
+    import mock
+
+from common import PromotionError
+from dlrn_hash import DlrnHash
+from test_unit_fixtures import ConfigSetup, hashes_test_cases
+
+
+class TestRegistriesClient(ConfigSetup):
+
+    def setUp(self):
+        super(TestRegistriesClient, self).setUp()
+        self.client = self.promoter.registries_client
+
+    def test_setup(self):
+        error_msg = "Container push logfile is misplaces"
+        assert self.client.logfile != "", error_msg
+
+    @mock.patch('subprocess.check_output')
+    def test_promote_success(self, check_output_mock):
+        candidate_hash =\
+            DlrnHash(source=hashes_test_cases['aggregate']['dict']['valid'])
+        target_label = "test"
+
+        check_output_mock.return_value = "test log"
+        self.client.promote(candidate_hash, target_label)
+
+        assert subprocess.check_output.called
+
+    @pytest.mark.xfail(reason="Not Implemented")
+    @mock.patch('subprocess.check_output')
+    def test_promote_failure(self, check_output_mock):
+        candidate_hash = \
+            DlrnHash(source=hashes_test_cases['aggregate']['dict']['valid'])
+        target_label = "test"
+        exception = subprocess.CalledProcessError(1, 2)
+        exception.output = b"test"
+        check_output_mock.side_effect = exception
+        with self.assertRaises(PromotionError):
+            self.client.promote(candidate_hash, target_label)
+        # Check debug called properly
