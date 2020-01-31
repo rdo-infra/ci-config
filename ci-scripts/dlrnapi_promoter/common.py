5e8dae54b3fe199f7c898d29f7a796b2745fc7b8
Gabriele Cerami <gcerami@redhat.com>
2020-01-31 16:22:27 +0000

    [WIP] Implement promotion for integration pipeline
    Promoter is currently working with a single definition of dlrn hash.
    With the coming of the component pipeline, the basic assumptions for all
    dlrn hashes are not true anymore.
    The component pipeline introduced the concept of aggregate hash, which
    does adds an attribute to the existing hash, which is computed from a
    repo file through md5sum.
    Known pipelines will still promote the dlrn hahses
    Component pipeline will need to promote aggregated hashes
    
    The workflow needs to take this difference into consideration to promote
    for the component pipeline.
    The difference in dlrnapi is rendered somewhat transparent as the api
    calls for this type of hash will be the same, but overloaded with
    different attributes.
    To render the difference completely transparent to the whole workflow
    and not repeat e.g. a check "Is this single or component pipeline?"
    everywhere in the workflow, and having to test multiple execution paths
    for every single answer to this question, this patch moves all the
    complexity to the DlrnHash class and its tests.
    The workflow should never need to address the type of pipeline directly,
    and work only with the DlrnHash object in all cases.
    Stage environment setup now is able to spawn and kill dlrn server from
    the stage config file, and we can pass alternative fixture files.
    
    Change-Id: I221f4b33cf2763875fc6394902f7923108a34d2a

--- ci-scripts/dlrnapi_promoter/common.py
+++ ci-scripts/dlrnapi_promoter/common.py
@@ -1,12 +1,129 @@
 """
 This file contains general functions and classes
 """
+import logging
+import logging.handlers
+import os
+import socket
+import sys
+import time
+
+try:
+    # In python 2 ConnectionRefusedError is not a builtin
+    from socket import error as ConnectionRefusedError
+except ImportError:
+    pass
+
+
+class PromotionError(Exception):
+    pass
+
+
+class LoggingError(Exception):
+    pass
 
 
 def str2bool(value):
     """
     Converts a string with a boolean value into a proper boolean
+    mostly useful for variables coming from ini parser
     """
     if value in ['yes', 'true', 'True', 'on', '1']:
         return True
     return False
+
+
+def check_port(host, port, timeout=None, port_mode="open"):
+    """
+    Check for connection to a host:port within a timeout
+    :param host: A string with the host
+    :param port: A integer with the port
+    :param timeout:  An integer with the timeout, if None an unresobnably
+    high value is set
+    :param port_mode: decide to check if the port is open or closed
+    :return: A bool to verify if the connection was successful
+    """
+    timeout = timeout
+    if timeout is None:
+        timeout = 999999
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    if port_mode == "open":
+        connected = False
+
+        while timeout > 0 and not connected:
+            try:
+                sock.connect((host, port))
+                connected = True
+            except ConnectionRefusedError:
+                # retry in 1 sec
+                time.sleep(1)
+                timeout -= 1
+        if not connected:
+            return False
+        return True
+
+    elif port_mode == "closed":
+        connected = True
+        while connected and timeout < timeout:
+            try:
+                sock.connect((host, port))
+                # retry in 1 sec
+                time.sleep(1)
+                timeout -= 1
+            except ConnectionRefusedError:
+                connected = False
+        if connected:
+            return False
+        return True
+
+
+def close_logging(name):
+    """
+    Since logging has a global configuration let's reset logger object
+    clear out any past configurations
+    :return:
+    """
+    logger = logging.getLogger(name)
+    print("Deconfiguring Logger with name: %s" % name)
+    list(map(logger.removeHandler, logger.handlers[:]))
+    list(map(logger.removeFilter, logger.filters[:]))
+
+
+def setup_logging(name, log_level, log_file=None):
+    """
+    Sets up logging for the whole workflow, using the file provided in
+    config
+    If the process is start in a tty, we will log to console too
+    :return: None
+    """
+
+    close_logging(name)
+    log_setup_msg = "Set up logging level %s on: "
+    log_handlers = []
+
+    logger = logging.getLogger(name)
+
+    logger.setLevel(log_level)
+    log_formatter = logging.Formatter('%(asctime)s %(process)d '
+                                      '%(levelname)-8s %(name)s '
+                                      '%(message)s')
+    if log_file is not None:
+        log_handler = logging.handlers.WatchedFileHandler(
+            os.path.expanduser(log_file))
+        log_handler.setFormatter(log_formatter)
+        logger.addHandler(log_handler)
+        log_handlers.append(" file {}".format(log_file))
+    if sys.stdout.isatty():
+        log_handler = logging.StreamHandler()
+        log_handler.setFormatter(log_formatter)
+        logger.addHandler(log_handler)
+        log_handlers.append("console")
+
+    if log_handlers:
+        log_setup_msg += " ,".join(log_handlers)
+
+    try:
+        logger.info(log_setup_msg, logging.getLevelName(log_level))
+    except (FileNotFoundError, PermissionError):
+        print("Invalid Log file: {}".format(log_file))
+        raise LoggingError
---
+++ ci-scripts/dlrnapi_promoter/qcow_client.py
@@ -0,0 +1,175 @@
+"""
+This file contains classes and functionto interact with qcow images servers
+"""
+import copy
+import logging
+import os
+import paramiko
+import subprocess
+
+from common import PromotionError
+
+
+class QcowClient(object):
+    """
+    This class interacts with qcow images servers
+    """
+    log = logging.getLogger("promoter")
+
+    def __init__(self, config):
+        self.config = config
+
+        self.git_root = self.config.git_root
+        self.promote_script = os.path.join(self.git_root,
+                                           'ci-scripts', 'promote-images.sh')
+
+        self.distro_name = self.config.distro_name
+        self.distro_version = self.config.distro_version
+        # Try to load experimental config
+        if hasattr(config, 'qcow_server'):
+            server_conf = self.config.qcow_server
+            self.user = server_conf['user']
+            self.root = server_conf['root']
+            self.host = server_conf['host']
+
+            self.images_dir = os.path.join(self.root, config.distro,
+                                           config.release, "rdo_trunk")
+            print(self.images_dir)
+
+            client = paramiko.SSHClient()
+            client.load_system_host_keys()
+
+            client.connect(self.host, username=self.user)
+            self.client = client.open_sftp()
+
+    def promote(self, candidate_hash, target_label, **kwargs):
+        """
+        This method promotes images contained inside a dir in the server
+        whose name is equal to the dlrn_id specified by creating a
+        symlink to it named as the target_label.
+        It currently uses an external Bash script for the effective promotion
+        :param candidate_hash:  The hash object to select images dir
+        :param target_label: The name of the symlink
+        :return: None
+        """
+        try:
+            self.log.info("Qcow promote '{}' to {}: Attempting promotion"
+                          "".format(candidate_hash, target_label))
+            # The script doesn't really use commit/distro or full hash,
+            # it just needs the hash to identify the dir, so it works with
+            # either dlrnhash or aggregated hash.
+            cmd = ['bash',
+                   self.promote_script,
+                   '--distro', self.distro_name,
+                   '--distro-version', self.distro_version,
+                   self.config.release,
+                   candidate_hash.full_hash,
+                   target_label
+                   ]
+            qcow_logs = subprocess.check_output(cmd, stderr=subprocess.STDOUT)
+            qcow_logs_lines = qcow_logs.decode("UTF-8").split("\n")
+            self.log.info("Qcow promote '{}' to {}: Successful promotion"
+                          "".format(candidate_hash, target_label))
+            self.log.info("Qcow promote '{}' to {}: Successful promotion "
+                          "start logs -----------------------------"
+                          "".format(candidate_hash, target_label))
+            for line in qcow_logs_lines:
+                self.log.info(line)
+            self.log.info("Qcow promote '{}' to {}: Successful promotion "
+                          "end logs -----------------------------"
+                          "".format(candidate_hash, target_label))
+        except subprocess.CalledProcessError as ex:
+            self.log.error("Qcow promote '{}' to {}: Failed promotion"
+                           "".format(candidate_hash, target_label))
+            self.log.error("Qcow promote '{}' to {}: Failed promotion start "
+                           "logs -----------------------------"
+                           "".format(candidate_hash, target_label))
+            for line in ex.output.decode("UTF-8").split("\n"):
+                self.log.error(line)
+            self.log.exception(ex)
+            self.log.error("Qcow promote '{}' to {}: Failed promotion end "
+                           "logs -----------------------------"
+                           "".format(candidate_hash, target_label))
+            raise PromotionError("Failed to promote overcloud images")
+
+# ###-------- The code that follows is experimental and untested
+
+    def validate_qcows(self, dlrn_hash, name=None, assume_valid=False):
+        """
+        Check we have the images dir in the server
+        if name is specified, verify that name points to the hash
+        - maybe qcow ran and failed
+        Check at which point of qcow promotion we stopped
+        1) did we create a new symlink ?
+        2) did we create the previous symlink ?
+        3) are all the images uploaded correctly ?
+        :param dlrn_hash: The hash to check
+        :param name: The promotion name
+        :param assume_valid: report everything worked unconditionally
+        :return:
+        """
+
+        results = {
+            "hash_valid": False,
+            "promotion_valid": False,
+            "qcow_valid": False,
+            "missing_qcows": copy.copy(self.config.qcow_images),
+            "present_qcows": [],
+        }
+        if assume_valid:
+            results = {
+                "hash_valid": True,
+                "promotion_valid": True,
+                "qcow_valid": True,
+                "present_qcows": copy.copy(self.config.qcow_images),
+                "missing_qcows": [],
+            }
+            return results
+
+        try:
+            images_path = os.path.join(self.images_dir, dlrn_hash.full_hash)
+            stat = self.client.stat(images_path)
+            if stat:
+                pass
+            results['hash_valid'] = True
+            images = sorted(self.client.listdir(images_path))
+            results['present_qcows'] = images
+            results['missing_qcows'] = \
+                list(set(self.config.qcow_images).difference(images))
+            if images == self.config.qcow_images:
+                results['qcow_valid'] = True
+        except IOError:
+            pass
+
+        if name is not None:
+            try:
+                link = self.client.readlink(name)
+                if link == dlrn_hash.full_hash:
+                    results['promotion_valid'] = True
+            except IOError:
+                pass
+
+        return results
+
+    def promote_experimental(self, dlrn_hash, name, create_previous=False):
+        try:
+            current_hash = self.client.readlink(name)
+        except IOError:
+            pass
+
+        if current_hash:
+            pass
+
+        try:
+            self.client.symlink(dlrn_hash.full_hash, name)
+        finally:
+            self.client.close()
+
+    def get_previous_hash(self, promote_name):
+        try:
+            image_dir = self.client.readlink("previous" + promote_name)
+        except IOError:
+            pass
+        previous_hash = image_dir
+
+        return previous_hash
