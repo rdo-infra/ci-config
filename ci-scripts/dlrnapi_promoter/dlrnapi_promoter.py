014b833eeb03f3ad15d8a0754f4a2ae39f460075
Gabriele Cerami <gcerami@redhat.com>
2020-02-06 14:28:45 +0000

    [WIP] Adapt staging environment for the component pipeline
    Change-Id: Ia2a82296fa660061429419d0d0240647300a4bd9

--- ci-scripts/dlrnapi_promoter/dlrnapi_promoter.py
+++ ci-scripts/dlrnapi_promoter/dlrnapi_promoter.py
@@ -18,10 +18,7 @@ import sys
 from common import str2bool
 from config import PromoterConfig
 from logic import PromoterLogic
-# Import previous content from the legacy_promoter file
-import legacy_promoter
 from legacy_promoter import legacy_main
-from legacy_promoter import fetch_current_named_hashes
 
 
 class Promoter(object):
@@ -84,7 +81,6 @@ def main(cmd_line=None):
         args = main_parser.parse_args(cmd_line.split())
     else:
         args = main_parser.parse_args()
-    logger = logging.getLogger('promoter')
     # Main execution paths branch we either use legacy code or we use
     # modularized
     if args.force_legacy or str2bool(os.environ.get("PROMOTER_FORCE_LEGACY",
