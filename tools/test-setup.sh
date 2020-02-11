344c38c485d79913828d31522a8fc15fbc4da2a0
Sorin Sbarnea <ssbarnea@redhat.com>
2020-02-11 14:47:16 +0000

    WIP: Prevent virtualenv failure due to older six
    virtualenv>=20 require six>=1.12 or it would fail with an ugly error.
    
    Installation of tox-venv should enable tox to use venv instead of
    virtualenv for creating the environments.
    
    Change-Id: I5b6f82a2763bf97b4701959b0d9a8d7cbe609311

---
+++ tools/test-setup.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+set -euxo pipefail
+# Used by Zuul CI to perform extra bootstrapping
+
+# Bumping system tox because version from CentOS 7 is too old
+# We are not using pip --user due to few bugs in tox role which does not allow
+# us to override how is called. Once these are addressed we will switch back
+# non-sudo mode.
+
+# Hack for fixing broken combination of nodepool image and ensure-tox role.
+
+PYTHON=$(command -v python3 python | head -n1)
+
+# pre debug
+$PYTHON -m virtualenv --version || true
+$PYTHON -m tox --version || true
+/usr/local/bin/tox --version  || true
+$PYTHON -m pip --version || true
+$PYTHON -m pip freeze
+
+# remove tox if it exists in user-libs, or it could be picked by python later
+"$PYTHON" -m pip uninstall tox || true
+
+sudo rm -f /usr/local/bin/tox
+sudo "$PYTHON" -m pip install "virtualenv<20.0" "tox>=3.14.3" tox-venv "zipp<0.6.0;python_version=='2.7'"
+
+# post debug
+$PYTHON --version
+$PYTHON -m virtualenv --version
+$PYTHON -m tox --version
+/usr/local/bin/tox --version  # this needs to work for ensure-tox in particular
+$PYTHON -m pip freeze
