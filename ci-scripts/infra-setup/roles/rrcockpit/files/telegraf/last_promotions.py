8102482ec51a707b18886eb3db342879cec62f80
Wes Hayutin <weshayutin@gmail.com>
2020-03-03 21:56:31 -0700

    update telegraph promotion monitor for centos-8
    Change-Id: I2e0db05ae09dba1e545c87dacd84770a4f11739b

--- ci-scripts/infra-setup/roles/rrcockpit/files/telegraf/last_promotions.py
+++ ci-scripts/infra-setup/roles/rrcockpit/files/telegraf/last_promotions.py
@@ -6,6 +6,7 @@ import dlrnapi_client
 from influxdb_utils import format_ts_from_float
 from promoter_utils import get_dlrn_instance
 from promoter_utils import get_promoter_config
+from promoter_utils import get_promoter_component_config
 
 PROMOTION_INFLUXDB_LINE = ("dlrn-promotion,"
                            "release={release},distro={distro},"
@@ -13,7 +14,8 @@ PROMOTION_INFLUXDB_LINE = ("dlrn-promotion,"
                            "commit_hash=\"{commit_hash}\","
                            "distro_hash=\"{distro_hash}\","
                            "repo_hash=\"{repo_hash}\","
-                           "repo_url=\"{repo_url}\" "
+                           "repo_url=\"{repo_url}\","
+                           "component=\"{component}\" "
                            "{timestamp}")
 
 
@@ -22,14 +24,19 @@ def influxdb(promotion):
     return PROMOTION_INFLUXDB_LINE.format(**promotion)
 
 
-def get_last_promotion(dlrn, release, distro, name):
+def get_last_promotion(dlrn, release, distro, name, component=None):
     query = dlrnapi_client.PromotionQuery()
     query.promote_name = name
+    if component:
+        query.component = component
     promotions = dlrn.api_promotions_get(query)
     if promotions:
         last_promotion = promotions[0].to_dict()
         last_promotion['release'] = release
         last_promotion['distro'] = distro
+        # sanitize dict
+        if last_promotion['component'] is None:
+            last_promotion['component'] = "none"
         return last_promotion
     return
 
@@ -41,13 +48,22 @@ if __name__ == '__main__':
 
     parser.add_argument('--release', required=True)
     parser.add_argument('--distro', default="CentOS-7")
+    parser.add_argument('--component', default=None)
     args = parser.parse_args()
 
-    promoter_config = get_promoter_config(args.release, args.distro)
+    if args.component is None:
+        promoter_config = get_promoter_config(args.release, args.distro)
+    else:
+        promoter_config = get_promoter_component_config(
+            args.release, args.distro)
     dlrn = get_dlrn_instance(promoter_config)
     if dlrn:
-        for promotion_name, _ in promoter_config.items('promote_from'):
-            promo = get_last_promotion(dlrn, args.release, args.distro,
-                                       promotion_name)
+        for promotion_name in promoter_config['promote_from']:
+            if args.component is None:
+                promo = get_last_promotion(dlrn, args.release, args.distro,
+                                           promotion_name)
+            else:
+                promo = get_last_promotion(dlrn, args.release, args.distro,
+                                           promotion_name, args.component)
             if promo:
                 print(influxdb(promo))
