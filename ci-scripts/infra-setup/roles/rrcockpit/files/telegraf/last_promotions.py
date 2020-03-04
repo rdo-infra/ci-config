ae83c43223fc7d2ed82b6240db351403e30ebe4e
Wes Hayutin <weshayutin@gmail.com>
2020-03-03 21:56:31 -0700

    WIP, update telegraph promotion monitor for centos-8
    Change-Id: I2e0db05ae09dba1e545c87dacd84770a4f11739b

--- ci-scripts/infra-setup/roles/rrcockpit/files/telegraf/last_promotions.py
+++ ci-scripts/infra-setup/roles/rrcockpit/files/telegraf/last_promotions.py
@@ -18,36 +18,37 @@ PROMOTION_INFLUXDB_LINE = ("dlrn-promotion,"
 
 
 def influxdb(promotion):
-    promotion['timestamp'] = format_ts_from_float(promotion['timestamp'])
-    return PROMOTION_INFLUXDB_LINE.format(**promotion)
+  promotion['timestamp'] = format_ts_from_float(promotion['timestamp'])
+  return PROMOTION_INFLUXDB_LINE.format(**promotion)
 
 
-def get_last_promotion(dlrn, release, distro, name):
-    query = dlrnapi_client.PromotionQuery()
-    query.promote_name = name
-    promotions = dlrn.api_promotions_get(query)
-    if promotions:
-        last_promotion = promotions[0].to_dict()
-        last_promotion['release'] = release
-        last_promotion['distro'] = distro
-        return last_promotion
-    return
+def get_last_promotion(dlrn, release, distro, name, component_name):
+  query = dlrnapi_client.PromotionQuery()
+  query.promote_name = name
+  promotions = dlrn.api_promotions_get(query)
+  if promotions:
+    last_promotion = promotions[0].to_dict()
+    last_promotion['release'] = release
+    last_promotion['distro'] = distro
+    return last_promotion
+  return
 
 
 if __name__ == '__main__':
 
-    parser = argparse.ArgumentParser(
-        description="Print release last promotions as influxdb lines")
+  parser = argparse.ArgumentParser(
+      description="Print release last promotions as influxdb lines")
 
-    parser.add_argument('--release', required=True)
-    parser.add_argument('--distro', default="CentOS-7")
-    args = parser.parse_args()
+  parser.add_argument('--release', required=True)
+  parser.add_argument('--distro', default="CentOS-7")
+  parser.add_argument('--component', default=None)
+  args = parser.parse_args()
 
-    promoter_config = get_promoter_config(args.release, args.distro)
-    dlrn = get_dlrn_instance(promoter_config)
-    if dlrn:
-        for promotion_name, _ in promoter_config.items('promote_from'):
-            promo = get_last_promotion(dlrn, args.release, args.distro,
-                                       promotion_name)
-            if promo:
-                print(influxdb(promo))
+  promoter_config = get_promoter_config(args.release, args.distro)
+  dlrn = get_dlrn_instance(promoter_config)
+  if dlrn:
+    for promotion_name, _ in promoter_config.items('promote_from'):
+      promo = get_last_promotion(dlrn, args.release, args.distro,
+                                 promotion_name)
+      if promo:
+        print(influxdb(promo))
--- ci-scripts/infra-setup/roles/rrcockpit/files/telegraf/promoter_utils.py
+++ ci-scripts/infra-setup/roles/rrcockpit/files/telegraf/promoter_utils.py
@@ -1,20 +1,36 @@
 import dlrnapi_client
 import ConfigParser
 import requests
+import yaml
 
 from StringIO import StringIO
 
 PROMOTER_CONFIG_URL = ("https://raw.githubusercontent.com/rdo-infra/ci-config/"
                        "master/ci-scripts/dlrnapi_promoter/config/{}/{}.ini")
 
+COMPONENT_CONFIG_URL = (https: // raw.githubusercontent.com / rdo - infra / ci - config /"
+                        "master/ci-scripts/dlrnapi_promoter/"
+                        "config/{}/component/{}.yaml)"
+
 
 def get_promoter_config(release, distro='CentOS-7'):
 
-    response = requests.get(PROMOTER_CONFIG_URL.format(distro, release))
+    response=requests.get(PROMOTER_CONFIG_URL.format(distro, release))
 
     if response.ok:
-        config = ConfigParser.SafeConfigParser(allow_no_value=True)
+        # https://docs.python.org/3/library/configparser.html
+        config=ConfigParser.SafeConfigParser(allow_no_value=True)
         config.readfp(StringIO(response.content))
+        return config
+    else:
+        return None
+
+def get_promoter_component_config(release, distro='CentOS-8'):
+
+    response=requests.get(COMPONENT_CONFIG_URL.format(distro, release)).text
+
+    if response.ok:
+        config=yaml.load(response, Loader=yaml.FullLoader)
         return config
     else:
         return None
@@ -25,11 +41,15 @@ def get_dlrn_instance(config=None):
     if config is None:
         return None
 
-    api_client = dlrnapi_client.ApiClient(host=config.get('main', 'api_url'))
+    if isinstance(config, dict):
+        api_client=dlrnapi_client.ApiClient(host=config['main']['api_url'])
+    else:
+        api_client=dlrnapi_client.ApiClient(host=config.get('main', 'api_url'))
+
     return dlrnapi_client.DefaultApi(api_client=api_client)
 
 
 def get_dlrn_instance_for_release(release):
-    promoter_config = get_promoter_config(release)
+    promoter_config=get_promoter_config(release)
     if promoter_config:
         return get_dlrn_instance(promoter_config)
