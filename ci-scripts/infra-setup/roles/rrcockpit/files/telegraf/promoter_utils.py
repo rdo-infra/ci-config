3aea96e9cfe18a4b67718b276bd20b6b1d087f39
Wes Hayutin <weshayutin@gmail.com>
2020-03-03 21:56:31 -0700

    update telegraph promotion monitor for centos-8
    Change-Id: I2e0db05ae09dba1e545c87dacd84770a4f11739b

--- ci-scripts/infra-setup/roles/rrcockpit/files/telegraf/promoter_utils.py
+++ ci-scripts/infra-setup/roles/rrcockpit/files/telegraf/promoter_utils.py
@@ -1,11 +1,16 @@
 import dlrnapi_client
 import ConfigParser
 import requests
+import yaml
 
 from StringIO import StringIO
 
 PROMOTER_CONFIG_URL = ("https://raw.githubusercontent.com/rdo-infra/ci-config/"
                        "master/ci-scripts/dlrnapi_promoter/config/{}/{}.ini")
+
+COMPONENT_CONFIG_URL = ("https://raw.githubusercontent.com/rdo-infra/ci-config/"
+                        "master/ci-scripts/dlrnapi_promoter/"
+                        "config/{}/component/{}.yaml")
 
 
 def get_promoter_config(release, distro='CentOS-7'):
@@ -15,6 +20,18 @@ def get_promoter_config(release, distro='CentOS-7'):
     if response.ok:
         config = ConfigParser.SafeConfigParser(allow_no_value=True)
         config.readfp(StringIO(response.content))
+        config = config._sections
+        return config
+    else:
+        return None
+
+
+def get_promoter_component_config(release, distro='CentOS-8'):
+
+    response = requests.get(COMPONENT_CONFIG_URL.format(distro, release))
+
+    if response.ok:
+        config = yaml.load(response.text, Loader=yaml.FullLoader)
         return config
     else:
         return None
@@ -25,7 +42,12 @@ def get_dlrn_instance(config=None):
     if config is None:
         return None
 
-    api_client = dlrnapi_client.ApiClient(host=config.get('main', 'api_url'))
+    if isinstance(config, dict):
+        api_client = dlrnapi_client.ApiClient(host=config['main']['api_url'])
+    else:
+        api_client = dlrnapi_client.ApiClient(
+            host=config.get('main', 'api_url'))
+
     return dlrnapi_client.DefaultApi(api_client=api_client)
 
 
