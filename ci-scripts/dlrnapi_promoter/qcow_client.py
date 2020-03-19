b93d5d448e7aedf594b0f71e38d508739156baad
Gabriele Cerami <gcerami@redhat.com>
2020-03-19 12:32:41 +0000

    Promoter: implement qcow promotion in python
    Currently the promoter is calling an external bash script to promote
    overcloud images.
    This patch implements the same functionality in python.
    The feature is experimentali, not fully tested and not enabled by default
    
    Task: https://tree.taiga.io/project/tripleo-ci-board/task/1744
    Change-Id: I47895bfbcb60125b91eb821466ca7c6715782c21

--- ci-scripts/dlrnapi_promoter/qcow_client.py
+++ ci-scripts/dlrnapi_promoter/qcow_client.py
@@ -24,6 +24,7 @@ class QcowClient(object):
 
         self.distro_name = self.config.distro_name
         self.distro_version = self.config.distro_version
+        self.rollback_links = {}
         # Try to load experimental config
         if hasattr(config, 'qcow_server'):
             # Currently paramiko is not in requirements, and I don't want to
@@ -39,9 +40,25 @@ class QcowClient(object):
 
             client = paramiko.SSHClient()
             client.load_system_host_keys()
+            client.set_missing_host_key_policy(paramiko.WarningPolicy)
 
-            client.connect(self.host, username=self.user)
+            keypath = os.path.expanduser('~/.ssh/id_rsa')
+            key = paramiko.rsakey.RSAKey(filename=keypath)
+            kwargs = {}
+            if self.user is not None:
+                kwargs['username'] = self.user
+            client.connect(self.host, pkey=key, **kwargs)
+            self.log.debug("Connected to %s", self.host)
             self.client = client.open_sftp()
+            try:
+                self.client.listdir(self.images_dir)
+                self.client.chdir(self.images_dir)
+            except IOError as ex:
+                self.log.error("Qcow-client: Image root dir %s does not exist "
+                               "in the server or is not accessible")
+                self.log.exception(ex)
+                self.client.close()
+                raise
 
     def promote(self, candidate_hash, target_label, **kwargs):
         """
@@ -148,19 +165,110 @@ class QcowClient(object):
 
         return results
 
-    def promote_experimental(self, dlrn_hash, name, create_previous=False):
+    def rollback(self):
+        """
+        Rolls back the link to the initial status
+        Rollback is guaranteed to work only for caught exceptions, and it may
+        not be really useful. We have a rollback only if a remove or a symlink
+        fails.
+        - If a remove fails, it means that we don't need to rollback
+        - If a symlink fails, then it will probably fail on rollback too.
+        :return: None
+        """
+        for name, target in self.rollback_links.items():
+            self.client.remove(name)
+            self.client.symlink(target, name)
+            self.rollback_links = {}
+
+    def promote_experimental(self, candidate_hash, target_label,
+                             candidate_label=None,
+                             create_previous=True):
+        log_header = "qcow promote '{}' to {}:".format(candidate_hash,
+                                                       target_label)
+        self.log.info("%s Attempting promotion", log_header)
+
+        # Check if candidate_hash dir is present
         try:
-            current_hash = self.client.readlink(name)
+            self.client.stat(candidate_hash.full_hash)
+        except IOError as ex:
+            self.log.error("%s images dir for hash %s not present or not "
+                           "accessible", log_header,
+                           candidate_hash)
+            self.log.exception(ex)
+            self.client.close()
+            raise
+
+        # Check if the target label exists and points to a hash dir
+        current_hash = None
+        try:
+            current_hash = self.client.readlink(target_label)
         except IOError:
-            pass
+            self.log.debug("%s No link named %s exists", log_header,
+                           target_label)
 
+        # If this exists Check if we can remove  the symlink
         if current_hash:
-            pass
+            self.rollback_links['target_label'] = current_hash
+            try:
+                self.client.remove(target_label)
+            except IOError as ex:
+                self.log.debug("Unable to remove the target_label: %s",
+                               target_label)
+                self.log.exception(ex)
+                self.client.close()
+                raise
 
+        # Check if a previous link exists and points to an hash-dir
+        previous_label = "previous-{}".format(target_label)
+        previous_hash = None
         try:
-            self.client.symlink(dlrn_hash.full_hash, name)
+            previous_hash = self.client.readlink(previous_label)
+        except IOError:
+            self.log.debug("%s No previous-link named %s exists",
+                           log_header,
+                           previous_label)
+
+        # If it exists and we are handling it, check if we can remove and
+        # reassign it
+        if previous_hash and create_previous:
+            self.rollback_links[previous_label] = previous_hash
+            try:
+                self.client.remove(previous_label)
+            except IOError as ex:
+                self.log.debug("Unable to remove the target_label: %s",
+                               target_label)
+                self.log.exception(ex)
+                self.client.close()
+                # Rollback is not tested, we enable it later, when tests are
+                # easier to add
+                # self.rollback()
+                raise
+            try:
+                self.client.symlink(current_hash, previous_label)
+            except IOError as ex:
+                self.log.error("%s failed to link %s to %s", log_header,
+                               previous_label, current_hash)
+                self.log.exception(ex)
+                # Rollback is not tested, we enable it later, when tests are
+                # easier to add
+                # self.rollback()
+                self.client.close()
+                raise
+
+        # Finally the effective promotion
+        try:
+            self.client.symlink(candidate_hash.full_hash, target_label)
+        except IOError as ex:
+            self.log.error("%s failed to link %s to %s", log_header,
+                           target_label, candidate_hash.full_hash)
+            self.log.exception(ex)
+            # Rollback is not tested, we enable it later, when tests are
+            # easier to add
+            # self.rollback()
         finally:
             self.client.close()
+
+        self.log.info("%s Successful promotion", log_header)
 
     def get_previous_hash(self, promote_name):
         try:
