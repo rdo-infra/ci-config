833b5d0268cc7f3f8b24f4f9ee80d6ba328da1c9
Mike Turek <mjturek@linux.vnet.ibm.com>
2020-03-26 14:36:20 +0000

    ppc64le: Switch to centos8 nodes in cico
    In order to restore the ppc64le build job we  need to migrate to
    centos8. Nodes became available there recently. This patch simply
    adds a --release 8 to the cico command. We anticipate more changes
    will be needed after this.
    
    Change-Id: I152347fb905424aa0d2bace3102f8f1fc566b066
    Co-Authored-By: Adam C Kimball <baha@astyr.net>

--- jenkins/jobs/scripts/cico-node-get-to-ansible.sh
+++ jenkins/jobs/scripts/cico-node-get-to-ansible.sh
@@ -15,7 +15,7 @@ localhost ansible_connection=local
 EOF
 
 # Get nodes
-nodes=$(cico -q node get --arch $CPU_ARCH --flavor $CICO_FLAVOR --retry-count 6 --retry-interval 60  --count ${NODE_COUNT} --column hostname --column ip_address --column comment -f value)
+nodes=$(cico -q node get --arch $CPU_ARCH --flavor $CICO_FLAVOR --release $CICO_OS_RELEASE --retry-count 6 --retry-interval 60  --count ${NODE_COUNT} --column hostname --column ip_address --column comment -f value)
 
 # Write nodes to inventory file and persist the SSID separately for simplicity
 touch ${SSID_FILE}
