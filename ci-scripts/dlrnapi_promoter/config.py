ef54d053d072b4c57d069b2ad394bd7577daa6d3
Gabriele Cerami <gcerami@redhat.com>
2020-05-15 18:18:18 +0100

    Promoter: make config root a variable
    To prepare running the promoter on multiple environments, we need first
    to be able to specify multiple configuration directories.
    
    This patch adds a new option for the promoter to pass the config root.
    It also makes moves the list of releases to promote, from being
    hardcoded in the start script, to be an export in a config file inside
    the default config root
    
    Change-Id: I818c7a4ebc5f32af933acf8fcb25046f66139edd

--- ci-scripts/dlrnapi_promoter/config.py
+++ ci-scripts/dlrnapi_promoter/config.py
@@ -59,7 +59,7 @@ class PromoterConfigBase(object):
     }
     log = logging.getLogger("promoter")
 
-    def __init__(self, config_file):
+    def __init__(self, config_root, config_file):
         """
         Initialize the config object loading from ini file
         :param config_path: the path to the configuration file to load
@@ -73,10 +73,12 @@ class PromoterConfigBase(object):
 
         if config_file is None:
             raise ConfigError("Empty config file")
-        # The path is either absolute ot it's relative to the code root
+        # The path is either absolute ot it's relative to the script_root
+        if not os.path.isabs(config_root):
+            config_root = os.path.join(self.script_root, config_root)
+        # The path is either absolute ot it's relative to the config_root
         if not os.path.isabs(config_file):
-            config_file = os.path.join(self.script_root, "config",
-                                       config_file)
+            config_file = os.path.join(config_root, config_file)
         try:
             os.stat(config_file)
         except OSError:
@@ -169,7 +171,7 @@ class PromoterConfig(PromoterConfigBase):
     class should be used by the promoter
     """
 
-    def __init__(self, config_file, overrides=None, filters="all",
+    def __init__(self, config_root, config_file, overrides=None, filters="all",
                  checks="all"):
         """
         Expands the parent init by adding config expansion, overrides
@@ -177,7 +179,7 @@ class PromoterConfig(PromoterConfigBase):
         :param config_path: the path to the configuration file to load
         :param overrides: An object with override for the configuration
         """
-        super(PromoterConfig, self).__init__(config_file)
+        super(PromoterConfig, self).__init__(config_root, config_file)
 
         config = {}
         if filters == "all":
