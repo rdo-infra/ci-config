c0d7bab1a30dd824f34c1e706eeac2b5fa427d94
Gabriele Cerami <gcerami@redhat.com>
2020-05-19 18:31:26 +0100

    Promoter: switch to qcow python promotion
    This change enables the qcow promotions as default method to promote
    images, and add some more unit tests for the QcowClient class
    
    Task: https://tree.taiga.io/project/tripleo-ci-board/task/1750
    Change-Id: I963192a8727cd23c2a3f96064044be5ec094b08a

--- ci-scripts/dlrnapi_promoter/test_qcow_client_unit.py
+++ ci-scripts/dlrnapi_promoter/test_qcow_client_unit.py
@@ -1,5 +1,12 @@
 import pytest
-import subprocess
+import os
+import shutil
+import stat
+import unittest
+
+from config import PromoterConfigBase
+from qcow_client import QcowConnectionClient
+
 try:
     # Python3 imports
     from unittest.mock import Mock, patch
@@ -9,46 +16,249 @@ except ImportError:
     from mock import Mock, patch
     import mock
 
+try:
+    FileExistsError
+except NameError:
+    FileExistsError = OSError
+
+from promoter_integration_checks import check_links
+
 from common import PromotionError
 from dlrn_hash import DlrnHash
 from test_unit_fixtures import ConfigSetup, hashes_test_cases
 
 
-class TestQcowClient(ConfigSetup):
+class TestQcowConnectionClient(unittest.TestCase):
 
     def setUp(self):
-        super(TestQcowClient, self).setUp()
+        self.server_conf_os = {
+            'client': 'os',
+            'host': 'localhost',
+            'user': os.environ['USER'],
+        }
+        self.server_conf_sftp = {
+            'host': 'localhost',
+            'user': os.environ['USER'],
+            'client': 'sftp',
+        }
+
+    @patch('paramiko.SSHClient.close')
+    @patch('paramiko.SSHClient.connect')
+    def test_instance_os_connect_close(self, paramiko_connect_mock,
+                                       paramiko_close_mock):
+        client = QcowConnectionClient(self.server_conf_os)
+        self.assertIsNotNone(client.getcwd())
+
+        client.connect()
+        self.assertFalse(paramiko_connect_mock.called)
+
+        client.close()
+        self.assertFalse(paramiko_close_mock.called)
+
+    @patch('paramiko.SSHClient.close')
+    @patch('paramiko.SSHClient.connect')
+    @patch('paramiko.SSHClient.open_sftp')
+    def test_instance_sftp_connect_close(self,
+                                         paramiko_sftp_mock,
+                                         paramiko_connect_mock,
+                                         paramiko_close_mock):
+        client = QcowConnectionClient(self.server_conf_sftp)
+        assert hasattr(client, "ssh_client")
+
+        client.connect()
+        paramiko_connect_mock.assert_has_calls([
+            mock.call("localhost", pkey=mock.ANY, username=os.environ['USER'])
+            ])
+        self.assertTrue(paramiko_connect_mock.called)
+        self.assertTrue(paramiko_sftp_mock.called)
+
+        client.close()
+        self.assertFalse(paramiko_close_mock.called)
+
+
+class TestQcowClientPromotion(ConfigSetup):
+
+    def setUp(self):
+        super(TestQcowClientPromotion, self).setUp()
         self.client = self.promoter.qcow_client
+
+        self.images_root = self.client.root
+        self.images_dir = self.client.images_dir
+        self.previous_hash_dir = os.path.join(self.images_dir, "efgh")
+        self.current_hash_dir = os.path.join(self.images_dir, "dunno")
+        self.candidate_hash_dir = os.path.join(self.images_dir, "abcd")
+        self.target_label = "test-label"
+        self.previous_target_label = "previous-{}".format(self.target_label)
+
+        try:
+            os.makedirs(self.candidate_hash_dir)
+        except FileExistsError:
+            pass
+        try:
+            os.makedirs(self.previous_hash_dir)
+        except FileExistsError:
+            pass
+        self.valid_candidate_hash = \
+            DlrnHash(source=hashes_test_cases['aggregate']['dict']['valid'])
+        self.missing_candidate_hash = \
+            DlrnHash(source=hashes_test_cases['aggregate']['dict']['different'])
+
+    def tearDown(self):
+        super(TestQcowClientPromotion, self).tearDown()
+        os.chdir("/")
+        shutil.rmtree(self.images_root)
 
     @patch('logging.Logger.error')
     @patch('logging.Logger.info')
-    @patch('subprocess.check_output')
-    def test_promote_success(self, check_output_mock,
-                             mock_log_info,
-                             mock_log_error
-                             ):
-        candidate_hash = \
-            DlrnHash(source=hashes_test_cases['aggregate']['dict']['valid'])
-        target_label = "test"
+    def test_promote_success_no_previous(self,
+                                         mock_log_info,
+                                         mock_log_error):
 
-        check_output_mock.return_value = b"test log"
-        self.client.promote(candidate_hash, target_label)
+        self.client.promote(self.valid_candidate_hash, self.target_label,
+                            create_previous=False)
 
-        self.assertTrue(check_output_mock.called)
+        promotion_link = os.path.join(self.images_dir, self.target_label)
+        check_links(os, promotion_link, "test", os.path.basename(self.candidate_hash_dir))
+
         self.assertFalse(mock_log_error.called)
 
     @patch('logging.Logger.error')
     @patch('logging.Logger.info')
-    @mock.patch('subprocess.check_output')
-    def test_promote_failure(self, check_output_mock,
-                             mock_log_info,
-                             mock_log_error):
-        candidate_hash = \
-            DlrnHash(source=hashes_test_cases['aggregate']['dict']['valid'])
-        target_label = "test"
-        exception = subprocess.CalledProcessError(1, 2)
-        exception.output = b"test"
-        check_output_mock.side_effect = exception
+    def test_promote_success_previous(self,
+                                      mock_log_info,
+                                      mock_log_error):
+
+        os.symlink(self.previous_hash_dir, os.path.join(self.images_dir,
+                                                        self.target_label))
+        os.symlink(self.previous_hash_dir,
+                   os.path.join(self.images_dir, self.previous_target_label))
+
+        self.client.promote(self.valid_candidate_hash, self.target_label)
+
+        promotion_link = os.path.join(self.images_dir, "test")
+        previous_link = os.path.join(self.images_dir, "previous-test")
+        previous_dir = os.path.join(self.images_dir, "efgh")
+        check_links(os, promotion_link, "test", os.path.basename(
+            self.candidate_hash_dir), previous_link=previous_link,
+                    previous_dir=previous_dir)
+
+        self.assertFalse(mock_log_error.called)
+
+    @patch('logging.Logger.error')
+    @patch('logging.Logger.info')
+    def test_promote_failure_missing_candidate_dir(self,
+                                                   mock_log_info,
+                                                   mock_log_error):
         with self.assertRaises(PromotionError):
-            self.client.promote(candidate_hash, target_label)
+            self.client.promote(self.missing_candidate_hash, self.target_label)
         self.assertTrue(mock_log_error.called)
+
+
+class TestQcowClientRollback(unittest.TestCase):
+
+    @pytest.mark.xfail(reason="Not yet enabled", run=False)
+    def test_rollback_pass(self):
+        assert False
+
+    @pytest.mark.xfail(reason="Not yet enabled", run=False)
+    def test_rollback_fail(self):
+        assert False
+
+
+class TestQcowClientValidation(ConfigSetup):
+
+    def setUp(self):
+        super(TestQcowClientValidation, self).setUp()
+        self.client = self.promoter.qcow_client
+
+        self.images_root = self.client.root
+        self.images_dir = self.client.images_dir
+        self.previous_hash_dir = os.path.join(self.images_dir, "efgh")
+        self.current_hash_dir = os.path.join(self.images_dir, "dunno")
+        self.candidate_hash_dir = os.path.join(self.images_dir, "abcd")
+        self.target_label = "test-label"
+        self.previous_target_label = "previous-{}".format(self.target_label)
+
+        try:
+            os.makedirs(self.candidate_hash_dir)
+        except FileExistsError:
+            pass
+        try:
+            os.makedirs(self.previous_hash_dir)
+        except FileExistsError:
+            pass
+        self.valid_candidate_hash = \
+            DlrnHash(source=hashes_test_cases['aggregate']['dict']['valid'])
+        self.missing_candidate_hash = \
+            DlrnHash(source=hashes_test_cases['aggregate']['dict']['different'])
+
+    def tearDown(self):
+        super(TestQcowClientValidation, self).tearDown()
+        os.chdir("/")
+        shutil.rmtree(self.images_root)
+
+    def test_validation_full_pass(self):
+        expected_qcows = PromoterConfigBase.defaults['overcloud_images'][
+            'qcow_images']
+        for image_file in expected_qcows:
+            with open(os.path.join(self.candidate_hash_dir, image_file), "w"):
+                pass
+
+        os.chdir(self.images_dir)
+        os.symlink(self.valid_candidate_hash.full_hash, "images_promoted")
+
+        validation_results = self.client.validate_qcows(
+            self.valid_candidate_hash, name="images_promoted")
+
+        self.assertTrue(validation_results['hash_valid'])
+        self.assertEqual(validation_results['present_qcows'], expected_qcows)
+        self.assertEqual(validation_results['missing_qcows'], [])
+        self.assertTrue(validation_results['qcow_valid'])
+        self.assertTrue(validation_results['promotion_valid'])
+
+    def test_validate_invalid_promotion(self):
+        expected_qcows = PromoterConfigBase.defaults['overcloud_images'][
+            'qcow_images']
+        for image_file in expected_qcows:
+            with open(os.path.join(self.candidate_hash_dir, image_file), "w"):
+                pass
+
+        validation_results = self.client.validate_qcows(
+            self.valid_candidate_hash, name="images_promoted")
+
+        self.assertTrue(validation_results['hash_valid'])
+        self.assertEqual(validation_results['present_qcows'], expected_qcows)
+        self.assertEqual(validation_results['missing_qcows'], [])
+        self.assertTrue(validation_results['qcow_valid'])
+        self.assertFalse(validation_results['promotion_valid'])
+
+    def test_validate_incomplete_images(self):
+        expected_qcows = PromoterConfigBase.defaults['overcloud_images'][
+            'qcow_images']
+        expected_qcows.remove('undercloud.qcow2')
+        for image_file in expected_qcows:
+            with open(os.path.join(self.candidate_hash_dir, image_file), "w"):
+                pass
+
+        validation_results = self.client.validate_qcows(
+            self.valid_candidate_hash, name="images_promoted")
+
+        self.assertTrue(validation_results['hash_valid'])
+        self.assertEqual(validation_results['present_qcows'], expected_qcows)
+        self.assertEqual(validation_results['missing_qcows'],
+                         ['undercloud.qcow2'])
+        self.assertFalse(validation_results['qcow_valid'])
+        self.assertFalse(validation_results['promotion_valid'])
+
+    def test_validate_invalid_hash(self):
+        missing_qcows = PromoterConfigBase.defaults['overcloud_images'][
+            'qcow_images']
+
+        validation_results = self.client.validate_qcows(
+            self.missing_candidate_hash, name="images_promoted")
+
+        self.assertFalse(validation_results['hash_valid'])
+        self.assertEqual(validation_results['present_qcows'], [])
+        self.assertEqual(validation_results['missing_qcows'], missing_qcows)
+        self.assertFalse(validation_results['qcow_valid'])
+        self.assertFalse(validation_results['promotion_valid'])
