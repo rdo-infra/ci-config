248f55fbf6d66f3cf2d29ad1632adab90781abfc
Gabriele Cerami <gcerami@redhat.com>
2020-05-19 18:31:26 +0100

    Promoter: switch to qcow python promotion
    This change enables the qcow promotions as default method to promote
    images, and add some more unit tests for the QcowClient class
    
    Task: https://tree.taiga.io/project/tripleo-ci-board/task/1750
    Change-Id: I963192a8727cd23c2a3f96064044be5ec094b08a

--- ci-scripts/dlrnapi_promoter/qcow_client.py
+++ ci-scripts/dlrnapi_promoter/qcow_client.py
@@ -4,9 +4,52 @@ This file contains classes and functionto interact with qcow images servers
 import copy
 import logging
 import os
-import subprocess
 
+import paramiko
 from common import PromotionError
+
+
+class QcowConnectionClient(object):
+    """
+    Proxy class for client connection
+    """
+
+    _log = logging.getLogger("promoter")
+
+    def __init__(self, server_conf):
+        self._host = server_conf['host']
+        self._user = server_conf['user']
+        self._client_type = server_conf['client']
+        self._client = os
+        if server_conf['client'] == "sftp":
+            client = paramiko.SSHClient()
+            client.load_system_host_keys()
+            client.set_missing_host_key_policy(paramiko.WarningPolicy)
+
+            keypath = os.path.expanduser('~/.ssh/id_rsa')
+            self.key = paramiko.rsakey.RSAKey(filename=keypath)
+            self.kwargs = {}
+            if self._user is not None:
+                self.kwargs['username'] = self._user
+            else:
+                self.kwargs['username'] = os.environ.get("USER")
+
+            self._log.debug("Connecting to %s as user %s", self._host,
+                            self._user)
+
+            self.ssh_client = client
+
+    def connect(self):
+        if hasattr(self, 'ssh_client'):
+            self.ssh_client.connect(self._host, pkey=self.key, **self.kwargs)
+            self._client = self.ssh_client.open_sftp()
+
+    def __getattr__(self, item):
+        return getattr(self._client, item)
+
+    def close(self):
+        if self._client_type == "sftp":
+            self._client.close()
 
 
 class QcowClient(object):
@@ -25,88 +68,14 @@ class QcowClient(object):
         self.distro_name = self.config.distro_name
         self.distro_version = self.config.distro_version
         self.rollback_links = {}
-        # Try to load experimental config
-        if hasattr(config, 'qcow_server'):
-            # Currently paramiko is not in requirements, and I don't want to
-            # add it for the experimental code.
-            import paramiko
-            server_conf = self.config.qcow_server
-            self.user = server_conf['user']
-            self.root = server_conf['root']
-            self.host = server_conf['host']
+        server_conf = self.config.qcow_server
+        self.user = server_conf['user']
+        self.root = server_conf['root']
+        self.host = server_conf['host']
 
-            self.images_dir = os.path.join(self.root, config.distro,
-                                           config.release, "rdo_trunk")
-
-            client = paramiko.SSHClient()
-            client.load_system_host_keys()
-            client.set_missing_host_key_policy(paramiko.WarningPolicy)
-
-            keypath = os.path.expanduser('~/.ssh/id_rsa')
-            key = paramiko.rsakey.RSAKey(filename=keypath)
-            kwargs = {}
-            if self.user is not None:
-                kwargs['username'] = self.user
-            client.connect(self.host, pkey=key, **kwargs)
-            self.log.debug("Connected to %s", self.host)
-            self.client = client.open_sftp()
-            try:
-                self.client.listdir(self.images_dir)
-                self.client.chdir(self.images_dir)
-            except IOError as ex:
-                self.log.error("Qcow-client: Image root dir %s does not exist "
-                               "in the server or is not accessible")
-                self.log.exception(ex)
-                self.client.close()
-                raise
-
-    def promote(self, candidate_hash, target_label, **kwargs):
-        """
-        This method promotes images contained inside a dir in the server
-        whose name is equal to the dlrn_id specified by creating a
-        symlink to it named as the target_label.
-        It currently uses an external Bash script for the effective promotion
-        :param candidate_hash:  The hash object to select images dir
-        :param target_label: The name of the symlink
-        :return: None
-        """
-        log_header = "Qcow promote '{}' to {}:".format(candidate_hash,
-                                                       target_label)
-        try:
-            self.log.info("%s Attempting promotion", log_header)
-            # The script doesn't really use commit/distro or full hash,
-            # it just needs the hash to identify the dir, so it works with
-            # either dlrnhash or aggregated hash.
-            cmd = ['bash',
-                   self.promote_script,
-                   '--distro', self.distro_name,
-                   '--distro-version', self.distro_version,
-                   self.config.release,
-                   candidate_hash.full_hash,
-                   target_label
-                   ]
-            qcow_logs = subprocess.check_output(cmd, stderr=subprocess.STDOUT)
-            qcow_logs_lines = qcow_logs.decode("UTF-8").split("\n")
-            self.log.info("%s Successful promotion", log_header)
-            self.log.info("%s Successful promotion "
-                          "start logs -----------------------------",
-                          log_header)
-            for line in qcow_logs_lines:
-                self.log.info(line)
-            self.log.info("%s Successful promotion "
-                          "end logs -----------------------------", log_header)
-        except subprocess.CalledProcessError as ex:
-            self.log.error("%s Failed promotion", log_header)
-            self.log.error("%s Failed promotion start "
-                           "logs -----------------------------", log_header)
-            for line in ex.output.decode("UTF-8").split("\n"):
-                self.log.error(line)
-            self.log.exception(ex)
-            self.log.error("%s Failed promotion end "
-                           "logs -----------------------------", log_header)
-            raise PromotionError("Failed to promote overcloud images")
-
-# ###-------- The code that follows is experimental and untested
+        self.client = QcowConnectionClient(server_conf)
+        self.images_dir = os.path.join(self.root, config.distro,
+                                       config.release, "rdo_trunk")
 
     def validate_qcows(self, dlrn_hash, name=None, assume_valid=False):
         """
@@ -120,48 +89,58 @@ class QcowClient(object):
         :param dlrn_hash: The hash to check
         :param name: The promotion name
         :param assume_valid: report everything worked unconditionally
-        :return:
+        :return: A dict with result of the validation
         """
+
+        try:
+            self.client.listdir(self.images_dir)
+            self.client.chdir(self.images_dir)
+        except EnvironmentError as ex:
+            self.log.error("Qcow-client: Image root dir %s does not exist "
+                           "in the server, or is not accessible")
+            self.log.exception(ex)
+            raise
 
         results = {
             "hash_valid": False,
             "promotion_valid": False,
             "qcow_valid": False,
-            "missing_qcows": copy.copy(self.config.qcow_images),
+            "missing_qcows": copy.copy(
+                self.config.overcloud_images['qcow_images']),
             "present_qcows": [],
         }
-        if assume_valid:
-            results = {
-                "hash_valid": True,
-                "promotion_valid": True,
-                "qcow_valid": True,
-                "present_qcows": copy.copy(self.config.qcow_images),
-                "missing_qcows": [],
-            }
-            return results
 
+        stat = None
+        images = None
+        images_path = os.path.join(self.images_dir, dlrn_hash.full_hash)
         try:
-            images_path = os.path.join(self.images_dir, dlrn_hash.full_hash)
             stat = self.client.stat(images_path)
-            if stat:
-                pass
-            results['hash_valid'] = True
             images = sorted(self.client.listdir(images_path))
+        except EnvironmentError:
+            self.log.error("Images path for hash %s not present or "
+                           "accessible", dlrn_hash)
+
+        if not images:
+            self.log.error("No images found")
+
+        if stat and images:
+            results['hash_valid'] = True
             results['present_qcows'] = images
             results['missing_qcows'] = \
-                list(set(self.config.qcow_images).difference(images))
-            if images == self.config.qcow_images:
+                list(set(self.config.overcloud_images[
+                             'qcow_images']).difference(
+                    images))
+            if images == self.config.overcloud_images['qcow_images']:
                 results['qcow_valid'] = True
-        except IOError:
-            pass
 
         if name is not None:
             try:
                 link = self.client.readlink(name)
                 if link == dlrn_hash.full_hash:
                     results['promotion_valid'] = True
-            except IOError:
-                pass
+            except EnvironmentError:
+                self.log.error("%s was not promoted to %s",
+                               dlrn_hash.full_hash, name)
 
         return results
 
@@ -180,29 +159,46 @@ class QcowClient(object):
             self.client.symlink(target, name)
             self.rollback_links = {}
 
-    def promote_experimental(self, candidate_hash, target_label,
-                             candidate_label=None,
-                             create_previous=True):
-        log_header = "qcow promote '{}' to {}:".format(candidate_hash,
+    def promote(self, candidate_hash, target_label, candidate_label=None,
+                create_previous=True, validation=True):
+        """
+        Effective promotion of the images. This method will handle symbolic
+        links to the dir containing images from thje candidate hash,
+        optionally saving the current link as previous
+        :param candidate_hash: The dlrn hash to promote
+        :param target_label: The name of the link to create
+        :param candidate_label: Currently unused
+        :param create_previous: A bool to determine if previous link is created
+        :param validation: A bool to determine if qcow validation should be done
+        :return: None
+        """
+
+        self.client.connect()
+        self.client.chdir(self.images_dir)
+
+        if validation:
+            self.validate_qcows(candidate_hash)
+
+        log_header = "Qcow promote '{}' to {}:".format(candidate_hash,
                                                        target_label)
         self.log.info("%s Attempting promotion", log_header)
 
         # Check if candidate_hash dir is present
         try:
             self.client.stat(candidate_hash.full_hash)
-        except IOError as ex:
+        except EnvironmentError as ex:
             self.log.error("%s images dir for hash %s not present or not "
-                           "accessible", log_header,
-                           candidate_hash)
+                           "accessible", log_header, candidate_hash)
             self.log.exception(ex)
             self.client.close()
-            raise
+            raise PromotionError("{} No images dir for hash {}"
+                                 "".format(log_header, candidate_hash))
 
         # Check if the target label exists and points to a hash dir
         current_hash = None
         try:
             current_hash = self.client.readlink(target_label)
-        except IOError:
+        except EnvironmentError:
             self.log.debug("%s No link named %s exists", log_header,
                            target_label)
 
@@ -211,7 +207,7 @@ class QcowClient(object):
             self.rollback_links['target_label'] = current_hash
             try:
                 self.client.remove(target_label)
-            except IOError as ex:
+            except EnvironmentError as ex:
                 self.log.debug("Unable to remove the target_label: %s",
                                target_label)
                 self.log.exception(ex)
@@ -223,18 +219,18 @@ class QcowClient(object):
         previous_hash = None
         try:
             previous_hash = self.client.readlink(previous_label)
-        except IOError:
+        except EnvironmentError:
             self.log.debug("%s No previous-link named %s exists",
                            log_header,
                            previous_label)
-
+        self.log.debug("Previous hash %s", previous_hash)
         # If it exists and we are handling it, check if we can remove and
         # reassign it
-        if previous_hash and create_previous:
+        if current_hash and previous_hash and create_previous:
             self.rollback_links[previous_label] = previous_hash
             try:
                 self.client.remove(previous_label)
-            except IOError as ex:
+            except EnvironmentError as ex:
                 self.log.debug("Unable to remove the target_label: %s",
                                target_label)
                 self.log.exception(ex)
@@ -245,7 +241,7 @@ class QcowClient(object):
                 raise
             try:
                 self.client.symlink(current_hash, previous_label)
-            except IOError as ex:
+            except EnvironmentError as ex:
                 self.log.error("%s failed to link %s to %s", log_header,
                                previous_label, current_hash)
                 self.log.exception(ex)
@@ -258,7 +254,7 @@ class QcowClient(object):
         # Finally the effective promotion
         try:
             self.client.symlink(candidate_hash.full_hash, target_label)
-        except IOError as ex:
+        except EnvironmentError as ex:
             self.log.error("%s failed to link %s to %s", log_header,
                            target_label, candidate_hash.full_hash)
             self.log.exception(ex)
@@ -269,12 +265,3 @@ class QcowClient(object):
             self.client.close()
 
         self.log.info("%s Successful promotion", log_header)
-
-    def get_previous_hash(self, promote_name):
-        try:
-            image_dir = self.client.readlink("previous" + promote_name)
-        except IOError:
-            pass
-        previous_hash = image_dir
-
-        return previous_hash
