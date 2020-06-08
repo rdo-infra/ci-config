7f2cfe20147987112b6a21d19c6f67f90c068aa2
Gabriele Cerami <gcerami@redhat.com>
2020-06-08 13:48:31 +0100

    Promoter new configuration engine: Add Core
    This is part of a series of patch to add the new configuration engine to
    the promoter.
    
    Patch 1: Add the new configuration core
    
    This patch adds only the basic piece of the new configuration, the
    core class that implements the basic strategy
    
    The engine is not wired in the flow in this patch. So nothing in the
    production flow will use the new engine until the engine is fully
    tested.
    
    Task: https://tree.taiga.io/project/tripleo-ci-board/task/1743
    Change-Id: I527f069ccf1143ccbef1659ff43ab7fbe9844f24

--- ci-scripts/dlrnapi_promoter/test_unit_fixtures.py
+++ ci-scripts/dlrnapi_promoter/test_unit_fixtures.py
@@ -14,6 +14,7 @@ except ImportError:
     from mock import Mock
 
 # Cases of ini configuration
+# TODO: remove together with legacy config
 test_ini_configurations = dict(
     not_ini='''
     I am not a ini file
