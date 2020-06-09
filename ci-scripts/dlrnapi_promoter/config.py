692d0b92d0c0ed22499946a20ae285eb52ed7bd6
Gabriele Cerami <gcerami@redhat.com>
2020-06-09 15:14:24 +0100

    Promoter new configuration engine: Add PromoterConfigFactory
    This is part of a series of patch to add the new configuration engine to
    the promoter.
    
    Patch 3: Add the PromoterConfigFactory
    
    This patch adds the factory class that will be responsible to create
    PromoterConfig instances. Will be the primary interface with the rest of
    the promoter code, will load config layers, and validate the created
    config
    
    The layers in promoter config has been rearranged, and tests refactored
    to support multiple test cases
    
    The engine is not wired in the flow in this patch. So nothing in the
    production flow will use the new engine until the engine is full tested.
    
    Task: https://tree.taiga.io/project/tripleo-ci-board/task/1743
    Change-Id: I7684ad5f706ce62a30aec19e282b133bac6ec816

--- ci-scripts/dlrnapi_promoter/config.py
+++ ci-scripts/dlrnapi_promoter/config.py
@@ -7,6 +7,8 @@ import os
 import pprint
 from collections import OrderedDict
 
+import yaml
+from common import get_root_paths, setup_logging
 from jinja2 import Template
 from jinja2.nativetypes import native_concat
 
@@ -16,6 +18,19 @@ try:
 except ImportError:
     # Python 3
     from urllib import parse as urlparse
+
+try:
+    FileNotFoundError
+except NameError:
+    FileNotFoundError = IOError
+try:
+    PermissionError
+except NameError:
+    PermissionError = IOError
+
+
+class ConfigError(Exception):
+    pass
 
 
 class ConfigCore(object):
@@ -34,7 +49,7 @@ class ConfigCore(object):
         self._log.debug("Config object configured with layers: %s",
                         ", ".join(layers_list))
 
-    def _dump(self):
+    def _dump_layers(self):
         pprint.pprint(self._layers)
 
     def _fill_layer_settings(self, layer_name, settings):
@@ -68,7 +83,8 @@ class ConfigCore(object):
             return constructor()
         except Exception as exc:
             self._log.error("The constructor for attribute %s generated an "
-                            "error, no value can be constructed")
+                            "error, no value can be constructed",
+                            attribute_name)
             self._log.exception(exc)
             raise AttributeError
 
@@ -80,6 +96,8 @@ class ConfigCore(object):
         :return: The value of the attribute or AttributeError if not found
         """
         for source_name, source in self.__getattribute__('_layers').items():
+            if not source:
+                continue
             try:
                 value = source[attribute_name]
                 self._log.debug("Getting attribute %s from layer '%s' ",
@@ -209,32 +227,40 @@ class PromoterConfig(ConfigCore):
     and filters for the complex configuration variables.
     """
 
-    def __init__(self, default_settings=None, file_settings=None,
-                 cli_settings=None, experimental_settings=None):
+    def __init__(self, global_defaults=None, environment_defaults=None,
+                 release_settings=None, cli_settings=None, extra_settings=None):
         """
         Loads the layers into the ConfigCore
 
-        :param default_settings: The default settings
-        :param file_settings: settings that come from files inside the
-        environment
+        :param environment_defaults: The default settings in the environment
+        :param global_defaults: The default settings for all environments
+        :param release_settings: settings that come from the release files
+        inside the environment
         :param cli_settings: settings that come from command lines
-        :param experimental_settings: settings that are for experimental use
+        :param extra_settings: settings that are for experimental use,
+        and are usually passed intentionally to the config
         """
-        super(PromoterConfig, self).__init__(['cli', 'file', 'default',
-                                              'experimental'])
+        super(PromoterConfig, self).__init__(['extra', 'cli', 'release',
+                                              'environment_defaults',
+                                              'global_defaults'])
+        if extra_settings is None:
+            extra_settings = {}
         if cli_settings is None:
             cli_settings = {}
-        if file_settings is None:
-            file_settings = {}
-        if default_settings is None:
-            default_settings = {}
-        if experimental_settings is None:
-            experimental_settings = {}
+        if release_settings is None:
+            release_settings = {}
+        if environment_defaults is None:
+            environment_defaults = {}
+        if global_defaults is None:
+            global_defaults = {}
+
+        self._layers["extra"] = extra_settings
         self._layers["cli"] = cli_settings
-        self._layers['file'] = file_settings
-        self._layers['default'] = default_settings
-        self._layers['experimental'] = experimental_settings
-        self._log.debug("Initialized")
+        self._layers['release'] = release_settings
+        self._layers['environment_defaults'] = environment_defaults
+        self._layers['global_defaults'] = global_defaults
+
+        self._log.debug("Initialized Promoter config layers")
 
     # Constructors
 
@@ -357,11 +383,203 @@ class PromoterConfig(ConfigCore):
         if isinstance(promotions, dict):
             self._log.debug("Promotions is a dict")
             for target_name, info in promotions.items():
-                print(type(info['criteria']))
-                if 'criteria' in info and not isinstance(info['criteria'], set):
-                    self._log.debug("Transforming criteria into a set")
-                    info['criteria'] = set(info['criteria'])
+                try:
+                    if isinstance(info['criteria'], set):
+                        self._log.debug("criteria is already a set")
+                    else:
+                        self._log.debug("Transforming criteria into a set")
+                        info['criteria'] = set(info['criteria'])
+                except KeyError:
+                    self._log.debug("No criteria info")
         else:
             self._log.debug("Promotions is not a dict")
 
         return promotions
+
+
+class PromoterConfigFactory(object):
+    """
+    This class builds a singleton object to be passed to all the other
+    functions in the workflow.
+    The base class should be only used for testing as it just performs the
+    basic loading.
+    """
+
+    log = logging.getLogger("promoter")
+
+    def __init__(self):
+        """
+        Initialize the config object loading from ini file
+        :param config_path: the path to the configuration file to load
+        :param validate: A comma separated list of checks for the config
+        file
+        """
+        # Initial log setup
+        setup_logging("promoter", logging.DEBUG)
+        self.git_root = None
+        self.script_root = None
+        self.git_root, self.script_root = get_root_paths(self.log)
+        self.log.debug("Git root %s", self.git_root)
+        self.log.debug("Script root %s", self.git_root)
+        self.rel_roots_map = {
+            "global_defaults": os.path.join(self.script_root,
+                                            "config_environments"),
+            "environment_pool": os.path.join(self.script_root,
+                                             "config_environments"),
+        }
+
+        self.global_defaults = self.load_yaml_config("global_defaults",
+                                                     "global_defaults.yaml")
+
+    def __call__(self, environment_root, release_settings_path, cli_args=None,
+                 validate="all"):
+
+        # set environment root as root for all other files
+        self.validate_path(environment_root, path_name="Environment root")
+
+        self.rel_roots_map['environment'] = \
+            self.convert_path_rel_to_abs("environments_pool", environment_root)
+
+        layers = {
+            'environment_defaults': self.load_yaml_config("environment",
+                                                          "defaults.yaml"),
+            'release_settings': self.load_yaml_config("environment",
+                                                      release_settings_path)
+        }
+        if cli_args:
+            layers['cli_settings'] = cli_args
+            try:
+                extra_settings = self.load_yaml_config(None,
+                                                       cli_args.extra_settings)
+                layers['extra_settings'] = extra_settings
+            except AttributeError:
+                pass
+
+        # importing custom config class is not implemented yet
+
+        config = PromoterConfig(**layers)
+
+        if not self.validate(config, checks=validate):
+            self.log.error("Error in configuration")
+            raise ConfigError
+
+        return config
+
+    def convert_path_rel_to_abs(self, default_root_name, config_path):
+        """
+        Convert relative paths to absolute path, with root according to
+        the relative map.
+        if None is relative the the current working directory
+        # The path is either absolute ot it's relative to the config
+        # environment root
+        :return:
+        """
+        if not config_path:
+            return config_path
+
+        if os.path.isabs(config_path):
+            self.log.debug("%s is an absolute path, not converting")
+            return config_path
+
+        _config_path = config_path
+        default_root = self.rel_roots_map[default_root_name]
+        config_path = os.path.join(default_root, config_path)
+        self.log.debug("Converted %s to %s", _config_path, config_path)
+        return config_path
+
+    def validate_path(self, path, path_name='Path'):
+        self.log.debug("%s: %s", path_name, path)
+        if not path:
+            self.log.error("%s can't be empty", path_name)
+            raise ConfigError
+        try:
+            os.stat(path)
+        except OSError:
+            self.log.error("%s %s not found", path_name, path)
+            raise
+
+    def load_yaml_config(self, default_root_name, config_path):
+        """
+        Loads configuration from a yaml file.
+        :param config_path: the path to the config file
+        :return: a dict with the configuration
+        """
+        config_path = self.convert_path_rel_to_abs(default_root_name,
+                                                   config_path)
+        self.validate_path(config_path)
+        self.log.debug("Using config file %s", config_path)
+        with open(config_path) as config_file:
+            try:
+                config = yaml.safe_load(config_file)
+            except yaml.YAMLError as exc:
+                self.log.error("Not a valid yaml: %s", config_path)
+                self.log.exception(exc)
+                raise ConfigError
+        if not isinstance(config, dict):
+            self.log.error("Config file %s does not contain valid data",
+                           config_path)
+            raise ConfigError
+
+        return config
+
+    def validate(self, config, checks="all"):
+        """
+        :param config: A PromoterConfig instance to check
+        :param checks: a comma separated list of checks to perform
+        :return: A boolean, True if the validation was successful,
+        false otherwise
+        """
+        if not checks:
+            return True
+
+        validation_errors = []
+        if checks == "all":
+            checks = ["logs", "password", "promotions"]
+
+        if 'logs' in checks:
+            try:
+                with open(config.log_file, "w"):
+                    pass
+            except (FileNotFoundError, PermissionError):
+                validation_errors.append("Invalid log file "
+                                         "{}".format(config.log_file))
+            try:
+                getattr(logging, config.log_level)
+            except AttributeError:
+                validation_errors.append("Unrecognized log level: {}"
+                                         "".format(config['log_level']))
+
+        if "promotions" in checks:
+            try:
+                promotions = config.promotions
+                if not promotions:
+                    validation_errors.append("Empty promotions section")
+                else:
+                    for target_name, info in promotions.items():
+                        if 'criteria' not in info:
+                            validation_errors.append(
+                                "Missing criteria for target "
+                                "{}".format(target_name))
+                        if 'criteria' in info and not info['criteria']:
+                            validation_errors.append(
+                                "Empty criteria for target {}"
+                                "".format(target_name))
+                        if 'candidate_label' not in info:
+                            validation_errors.append(
+                                "Missing candidate label for "
+                                "target {}"
+                                "".format(target_name))
+
+            except AttributeError:
+                validation_errors.append("Missing promotions section")
+
+        if "password" in checks:
+            if config.dlrnauth_password is None:
+                validation_errors.append("No dlrnapi password found in env")
+
+        if validation_errors:
+            self.log.error("Validation Error: %s", ", ".join(validation_errors))
+            return False
+
+        self.log.info("Validation Successful")
+        return True
