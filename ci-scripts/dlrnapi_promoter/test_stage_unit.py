8705a4707dbd6f6779e4355640482d80b626301d
Gabriele Cerami <gcerami@redhat.com>
2020-06-09 15:19:19 +0100

    Promoter new configuration engine: stage environment
    This is part of a series of patch to add the new configuration
    engine to
    the promoter.
    
    Patch 4: Move staging environment to use the new engine
    
    This patch refactor the staging environment configuration to
    use the new configuration engine and classes.
    Other adjustments are made to the rest of the staging code
    
    The engine is not wired in the flow in this patch. So nothing in the
    production flow will use the new engine until the engine is fully
    tested.
    
    Change-Id: I4e0957d3bb135e5d11471592bb0a6c4651135634
    Task: https://tree.taiga.io/project/tripleo-ci-board/task/1743

--- ci-scripts/dlrnapi_promoter/test_stage_unit.py
+++ ci-scripts/dlrnapi_promoter/test_stage_unit.py
@@ -1,33 +1,41 @@
 import unittest
 
 import pytest
-from stage import StageConfig, parse_args
+from stage import parse_args
 
 
 class TestMain(unittest.TestCase):
 
-    def test_parse_args(self):
-        line = ("--scenes dlrn,registries --dry-run --promoter-user prom"
-                " --stage-config-file config.yaml --db-data-file fix.yaml"
-                " setup")
-        args = parse_args(cmd_line=line)
-        assert args.action == "setup"
+    def setUp(self):
+        self.line = ("--scenes dlrn,registries --dry-run --promoter-user prom"
+                     " --db-data-file fix.yaml"
+                     " setup --release-config release.yaml")
+        self.defaults = {
+            'scenes': '',
+            'stage_info_file': '',
+            'db_data_file': '',
+        }
+
+    def test_parse_args_setup(self):
+        args = parse_args(self.defaults, cmd_line=self.line)
+        assert 'setup' in args.handler.__repr__()
         assert args.dry_run is True
         assert args.promoter_user == "prom"
         assert args.db_data_file == 'fix.yaml'
-        assert args.stage_config_file == 'config.yaml'
-        assert args.scenes == ['dlrn', 'registries']
+        assert args.scenes.split(',') == ['dlrn', 'registries']
+
+    def test_arg_parse_teardown(self):
         line = "teardown"
-        args = parse_args(cmd_line=line)
-        assert args.action == "teardown"
+        args = parse_args(self.defaults, cmd_line=line)
+        assert 'teardown' in args.handler.__repr__()
         assert args.dry_run is False
-        assert args.promoter_user == StageConfig.defaults.promoter_user
-        assert args.db_data_file == StageConfig.defaults.db_data_file
-        assert args.stage_config_file == StageConfig.defaults.stage_config_file
-        assert args.scenes == StageConfig.defaults.scenes
+        assert args.db_data_file == ''
+        assert args.scenes.split(',') == ['']
+
+    def test_arg_parse_fail(self):
         line = "--scenes dlrn"
         with self.assertRaises(SystemExit):
-            parse_args(cmd_line=line)
+            parse_args(self.defaults, cmd_line=line)
 
     @pytest.mark.xfail(reason="Not Implemented", run=False)
     def test_main_setup(self):
