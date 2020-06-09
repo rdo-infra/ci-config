af50a31b70dc635b9b739a751a391e0015a6ad92
Gabriele Cerami <gcerami@redhat.com>
2020-06-09 15:19:19 +0100

    Promoter new configuration engine: stage environment
    This is part of a series of patch to add the new configuration
    engine to
    the promoter.
    
    Patch 4: Move staging environment to use the new engine
    
    This patch refactor the staging environment configuration to
    use the new configuration engine and classes.
    Other adjustments are made to the rest of the staging code
    
    The engine is not wired in the flow in this patch. So nothing in the
    production flow will use the new engine until the engine is fully
    tested.
    
    Change-Id: I4e0957d3bb135e5d11471592bb0a6c4651135634
    Task: https://tree.taiga.io/project/tripleo-ci-board/task/1743
    Signed-off-by: Amol Kahat <amolkahat@gmail.com>

--- ci-scripts/dlrnapi_promoter/test_unit_fixtures.py
+++ ci-scripts/dlrnapi_promoter/test_unit_fixtures.py
@@ -13,6 +13,49 @@ except ImportError:
     # Python2 imports
     from mock import Mock
 
+
+# Passwordles ssh key
+SSH_CONTENT = """
+-----BEGIN OPENSSH PRIVATE KEY-----
+b3BlbnNzaC1rZXktdjEAAAAABG5vbmUAAAAEbm9uZQAAAAAAAAABAAABlwAAAAdzc2gtcn
+NhAAAAAwEAAQAAAYEAqiM6/iL7ZWYVHIU5ww41NmIQgMi5AJ4+0rCO7tWahc8/cdsU3plO
+QDzfBMmfPN0eq43SYIZSlUNQL1ivzIredAOUcfo7h3TSUqrHTehPiiJvaxiBsj4UBrzXRX
+uj9PRTkfKiHAhUj/NXPlg99gID49RlY8qGmOW6Bxb9ftrAlCD+sBdZNIRhCtlmrxkEHJNc
+dXZwG76p2W+Fe9vGXgISAmT3lmX96QGl4KIIP2PxPh3qyD1kz22+LZxwbDEOpYZE3YQvhY
+C/gj2YC9N14QrvNiAuag5GRUxN3F2TGKle2HLUeyTCSS2cHAdN+FaCSPs3cd+9+LubXXV4
+7YjtuzVEsSI4xO1KTJbv/gYmk1sIF+UCszx7lB05xP5z7ZHrbjknpr/eOQVN2atYox2llK
+34C7T5bF3+xqIqOXA6Yz+XxTLMdxn3RbcsIc6QeyDzQCVjAWWXgJa/fRrUomUD+0p/4bMP
+eIr5r8cQJfdZuR6tUOE5IdomEjpYq6rAW/NguIyhAAAFiClrdbkpa3W5AAAAB3NzaC1yc2
+EAAAGBAKojOv4i+2VmFRyFOcMONTZiEIDIuQCePtKwju7VmoXPP3HbFN6ZTkA83wTJnzzd
+HquN0mCGUpVDUC9Yr8yK3nQDlHH6O4d00lKqx03oT4oib2sYgbI+FAa810V7o/T0U5Hyoh
+wIVI/zVz5YPfYCA+PUZWPKhpjlugcW/X7awJQg/rAXWTSEYQrZZq8ZBByTXHV2cBu+qdlv
+hXvbxl4CEgJk95Zl/ekBpeCiCD9j8T4d6sg9ZM9tvi2ccGwxDqWGRN2EL4WAv4I9mAvTde
+EK7zYgLmoORkVMTdxdkxipXthy1HskwkktnBwHTfhWgkj7N3Hfvfi7m111eO2I7bs1RLEi
+OMTtSkyW7/4GJpNbCBflArM8e5QdOcT+c+2R6245J6a/3jkFTdmrWKMdpZSt+Au0+Wxd/s
+aiKjlwOmM/l8UyzHcZ90W3LCHOkHsg80AlYwFll4CWv30a1KJlA/tKf+GzD3iK+a/HECX3
+WbkerVDhOSHaJhI6WKuqwFvzYLiMoQAAAAMBAAEAAAGASC+WcgkpnMYJIwarkUTP8vj8g4
+emZsq9YOskWdUrMKbUBlyrqB5ngv3QqdlZxJsUzjjoD6guFcJvnQcF38TzyUlTjGBdLYW1
+TvnCgh2U0cj2ePv220dXe9xXgdWJpP6dDolhmn82UbUvSPZro5sLR3jwY7ykCu89VJC+kT
+oDB1ZQeSoO4SdhfRbsaFI22mDzk6riugLVUbntarW+nlhGh9mK6rbvWhMm6/4TfcHLs01C
+Nh9GTHQFgpijYmQWEMi8ce2XTL3TPnd60olK4rAhixIEpVGSOsyNTJUp/CBBgmOtRWOUk4
+/eCUtG17Ue33UUjaiXeRNjCoerY+ryrcPX4wvo6etQ1UKmNIsrFDJW3WMeCEMyIF8TNJsF
+6VjA7Esv2NdH4/dPiXS7DGJV6osV3iBm4TJYt1ImwS7pLdr9oYcu3DyXT9kNfMvO/RF8n1
+/2nXGCHaWKPKcHvFsCV3Bobf7nE1wgOAQgXTReXOFmROwG6lphL/0HE3TrH1sdE1ZZAAAA
+wCW6FMTpkp7gJ3OArlC29tzYb0QVY0Pu20MCpYRCeIOkJQ9KmEIDc7vMkdXNQggoIwLhN
+tn9Drcv0OAmE1zwIAOrfYW/y70MNb5V+An63zb/hjHg+wWFYVfYzfArjF5ncWka2eMpZd+
+K9Yv9wPMR9srPSNPkYVHTEYs4uNaK3ok4srjHINOvtvspOGMwTahEZbI43E4mVehUUhScG
+/pRiTnJte0SxcBun0N7EkMfEPdQom4CpQ6uQE98YJSX6O/cwAAAMEA2rheNTq9VCMbdLl2
+ij53Iwuon4OgCG03Ubp68zFfnI1c5zooOjOihVzvbGlXyhvheEExWQ9VezldShTQBeJV6w
+ybvWZU/LbDKsFNGbxJbjkLTdi2hrXqgFnLn+mtNWLTZ2QZYrUCXCT5O6/rSsM224GnzA8I
+Eyty9+5f33UhowlEJJ8KCbiWRwfxaorxN1/V+/GE4yeLVIKKX/bYVwmitKViHiIbT6i5Eb
+ctt7VBdOSbKzHpVVQuhhvtBBV00GuLAAAAwQDHIwL9VGOxtwsEVAEuOZDPmXgdv9ANC4AA
+oA0IAVhZLlTlUFEvxDY7MdD1LVlxtRPFwcgs5R0eEkrHwvAXjdcMm/UHMZGitBSG850RIU
+kF1JdpzDAceRUVx32Zeh2Cp7SRMfbH6/vdPBa8Iqwe7mDWulDbRmJGSIjcSt4+4wLX0f39
+DCzGSxlFiui1uvwULrpJYQAne2T4brWT67S0vqovlQq0xWGDfL3Zj446mw2P2FgnvC7eic
+Z9UNKaqwqnHgMAAAAPYXJ4Y3J1ekB0b29sYm94AQIDBA==
+-----END OPENSSH PRIVATE KEY-----
+"""
+
 # Cases of ini configuration
 # TODO: remove together with legacy config
 test_ini_configurations = dict(
@@ -179,8 +222,10 @@ class LegacyConfigSetup(unittest.TestCase):
     def setUp(self):
         content = test_ini_configurations['correct']
         fp, self.filepath = tempfile.mkstemp(prefix="instance_test")
+
         with os.fdopen(fp, "w") as test_file:
             test_file.write(content)
+
         cli = "--config-file {} promote-all".format(self.filepath)
         os.environ["DLRNAPI_PASSWORD"] = "test"
         overrides = {
