45134d48194c4e6e0b3947e7e06e06a9a062988f
Gabriele Cerami <gcerami@redhat.com>
2020-06-18 12:32:08 +0100

    Promoter: enable qcow promotion rollback
    Change-Id: I9a63e2067c45d3232d8547e31b76fb2b8e8ef6bf

--- ci-scripts/dlrnapi_promoter/qcow_client.py
+++ ci-scripts/dlrnapi_promoter/qcow_client.py
@@ -235,9 +235,7 @@ class QcowClient(object):
                                target_label)
                 self.log.exception(ex)
                 self.client.close()
-                # Rollback is not tested, we enable it later, when tests are
-                # easier to add
-                # self.rollback()
+                self.rollback()
                 raise
             try:
                 self.client.symlink(current_hash, previous_label)
@@ -245,9 +243,7 @@ class QcowClient(object):
                 self.log.error("%s failed to link %s to %s", log_header,
                                previous_label, current_hash)
                 self.log.exception(ex)
-                # Rollback is not tested, we enable it later, when tests are
-                # easier to add
-                # self.rollback()
+                self.rollback()
                 self.client.close()
                 raise
 
@@ -258,9 +254,7 @@ class QcowClient(object):
             self.log.error("%s failed to link %s to %s", log_header,
                            target_label, candidate_hash.full_hash)
             self.log.exception(ex)
-            # Rollback is not tested, we enable it later, when tests are
-            # easier to add
-            # self.rollback()
+            self.rollback()
         finally:
             self.client.close()
 
