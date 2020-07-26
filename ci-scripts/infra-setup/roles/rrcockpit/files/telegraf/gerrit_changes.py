389c961917339381998b03b4e32d8e8b9706145e
Sagi Shnaidman <sshnaidm@redhat.com>
2020-07-26 18:32:07 +0300

    Retrieve all patches for gerrit
    And display NEW only (not merged)
    Change-Id: Ic24339be597227feac4b0726939f4e813bf73f72

--- ci-scripts/infra-setup/roles/rrcockpit/files/telegraf/gerrit_changes.py
+++ ci-scripts/infra-setup/roles/rrcockpit/files/telegraf/gerrit_changes.py
@@ -41,35 +41,43 @@ def get_username(host, uid):
     return ''
 
 
-def get_gerrit_data(host, project):
-    url = (
-        "%s/changes/?q=status:open+project:%s"
-        "+-label:Workflow=-1+-label:Code-Review=-2"
-        "+-message:\"WIP\"+-message:\"DNM\"") % (
-        host, project)
-    data = requests.get(url)
-    json_raw = data.content[5:]
-    try:
-        json_data = json.loads(json_raw)
-        return json_data
-    except Exception:
-        return None
+def get_gerrit_data(host, project, pages=1):
+    total_data = []
+    for i in range(pages):
+        url = (
+            "%s/changes/?q=project:%s"
+            "+-label:Workflow=-1+-label:Code-Review=-2"
+            "+-message:\"WIP\"+-message:\"DNM\"&n=50&start=%s") % (
+            host, project, i * 50)
+        data = requests.get(url)
+        json_raw = data.content[5:]
+        try:
+            json_data = json.loads(json_raw)
+            total_data += json_data
+        except Exception:
+            return total_data
+    return total_data
 
 
 def pretty_print(patch, host, project):
     patch_id = patch['_number']
     patch_created = time_convert(patch['created'], nano=False) + "000"
     patch_updated = time_convert(patch['updated'], nano=False) + "000"
-    patch_mergeable = patch['mergeable']
-    patch_title = patch['subject']
+    patch_mergeable = patch.get('mergeable', False)
+    patch_title = patch['subject'].replace('"', '\\"')
+    patch_status = patch['status']
+    patch_merged_at = 0
+    if 'submitted' in patch:
+        patch_merged_at = time_convert(patch['submitted'], nano=False) + "000"
     patch_user = get_username(host, patch['owner']['_account_id'])
     patch_link = "<a href='%s/%s' target=_blank>%s</a>" % (
         host, patch_id, patch_title)
-    return ('patch,'
+    return (u'patch,'
             'id=%s,'
             'created=%s,'
             'updated=%s,'
             'mergeable=%s,'
+            'status=%s,'
             'owner=%s,'
             'project=%s'
             ' '
@@ -77,6 +85,8 @@ def pretty_print(patch, host, project):
             'created=%s,'
             'updated=%s,'
             'mergeable=%s,'
+            'status="%s",'
+            'merged=%s,'
             'owner="%s",'
             'subject="%s",'
             'project="%s",'
@@ -87,6 +97,7 @@ def pretty_print(patch, host, project):
                 patch_created,
                 patch_updated,
                 patch_mergeable,
+                patch_status,
                 influx_esc(patch_user),
                 project,
 
@@ -94,13 +105,15 @@ def pretty_print(patch, host, project):
                 patch_created,
                 patch_updated,
                 patch_mergeable,
+                patch_status,
+                patch_merged_at,
                 patch_user,
                 patch_title,
                 project,
                 patch_link,
                 time_convert(
                     patch['created'], nano=True)
-            ))
+            )).encode('utf-8')
 
 
 def main():
@@ -112,9 +125,11 @@ def main():
         help="(default: %(default)s)")
     parser.add_argument('--project', help='Project name in Gerrit'
                         '(including "openstack/")')
+    parser.add_argument(
+        '--pages', type=int, default=1, help='How many pages of 50 changes')
     args = parser.parse_args()
 
-    changes = get_gerrit_data(args.host, args.project)
+    changes = get_gerrit_data(args.host, args.project, args.pages)
     if changes:
         patches = [pretty_print(c, args.host, args.project) for c in changes]
         for p in patches:
