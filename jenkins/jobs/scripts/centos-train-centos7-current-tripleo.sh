8c63dd85f84591498a3dd7edf332f67bd840acf8
yatinkarel <ykarel@redhat.com>
2020-08-26 16:09:06 +0530

    Switch RDO phase1 train pipeline to CentOS8
    TripleO promotions for Train CentOS8 is ready
    and working fine[1], let's add jobs for RDO phase 1.
    
    In parallel keep running centos7 train promotions
    excluding tripleo job, tripleo switched to CentOS8 for
    train with [2].
    
    Also remove tripleo job from CentOS7 train promotion
    criteria and add missing jobs for CentOS8 train criteria.
    
    [1] https://review.rdoproject.org/zuul/builds?pipeline=openstack-periodic-integration-stable2
    [2] https://review.opendev.org/#/c/745327/
    
    Change-Id: Ia5480759ff8aaa0d1b04ca7bb9e5cc4b7f82ab2d

--- jenkins/jobs/scripts/centos-train-centos7-current-tripleo.sh
+++ jenkins/jobs/scripts/centos-train-centos7-current-tripleo.sh
@@ -1,13 +1,13 @@
 set -e
-export RDO_VERSION='centos8-train'
+export RDO_VERSION='centos-train'
 export DELOREAN_HOST='trunk-primary.rdoproject.org'
 export DELOREAN_PUBLIC_HOST='trunk.rdoproject.org'
-export DELOREAN_URL="https://$DELOREAN_PUBLIC_HOST/centos8-train/current-tripleo/delorean.repo"
+export DELOREAN_URL="https://$DELOREAN_PUBLIC_HOST/centos7-train/current-tripleo/delorean.repo"
 # The softlinks used in promotion should be cumulative. This job starts w/ current-tripleo
 # If the job passes at the rdo level, rdo is appended to the new softlink name.
 # End result would be two seperate softlinks e.g. current-tripleo, and current-tripleo-rdo
 export LINKNAME='current-tripleo-rdo'
-export LAST_PROMOTED_URL="https://$DELOREAN_PUBLIC_HOST/centos8-train/$LINKNAME/delorean.repo"
+export LAST_PROMOTED_URL="https://$DELOREAN_PUBLIC_HOST/centos7-train/$LINKNAME/delorean.repo"
 export RDO_VERSION_DIR='train'
 # The LOCATION var is handed off to the ansible-role-tripleo-image build (atrib) role to define where testing/staged images are uploaded
 export LOCATION='current-tripleo'
@@ -17,6 +17,4 @@ export BUILD_SYS='delorean'
 # When ENABLE_PUPPET_MODULES_RPM is true, puppet modules are installed from
 # rpm instead of git repo in p-o-i jobs
 export ENABLE_PUPPET_MODULES_RPM=true
-export HASH_FILE='/tmp/delorean_train_current_tripleo_hash'
-export IMAGE=template-rdo-centos8-stable
-export ANSIBLE_PYTHON_INTERPRETER=/usr/libexec/platform-python
+export HASH_FILE='/tmp/delorean_train_centos7_current_tripleo_hash'
