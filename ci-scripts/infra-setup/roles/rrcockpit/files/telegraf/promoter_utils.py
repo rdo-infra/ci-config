67588084fbe1fa19e4960922a41e1943878f8dee
Lance Bragstad <lbragstad@gmail.com>
2020-10-02 14:55:12 -0500

    Refactor imports to work with python3 in last_promotions.py
    Despite having a py37 tox environment, the last_promotions.py script
    doesn't work with python3.7 because of import differences between
    python3 and python2.
    
    Change-Id: Id41869951c2989df0461526ee1296655c7c6bf64

--- ci-scripts/infra-setup/roles/rrcockpit/files/telegraf/promoter_utils.py
+++ ci-scripts/infra-setup/roles/rrcockpit/files/telegraf/promoter_utils.py
@@ -1,8 +1,15 @@
-import ConfigParser
+import six
+
 import dlrnapi_client
 import requests
 import yaml
-from StringIO import StringIO
+
+if six.PY2:
+    import ConfigParser
+    from StringIO import StringIO
+elif six.PY3:
+    import configparser as ConfigParser
+    from io import StringIO
 
 PROMOTER_CONFIG_URL = ("https://raw.githubusercontent.com/rdo-infra/ci-config/"
                        "master/ci-scripts/dlrnapi_promoter/config/{}/{}.ini")
