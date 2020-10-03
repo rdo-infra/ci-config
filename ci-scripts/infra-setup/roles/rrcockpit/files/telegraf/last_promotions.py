e4a7f53e2d6373793a4f7e98eda57c697edef2c7
Lance Bragstad <lbragstad@gmail.com>
2020-10-02 20:24:04 -0500

    Generalize last_promotions.py script
    This commit makes the last_promotions.py script more reuseable for other
    applications by:
    
      - generalizing common data and methods
      - clarifing script parameters
      - providing usable help text
      - removing conditional imports
      - raises exceptions when request fail letting callers know something
        failed and to correct their input
      - removes None type checking
    
    Change-Id: Id5f884d371d513b707b60ba54526b2fe4333c8c4
    Signed-off-by: Wes Hayutin <weshayutin@gmail.com>

--- ci-scripts/infra-setup/roles/rrcockpit/files/telegraf/last_promotions.py
+++ ci-scripts/infra-setup/roles/rrcockpit/files/telegraf/last_promotions.py
@@ -1,20 +1,13 @@
 #!/usr/bin/env python
 
 import argparse
-
+from datetime import datetime
 import dlrnapi_client
-from influxdb_utils import format_ts_from_float, format_ts_from_last_modified
+from influxdb_utils import (format_ts_from_float,
+                           format_ts_from_last_modified,
+                           format_ts_from_date)
 
-# only run upstream or downstream promotion checks
-# do not run both.
-try:
-    from internal_promoter_utils import (get_consistent, get_dlrn_instance,
-                                         get_promoter_component_config,
-                                         get_promoter_config)
-except ImportError:
-    from promoter_utils import (get_consistent, get_dlrn_instance,
-                                get_promoter_component_config,
-                                get_promoter_config, get_url_promotion_details)
+import promoter_utils
 
 PROMOTION_INFLUXDB_LINE = ("dlrn-promotion,"
                            "release={release},distro={distro},"
@@ -28,18 +21,21 @@ PROMOTION_INFLUXDB_LINE = ("dlrn-promotion,"
                            "component=\"{component}\" "
                            "{timestamp}")
 
+DEFAULT_PROMOTER_BASE_URL = (
+    "https://raw.githubusercontent.com/rdo-infra/ci-config/master"
+)
 
 def influxdb(promotion):
     promotion['timestamp'] = format_ts_from_float(promotion['timestamp'])
     return PROMOTION_INFLUXDB_LINE.format(**promotion)
 
 
-def get_last_promotion(dlrn, release, distro, name, component=None):
+def get_last_promotion(dlrn_client, release, distro, name, component=None):
     query = dlrnapi_client.PromotionQuery()
     query.promote_name = name
     if component:
         query.component = component
-    promotions = dlrn.api_promotions_get(query)
+    promotions = dlrn_client.api_promotions_get(query)
     if promotions:
         last_promotion = promotions[0].to_dict()
         last_promotion['release'] = release
@@ -51,46 +47,77 @@ def get_last_promotion(dlrn, release, distro, name, component=None):
     return
 
 
+def get_promotion(url, release, distro, component):
+    promoter_config = promoter_utils.get_promoter_config(
+        url, release, distro, component
+    )
+
+    dlrn_client = promoter_utils.get_dlrn_client(promoter_config)
+
+    promotions = []
+    for promotion_name in promoter_config['promote_from']:
+        if component is None:
+            promo = get_last_promotion(
+                dlrn_client, release, distro, promotion_name
+            )
+            consistent = format_ts_from_last_modified(
+                promoter_utils.get_consistent(promoter_config))
+            if promo:
+                promotion_details = promoter_utils.get_url_promotion_details(
+                    promoter_config, promo)
+        else:
+            promo = get_last_promotion(
+                dlrn_client, release, distro, promotion_name, component
+            )
+            consistent = format_ts_from_last_modified(
+                promoter_utils.get_consistent(promoter_config, component)
+            )
+            promotion_details = "None"
+        # get the promotion for consistent to establish how old the
+        # promotion is.  date(promomtion) - date(consistent)
+        if promo and consistent:
+            promo.update({'consistent_date': consistent})
+        if promo and promotion_details:
+            promo.update({'promotion_details': promotion_details})
+        if promo:
+            promotions.append(influxdb(promo))
+
+    return promotions
+
+
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser(
-        description="Print release last promotions as influxdb lines")
+        description="Print release last promotions as influxdb lines",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter
+    )
 
-    parser.add_argument('--release', required=True)
-    parser.add_argument('--distro', default="CentOS-7")
-    parser.add_argument('--component', default=None)
+    parser.add_argument(
+        '--release',
+        required=True,
+        choices=[
+            'rhos-17', 'rhos-16.2', 'master', 'ussuri', 'train', 'stein',
+            'rocky', 'queens'
+        ],
+        help='Upstream or downstream release.'
+    )
+    parser.add_argument(
+        '--distro',
+        default='CentOS-7',
+        choices=['CentOS-7', 'CentOS-8', 'RedHat-8'],
+        help='Distribution to query.'
+    )
+    parser.add_argument(
+        '--component',
+        default=None,
+        help='Specific OpenStack component (e.g., compute, security).'
+    )
+    parser.add_argument(
+        '--url',
+        default=DEFAULT_PROMOTER_BASE_URL,
+        help='Promoter repository base URL.'
+    )
     args = parser.parse_args()
-
-    if args.component is None:
-        promoter_config = get_promoter_config(args.release, args.distro)
-    else:
-        promoter_config = get_promoter_component_config(
-            args.release, args.distro)
-
-    dlrn = get_dlrn_instance(promoter_config)
-    if dlrn:
-        for promotion_name in promoter_config['promote_from']:
-            if args.component is None:
-                promo = get_last_promotion(dlrn, args.release, args.distro,
-                                           promotion_name)
-                consistent = format_ts_from_last_modified(
-                    get_consistent(promoter_config))
-                if promo:
-                    promotion_details = get_url_promotion_details(
-                        promoter_config, promo)
-            else:
-                promo = get_last_promotion(dlrn, args.release, args.distro,
-                                           promotion_name, args.component)
-                consistent = format_ts_from_last_modified(
-                    get_consistent(promoter_config, args.component))
-                promotion_details = "None"
-            # get the promotion for consistent to establish how old the
-            # promotion is.  date(promomtion) - date(consistent)
-            if promo and consistent:
-                promo.update({'consistent_date': consistent})
-            if promo and promotion_details:
-                promo.update({'promotion_details': promotion_details})
-            if promo:
-                print(influxdb(promo))
-    else:
-        print('the dlrn configuration was not found')
+    promotions = get_promotion(args.url, args.release, args.distro, args.component)
+    for promotion in promotions:
+        print(promotion)
