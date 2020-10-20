f8eef9f6e71afc791ee140fe04439e47549167c9
Pooja Jadhav <pojadhav@redhat.com>
2020-10-20 22:59:14 +0530

    Move gerrit_changes script to PY3 dir
    Change-Id: Idfe6e87e08ddd637219c75032b27a9625fd496dd

---
+++ ci-scripts/infra-setup/roles/rrcockpit/files/telegraf_py3/test_gerrit_changes.py
@@ -0,0 +1,12 @@
+import gerrit_changes
+import test_data
+
+def test_gerrit_changes():
+    host = 'https://review.opendev.org'
+    project = 'openstack/puppet-tripleo'
+    pages = 1
+    expected = test_data.data
+    obtained = gerrit_changes.get_gerrit_data(host, project, pages)
+    print (obtained)
+    
+    assert (expected == obtained)
