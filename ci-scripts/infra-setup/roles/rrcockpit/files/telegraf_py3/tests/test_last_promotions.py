d8247a847d15472a729139b4dba776b998daa17f
Pooja Jadhav <pojadhav@redhat.com>
2021-01-26 19:09:53 +0530

    Adding unit test for last promotions module
    This patch adds some unit tests for last promotions
    module to avoid code breaking.
    
    Change-Id: Ib4fe02ba9c98937462fc852e822d253f43299d0e

---
+++ ci-scripts/infra-setup/roles/rrcockpit/files/telegraf_py3/tests/test_last_promotions.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# pylint: disable=C0413
+
+import json
+import os
+import sys
+import unittest
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+import last_promotions  # noqa
+
+
+class TestLastPromotions(unittest.TestCase):
+
+    def setUp(self):
+        self.url = last_promotions.DEFAULT_PROMOTER_BASE_URL
+        self.upstream_releases = [
+            'master', 'victoria', 'ussuri', 'train']
+
+    def test_get_last_promotion(self):
+        for release in self.upstream_releases:
+            args = {
+                'url': self.url,
+                'release': release,
+                'distro': 'CentOS-8',
+                'component': None
+            }
+
+            result = last_promotions.get_promotion(
+                args['url'],
+                args['release'],
+                args['distro'],
+                args['component']
+            )
+            result = json.dumps(result)
+            self.assertIn("name=current-tripleo", result, msg=None)
+            self.assertIn("name=current-tripleo-rdo", result, msg=None)
+
+    def test_get_invalid_promotion(self):
+        self.assertRaises(
+            Exception, last_promotions.get_promotion,
+            self.url, 'test_release', 'CentOS-8', None)
