00747533a9c9b43479e165d3a017a9ff40d03700
Pooja Jadhav <pojadhav@redhat.com>
2021-02-01 16:34:02 +0530

    WIP : Adding unit tests for storyboard module
    Change-Id: I5fa5f8ce782e69a0c3544a0d7a6e94a6fd6546ae

---
+++ ci-scripts/infra-setup/roles/rrcockpit/files/telegraf_py3/tests/test_storyboard.py
@@ -0,0 +1,32 @@
+import json
+import os
+import unittest
+
+from telegraf_py3 import storyboard
+
+
+class TestStoryBoard(unittest.TestCase):
+
+    def setUp(self):
+
+        full_path = os.path.dirname(os.path.abspath(__file__))
+        with open(full_path + '/data/storyboard-test-data.json') as f:
+            self.data = json.load(f)
+        self.host = 'https://storyboard.openstack.org'
+        self.project_id = 1164
+        self.story_status = 'active'
+        self.limit = 100
+
+    def test_get_storyboard_data(self):
+        result = storyboard.get_storyboard_data(
+            self.host, self.project_id, self.story_status, self.limit)
+        self.assertIsNotNone(result)
+        assert (self.data == result)
+
+    def test_invalid_storyboard_data(self):
+        self.project_id = 0000
+        self.story_status = 'inactive'
+        expected = []
+        result = storyboard.get_storyboard_data(
+            self.host, self.project_id, self.story_status, self.limit)
+        assert(expected == result)
