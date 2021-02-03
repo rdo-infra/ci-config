b09795fd6b2be174fb7a892097539c9120f6a0d3
Bhagyashri Shewale <bshewale@redhat.com>
2021-02-03 18:13:38 +0530

    Create a separate dir for container push logs
    This patch creates separate dir for container push
    logs under ~/web/container-push/
    
    Signed-off-by: Bhagyashri Shewale <bshewale@redhat.com>
    Change-Id: I7eda9c92630918da11d60192d02aa4fba4f2644e

--- ci-scripts/dlrnapi_promoter/registries_client.py
+++ ci-scripts/dlrnapi_promoter/registries_client.py
@@ -24,9 +24,13 @@ class RegistriesClient(object):
         self.config = config
         self.git_root = self.config.git_root
         self.log_root = os.path.expanduser(self.config.log_root)
+        container_push_log_dir = os.path.join(self.log_root,
+            "container-push")
+        if not os.path.exists(os.path.dirname(container_push_log_dir)):
+            os.mkdir(os.path.join(self.log_root, "container-push"))
         self.logfile = os.path.join(
             self.log_root,
-            "%s.log" % datetime.datetime.now().strftime(
+            "container-push/%s.log" % datetime.datetime.now().strftime(
                 "%Y%m%d-%H%M%S"))
         self.promote_playbook = os.path.join(self.git_root,
                                              'ci-scripts',
