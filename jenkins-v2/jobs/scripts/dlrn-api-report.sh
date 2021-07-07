c02f87d8e0de787efd83135b3dbfb773e8627ba8
Joel Capitao <jcapitao@redhat.com>
2021-07-07 17:03:29 +0200

    Add promote-master-current-tripleo job in new jenkins
    This is part of the effort [1] to move away from legacy Jenkins
    instance [2] to specific SIG Jenkins instance in OCP cluster [3]
    
    [1] https://review.rdoproject.org/etherpad/p/jenkins-migration
    [2] http://ci.centos.org/
    [3] http://console-openshift-console.apps.ocp.ci.centos.org/
    
    Change-Id: I049478e5c8835f37249c01472d43b19201f92f44

---
+++ jenkins-v2/jobs/scripts/dlrn-api-report.sh
@@ -0,0 +1,23 @@
+python3 -m venv $WORKSPACE/venv_dlrnapi
+source $WORKSPACE/venv_dlrnapi/bin/activate
+
+pip install dlrnapi_client shyaml
+
+if [[ "$delorean_current_hash" == *"_"* ]]; then
+    curl -sLo $BUILD_TAG.yaml $(echo $DELOREAN_URL | sed 's/delorean\.repo/commit.yaml/')
+    COMMIT_HASH=$(cat $BUILD_TAG.yaml| shyaml get-value commits.0.commit_hash)
+    DISTRO_HASH=$(cat $BUILD_TAG.yaml| shyaml get-value commits.0.distro_hash)
+    HASH_ARGS="--commit-hash $COMMIT_HASH --distro-hash $DISTRO_HASH"
+else
+    AGG_HASH=`curl -L ${DELOREAN_URL}.md5`
+    HASH_ARGS="--agg-hash $AGG_HASH"
+fi
+
+dlrnapi --url https://$DELOREAN_PUBLIC_HOST/api-$RDO_VERSION \
+    --username ciuser \
+    report-result \
+    --job-id $JOB_NAME \
+    ${HASH_ARGS} \
+    --timestamp $(date +%s) \
+    --info-url https://jenkins-cloudsig-ci.apps.ocp.ci.centos.org/job/$JOB_NAME/$BUILD_ID/console.txt.gz \
+    --success $JOB_SUCCESS
