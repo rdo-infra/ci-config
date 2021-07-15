27a3ecac0f12a5a2272a5a840aa8021abeaf1f7d
Joel Capitao <jcapitao@redhat.com>
2021-07-15 15:21:12 +0200

    Add remaining tripleo promotion jobs
    This patch migrates from ci.c.o, the projects
    (and sub-jobs associated) below:
    - rdo_trunk-promote-wallaby-current-tripleo
    - rdo_trunk-promote-victoria-current-tripleo
    - rdo_trunk-promote-ussuri-current-tripleo
    - rdo_trunk-promote-train-current-tripleo
    - rdo_trunk-promote-train-centos7-current-tripleo
    
    This is part of the effort [1] to move away from legacy Jenkins
    instance [2] to specific SIG Jenkins instance in OCP cluster [3]
    
    [1] https://review.rdoproject.org/etherpad/p/jenkins-migration
    [2] http://ci.centos.org/
    [3] http://console-openshift-console.apps.ocp.ci.centos.org/
    
    Change-Id: Ic69655844d5f8ee30328e9d17baddedd4dadbf91

--- jenkins-v2/jobs/scripts/centos8-victoria-current-tripleo.sh
+++ jenkins-v2/jobs/scripts/centos8-victoria-current-tripleo.sh
@@ -1,13 +1,13 @@
 set -e
-export RDO_VERSION='centos8-master-uc'
+export RDO_VERSION='centos8-victoria'
 export DELOREAN_PUBLIC_HOST='trunk.rdoproject.org'
-export DELOREAN_URL="https://$DELOREAN_PUBLIC_HOST/centos8-master/current-tripleo/delorean.repo"
+export DELOREAN_URL="https://$DELOREAN_PUBLIC_HOST/centos8-victoria/current-tripleo/delorean.repo"
 # The softlinks used in promotion should be cumulative. This job starts w/ current-tripleo
 # If the job passes at the rdo level, rdo is appended to the new softlink name.
 # End result would be two seperate softlinks e.g. current-tripleo, and current-tripleo-rdo
 export LINKNAME='current-tripleo-rdo'
-export LAST_PROMOTED_URL="https://$DELOREAN_PUBLIC_HOST/centos8-master/$LINKNAME/delorean.repo"
-export RDO_VERSION_DIR='master'
+export LAST_PROMOTED_URL="https://$DELOREAN_PUBLIC_HOST/centos8-victoria/$LINKNAME/delorean.repo"
+export RDO_VERSION_DIR='victoria'
 # The LOCATION var is handed off to the ansible-role-tripleo-image build (atrib) role to define where testing/staged images are uploaded
 export LOCATION='current-tripleo'
 # The BUILD_SYS var stores what build system was used. It becomes part of the
@@ -16,6 +16,6 @@ export BUILD_SYS='delorean'
 # When ENABLE_PUPPET_MODULES_RPM is true, puppet modules are installed from
 # rpm instead of git repo in p-o-i jobs
 export ENABLE_PUPPET_MODULES_RPM=true
-export HASH_FILE="$WORKSPACE/delorean_master_centos8_current_tripleo_hash"
+export HASH_FILE="$WORKSPACE/delorean_victoria_centos8_current_tripleo_hash"
 export CICO_OS_RELEASE=8-stream
 export ANSIBLE_PYTHON_INTERPRETER=/usr/libexec/platform-python
