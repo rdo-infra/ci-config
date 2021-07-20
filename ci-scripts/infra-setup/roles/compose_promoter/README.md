3597c8ed9d2d566e97d1e9873d7c571ff5fd8ab0
Douglas Viroel <viroel@gmail.com>
2021-07-20 17:08:38 -0300

    Add CentOS composes promoter role
    This patch adds a new role to assist on centos compose-id promotion,
    which fetches data from a remote url and create compose-id artifacts
    in the provided remote server. It includes a module that expects both
    candidate and target labels as parameters of a promotion.
    At this moment, the supported promotion is 'latest-compose' to
    'tripleo-ci-testing'.
    
    Change-Id: Iac19138dc8354d9c98fb26b7d0298db0a83ce165
    Signed-off-by: Douglas Viroel <viroel@gmail.com>

---
+++ ci-scripts/infra-setup/roles/compose_promoter/README.md
@@ -0,0 +1,29 @@
+#compose_promoter
+
+Role to promote a CentOs Compose-ID for a given candidate and target label.
+
+## Variables
+* `compose_promoter_host`: (String) Host or IP of the server to store the compose-ids. Default: '127.0.0.1'
+* `compose_promoter_port`: (String) TCP destination port to be used on SSH connection with the remote server. Default: '22'
+* `compose_promoter_user`: (String) User to be used on SSH connection with the remote server. Default: '${USER}'
+* `compose_promoter_key_path`: (String) Absolute path to the private key to be used by the SSH client. Default: '~/.ssh/id_rsa'
+* `compose_promoter_working_dir`: (String) Path to the working directory (in the remote server) where artifacts and labels should be created. Default: '${HOME}'
+* `compose_promoter_distro`: (String) Target distro used when retrieving latest Compose-ID from URL. Default: 'centos-stream-8'
+* `compose_promoter_latest_compose_url`: (String) URL used by the promoter get latest compose-id value. Default: 'https://composes.centos.org/latest-CentOS-Stream-8/COMPOSE_ID'
+* `compose_promoter_candidate_label`: (String) Candidate label of compose promotion. Default: 'latest-compose'
+* `compose_promoter_target_label`: (String) Target label of compose promotion. Default: 'tripleo-ci-testing
+
+Example:
+```yaml
+- hosts: all
+  tasks:
+    - name: Promote latest compose-id
+      import_role:
+        name: compose_promoter
+      vars:
+        compose_promoter_server: "127.0.0.1"
+        compose_promoter_key_path: "~/.ssh/id_rsa"
+        compose_promoter_latest_compose_url: "https://composes.centos.org/latest-CentOS-Stream-8/COMPOSE_ID"
+        compose_promoter_candidate_label: 'latest-compose'
+        compose_promoter_target_label: 'tripleo-ci-testing'
+```
---
+++ ci-scripts/infra-setup/roles/compose_promoter/library/compose_promoter.py
@@ -0,0 +1,167 @@
+#!/usr/bin/python
+#   Copyright 2021 Red Hat, Inc.
+#
+#   Licensed under the Apache License, Version 2.0 (the "License"); you may
+#   not use this file except in compliance with the License. You may obtain
+#   a copy of the License at
+#
+#        http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#   WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#   License for the specific language governing permissions and limitations
+#   under the License.
+
+from ansible.module_utils.basic import AnsibleModule
+from ansible.module_utils.compose_promoter import promoter
+
+DOCUMENTATION = r'''
+---
+module: compose_promoter
+
+short_description: Promote CentOS Compose-ID.
+version_added: "1.0.0"
+author: Douglas Viroel (@viroel)
+
+description:
+    - Promote CentOS Compose-ID from candidate label to target label,
+      given an destination server and working directory.
+
+options:
+    server:
+        description:
+          - Hostname or IP of the server that stores artifacts and labels.
+        required: true
+        type: str
+    port:
+        description:
+          - Destination port to be used when connection through SSH protocol.
+        type: str
+    user:
+        description:
+          - User name to be used by SSH client.
+        type: str
+    private_key_path:
+        description:
+          - Absolute path to a private key to be used by SSH client.
+        type: path
+    working_dir:
+        description:
+          - Path to the working directory where artifacts and labels should
+            be created.
+        required: true
+        type: path
+    distro:
+        description:
+          - Distro used when retrieving Compose-ID from source URL.
+        type: str
+        choices: ['centos-stream-8']
+    latest_compose_url:
+        description:
+          - Full URL to be used by the promoter to get the latest compose-id.
+        type: str
+    candidate_label:
+        description:
+          - Compose-ID candidate label of a promotion.
+        required: true
+        type: str
+        choices: ['latest-compose']
+    target_label:
+        description:
+          - Compose-ID target label of a promotion.
+        required: true
+        type: str
+        choices: ['tripleo-ci-testing']
+'''
+
+EXAMPLES = r'''
+#
+- name: Promote compose-id to tripleo-ci-testing in a remote server
+  compose_promoter:
+    server: 10.0.0.10
+    user: centos
+    distro: centos-stream-8
+    private_key_path: ~/.ssh/id_rsa
+    working_dir: /var/www/html/centos8
+    candidate_label: latest-compose
+    target_label: tripleo-ci-testing
+'''
+
+RETURN = r''' # '''
+
+
+def run_module():
+    # define available arguments/parameters a user can pass to the module
+    target_label_choices = ['tripleo-ci-testing']
+    candidate_label_choices = ['latest-compose']
+    distro_choices = ['centos-stream-8']
+    module_args = dict(
+        server=dict(type='str', required=True),
+        port=dict(type='str', default='22'),
+        user=dict(type='str'),
+        private_key_path=dict(type='str'),
+        working_dir=dict(type='str', required=True),
+        distro=dict(type='str', choices=distro_choices,
+                    default='centos-stream-8'),
+        latest_compose_url=dict(type='str'),
+        candidate_label=dict(type='str', choices=candidate_label_choices,
+                             required=True),
+        target_label=dict(type='str', choices=target_label_choices,
+                          required=True),
+    )
+
+    result = dict(
+        changed=False,
+        msg=''
+    )
+
+    module = AnsibleModule(
+        argument_spec=module_args,
+        supports_check_mode=False
+    )
+
+    try:
+        # 1. Create SFTP client
+        _port = int(module.params['port']) if module.params['port'] else None
+        sftp_client = promoter.SftpClient(
+            hostname=module.params['server'],
+            port=_port,
+            user=module.params['user'],
+            pkey_path=module.params['private_key_path']
+        )
+
+        # 2. Create promoter
+        prom_obj = promoter.ComposePromoter(
+            sftp_client,
+            working_dir=module.params['working_dir'],
+            distro=module.params['distro'],
+            compose_url=module.params['latest_compose_url'],
+        )
+
+        # 3. Promote
+        prom_obj.promote(
+            module.params['target_label'],
+            candidate_label=module.params['candidate_label'],
+        )
+    except Exception as exc:
+        result['msg'] = str(exc)
+        module.fail_json(**result)
+
+    # Successful module execution
+    result['changed'] = True
+    result['msg'] = (
+        "Successfully promoted compose-id from '%(candidate)s' to "
+        "'%(target)s'" % {
+            'candidate': module.params['candidate_label'],
+            'target': module.params['target_label']
+        })
+    module.exit_json(**result)
+
+
+def main():
+    run_module()
+
+
+if __name__ == '__main__':
+    main()
