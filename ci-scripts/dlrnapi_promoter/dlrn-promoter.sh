a1c9a27423c791f7c7c3e48c411b41838c3f0e5a
Amol Kahat <amolkahat@gmail.com>
2021-08-19 11:37:26 -0400

    Set hashes_count and logging level
    Some minor changes in patch:
    - Set hashes_count in release file to 100
    - Set hashes_count in default to 50
    - Added new LOG_LEVEL env variable in promoter_env
    - Moved IMAGE_SERVER_USER_HOST to promoter_env
    - Removed old/not supported releases.
    
    Signed-off-by: Amol Kahat <amolkahat@gmail.com>
    Change-Id: I3d5cdaa9f0b9a3f835719233ad4037ddfcdc60fb

--- ci-scripts/dlrnapi_promoter/dlrn-promoter.sh
+++ ci-scripts/dlrnapi_promoter/dlrn-promoter.sh
@@ -15,7 +15,6 @@ set -x
 
 TIMEOUT=115m
 KILLTIME=120m
-LOG_LEVEL="INFO"
 STAGING_DIR=""
 PROMOTER_CONFIG_ROOT="${PROMOTER_CONFIG_ROOT:=staging}"
 PROMOTER_TYPE="${PROMOTER_TYPE:=upstream}"
@@ -23,8 +22,7 @@ PROMOTER_TYPE="${PROMOTER_TYPE:=upstream}"
 if [[ $PROMOTER_TYPE == "upstream" ]]; then
     DEFAULT_RELEASES=( "CentOS-8/master" "CentOS-8/wallaby" \
                        "CentOS-8/victoria" "CentOS-8/ussuri" \
-                       "CentOS-8/train" "CentOS-7/stein" \
-                       "CentOS-7/queens" "CentOS-7/train" )
+                       "CentOS-8/train" )
 else
     DEFAULT_RELEASES=( "RedHat-8/rhos-16.2" "RedHat-8/rhos-17" )
 fi
@@ -42,9 +40,8 @@ while getopts "t:k:sh" arg; do
         ;;
     s)
         echo "Staging promoter mode enabled"
-        LOG_LEVEL="DEBUG"
+        LOG_LEVEL="${LOG_LEVEL}"
         STAGING_DIR="staging/"
-        export IMAGE_SERVER_USER_HOST="foo@localhost"
         ;;
     h)
         usage
