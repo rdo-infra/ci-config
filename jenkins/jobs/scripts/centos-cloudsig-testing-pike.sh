3527c111b74ecfb69d521bfab30b2df944e7d326
yatinkarel <ykarel@redhat.com>
2021-09-03 17:53:08 +0530

    Drop legacy jenkins jobs
    The required jobs are migrated[1] to new SIG specific
    jenkins instance[2]. This patch cleans up all these
    legacy jenkins jobs and tox-jjb zuul job.
    
    Job updates are being stopped with[3].
    
    [1] https://review.rdoproject.org/r/q/topic:jenkins-v2
    [2] https://jenkins-cloudsig-ci.apps.ocp.ci.centos.org
    [3] https://review.rdoproject.org/r/c/config/+/35291
    
    Change-Id: I646b7b77ffcd0edf766d038bba5f5c09b0890164

--- jenkins/jobs/scripts/centos-cloudsig-testing-pike.sh
+++
@@ -1,9 +0,0 @@
-set -e
-export RDO_VERSION_DIR='pike'
-# The LOCATION var stores what repo symlink was used. It becomes part of the
-# path where images are stored.
-export LOCATION='cloudsig-testing'
-# The BUILD_SYS var stores what build system was used. It becomes part of the
-# path where images are stored.
-export BUILD_SYS='cbs'
-export HASH_FILE='/tmp/cloudsig_testing_pike_hash'
