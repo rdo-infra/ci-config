1370fbbeed49dfefb05b1aa8775cd670211a62f8
yatinkarel <ykarel@redhat.com>
2021-09-03 17:53:08 +0530

    Drop legacy jenkins jobs
    The required jobs are migrated[1] to new SIG specific
    jenkins instance[2]. This patch cleans up all these
    legacy jenkins jobs and tox-jjb zuul job.
    
    Job updates are being stopped with[3].
    
    [1] https://review.rdoproject.org/r/q/topic:jenkins-v2
    [2] https://jenkins-cloudsig-ci.apps.ocp.ci.centos.org
    [3] https://review.rdoproject.org/r/c/config/+/35291
    
    Change-Id: I646b7b77ffcd0edf766d038bba5f5c09b0890164

--- jenkins/jobs/scripts/centos-cloudsig-stable-ocata.sh
+++
@@ -1,9 +0,0 @@
-set -e
-export RDO_VERSION_DIR='ocata'
-# The LOCATION var stores what repo symlink was used. It becomes part of the
-# path where images are stored.
-export LOCATION='cloudsig-stable'
-# The BUILD_SYS var stores what build system was used. It becomes part of the
-# path where images are stored.
-export BUILD_SYS='cbs'
-export HASH_FILE='/tmp/cloudsig_stable_ocata_hash'
--- jenkins/jobs/scripts/cico-node-done-from-ansible.sh
+++
@@ -1,7 +0,0 @@
-# cico-node-done-from-ansible.sh
-# A script that releases nodes from a SSID file written by
-SSID_FILE=${SSID_FILE:-$WORKSPACE/cico-ssid}
-
-for ssid in $(cat ${SSID_FILE}); do
-    cico -q node done $ssid
-done
