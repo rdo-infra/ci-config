898074a5277b16771cefc271740f8cebac17e95a
yatinkarel <ykarel@redhat.com>
2021-09-03 17:53:08 +0530

    Drop legacy jenkins jobs
    The required jobs are migrated[1] to new SIG specific
    jenkins instance[2]. This patch cleans up all these
    legacy jenkins jobs and tox-jjb zuul job.
    
    Job updates are being stopped with[3].
    
    [1] https://review.rdoproject.org/r/q/topic:jenkins-v2
    [2] https://jenkins-cloudsig-ci.apps.ocp.ci.centos.org
    [3] https://review.rdoproject.org/r/c/config/+/35291
    
    Change-Id: I646b7b77ffcd0edf766d038bba5f5c09b0890164

--- jenkins/jobs/scripts/promote-repo-promote.sh
+++
@@ -1,6 +0,0 @@
-export PROMOTE_HASH=`echo $delorean_current_hash | awk -F '/' '{ print $3}'`
-
-# Promote on the internal server and on the public, passive, one as well
-ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i ~/.ssh/rhos-ci -p 3300 promoter@$DELOREAN_HOST "sudo /usr/local/bin/promote.sh $PROMOTE_HASH" $RDO_VERSION $LINKNAME
-ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i ~/.ssh/rhos-ci -p 3300 promoter@$DELOREAN_PUBLIC_HOST "sudo /usr/local/bin/promote.sh $PROMOTE_HASH" $RDO_VERSION $LINKNAME
-curl --max-time 60 http://rdo-feeder.distributed-ci.io/?RDO_VERSION=$RDO_VERSION || true
