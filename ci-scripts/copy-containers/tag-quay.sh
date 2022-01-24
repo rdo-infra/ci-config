da291c416ebae706a2879bd7959a9579653aa6a0
Arx Cruz <arxcruz@redhat.com>
2022-01-24 13:56:12 +0100

    Update copy-quay to use yaml file as config
    With the increase number of releases and distros, it's being hard to
    always need to update and build the copy quay to add new dlrn apis and
    releases. This patch make it possible to load new entries just adding it
    on the yaml file, without the need to update the binary
    
    Change-Id: Ica9bd3d360970bd0ed4d787673363636dfc50c79

--- ci-scripts/copy-containers/tag-quay.sh
+++ ci-scripts/copy-containers/tag-quay.sh
@@ -9,40 +9,25 @@ if [ "$?" -gt "0" ]; then
 fi
 
 # Master
-/usr/local/bin/copy-quay --token $TOKEN --from-namespace tripleomaster \
-                    --to-namespace tripleomaster tag --release master \
-                    --html /root/logs/tag/master-report.html &>>/root/logs/tag/master.txt
+/usr/local/bin/copy-quay --config /root/copy-quay/config.yaml --token $TOKEN --release master --html /root/logs/tag/master-report.html tag &>>/root/logs/tag/master.txt
+
+# Master centos 9
+/usr/local/bin/copy-quay --config /root/copy-quay/config.yaml --token $TOKEN --release mastercentos9 --html /root/logs/tag/master-centos9-report.html tag &>>/root/logs/tag/mastercentos9.txt
 
 # Wallaby
+/usr/local/bin/copy-quay --config /root/copy-quay/config.yaml --token $TOKEN --release wallaby --html /root/logs/tag/wallaby-report.html tag &>>/root/logs/tag/wallaby.txt
 
-/usr/local/bin/copy-quay --token $TOKEN --from-namespace tripleowallaby \
-                    --to-namespace tripleowallaby tag --release wallaby \
-                    --html /root/logs/tag/wallaby-report.html &>>/root/logs/tag/wallaby.txt
+# Wallaby
+/usr/local/bin/copy-quay --config /root/copy-quay/config.yaml --token $TOKEN --release wallabycentos9 --html /root/logs/tag/wallaby-centos9-report.html tag &>>/root/logs/tag/wallabycentos9.txt
+
 # Victoria
-/usr/local/bin/copy-quay --token $TOKEN --from-namespace tripleovictoria \
-                    --to-namespace tripleovictoria tag --release victoria \
-                    --html /root/logs/tag/victoria-report.html &>>/root/logs/tag/victoria.txt
+/usr/local/bin/copy-quay --config /root/copy-quay/config.yaml --token $TOKEN --release victoria --html /root/logs/tag/victoria-report.html tag &>>/root/logs/tag/victoria.txt
 
 # Ussuri
-/usr/local/bin/copy-quay --token $TOKEN --from-namespace tripleoussuri \
-                    --to-namespace tripleoussuri tag --release ussuri \
-                    --html /root/logs/tag/ussuri-report.html &>>/root/logs/tag/ussuri.txt
+/usr/local/bin/copy-quay --config /root/copy-quay/config.yaml --token $TOKEN --release ussuri --html /root/logs/tag/ussuri-report.html tag &>>/root/logs/tag/ussuri.txt
+
 # Train 7
-/usr/local/bin/copy-quay --token $TOKEN --from-namespace tripleotrain \
-                    --to-namespace tripleotrain tag --release train \
-                    --html /root/logs/tag/train-report.html &>>/root/logs/tag/train.txt
+/usr/local/bin/copy-quay --config /root/copy-quay/config.yaml --token $TOKEN --release train7 --html /root/logs/tag/train-report.html tag &>>/root/logs/tag/train.txt
 
 # Train 8
-/usr/local/bin/copy-quay --token $TOKEN --from-namespace tripleotraincentos8 \
-                    --to-namespace tripleotraincentos8 tag --release train8 \
-                    --html /root/logs/tag/train8-report.html &>>/root/logs/tag/train8.txt
-
-# Stein
-/usr/local/bin/copy-quay --token $TOKEN --from-namespace tripleostein \
-                    --to-namespace tripleostein tag --release stein \
-                    --html /root/logs/tag/stein-report.html &>>/root/logs/tag/stein.txt
-
-# Rocky
-/usr/local/bin/copy-quay --token $TOKEN --from-namespace tripleorocky \
-                    --to-namespace tripleorocky tag --release \
-                    --html /root/logs/tag/rocky-report.html &>>/root/logs/tag/rocky.txt
+/usr/local/bin/copy-quay --config /root/copy-quay/config.yaml --token $TOKEN --release train8 --html /root/logs/tag/train8-report.html tag &>>/root/logs/tag/train8.txt
--- ci-scripts/infra-setup/roles/copy_container/files/copy-quay/tag.go
+++ ci-scripts/infra-setup/roles/copy_container/files/copy-quay/tag.go
@@ -1,10 +1,10 @@
 package main
 
 import (
-	"fmt"
+    "fmt"
 
-	"github.com/sirupsen/logrus"
-	"github.com/spf13/cobra"
+    "github.com/sirupsen/logrus"
+    "github.com/spf13/cobra"
 )
 
 type tagOptions struct {
@@ -27,26 +27,16 @@ func tagCmd(global *globalOptions) *cobra.Command {
     }
     cmd.Flags().StringVar(&optsTag.tag, "tag", "current-tripleo", "Image tag name")
     cmd.Flags().StringVar(&optsTag.hash, "force-hash", "", "Force an specific hash, overwriting delorean api")
-	cmd.Flags().StringVar(&optsTag.htmlOutput, "html", "", "HTML output report file")
+    cmd.Flags().StringVar(&optsTag.htmlOutput, "html", "", "HTML output report file")
     return cmd
 }
 
 func (opts *tagOptions) run(cmd *cobra.Command, args []string) error {
 
-    var urlReleases = map[string]string {
-        "master":    "https://trunk.rdoproject.org/api-centos8-master-uc",
-        "queens":    "https://trunk.rdoproject.org/api-centos-queens",
-        "rocky":     "https://trunk.rdoproject.org/api-centos-rocky",
-        "stein":     "https://trunk.rdoproject.org/api-centos-stein",
-        "train":     "https://trunk.rdoproject.org/api-centos-train",
-        "train8":    "https://trunk.rdoproject.org/api-centos8-train",
-        "ussuri":    "https://trunk.rdoproject.org/api-centos8-ussuri",
-        "victoria":  "https://trunk.rdoproject.org/api-centos8-victoria",
-        "wallaby":   "https://trunk.rdoproject.org/api-centos8-wallaby",
-    }
-    urlApi := urlReleases[opts.global.release]
+    var urlApi = opts.global.Entry.Api
+
     if urlApi == "" {
-        return fmt.Errorf("Invalid release. Valid values are: master, queens, rocky, stein, train, train8, ussuri and victoria")
+        return fmt.Errorf("invalid release")
     }
 
     var promoted_hash = ""
@@ -68,17 +58,13 @@ func (opts *tagOptions) run(cmd *cobra.Command, args []string) error {
             }
         }
     } else {
-        var job = opts.global.job
-        if opts.global.job == "" {
-            job = getJobPerRelease(opts.global.release)
-        }
-        logrus.Infoln("Job: ", job)
-        image := getLatestGoodBuildURL(job, opts.global)
+        image := getLatestGoodBuildURL(opts.global.job, opts.global)
         data := fetchLogs(image)
         res := parseLog(data)
 
-		failed_tag := make([]string, 0)
-		success_tag := make([]string, 0)
+        logrus.Infof("image: %s namespace: %s promoted_hash: %s", image, opts.global.toNamespace, promoted_hash)
+        failed_tag := make([]string, 0)
+        success_tag := make([]string, 0)
         for _, res := range res {
             sha, err := getImageManifest(opts.global.toNamespace, res[0], promoted_hash)
             if err != nil {
