d0ffb88c11b328e41a05844ec6986d9b12859a35
Rafael Castillo <rcastill@redhat.com>
2022-02-10 15:24:49 -0700

    Specify DELOREAN_URL in HASH_FILE
    Before this patch, cico quickstart jobs would always report for the
    current-tripleo hash. This instead saves the URL to the correct hash in
    HASH_FILE so the hash stays the same between the get_hash jobs and the
    promotion jobs.
    
    Change-Id: Ifc0bd1d73ecb0fc00665b2f26e5960ce2a4349df

--- jenkins-v2/jobs/scripts/centos9-master-current-tripleo.sh
+++ jenkins-v2/jobs/scripts/centos9-master-current-tripleo.sh
@@ -1,12 +1,12 @@
 set -e
 export RDO_VERSION='centos9-master-uc'
 export DELOREAN_PUBLIC_HOST='trunk.rdoproject.org'
-export DELOREAN_URL="https://$DELOREAN_PUBLIC_HOST/centos9-master/current-tripleo/delorean.repo"
+export DELOREAN_REPO='centos9-master'
 # The softlinks used in promotion should be cumulative. This job starts w/ current-tripleo
 # If the job passes at the rdo level, rdo is appended to the new softlink name.
 # End result would be two seperate softlinks e.g. current-tripleo, and current-tripleo-rdo
-export LINKNAME='current-tripleo-rdo'
-export LAST_PROMOTED_URL="https://$DELOREAN_PUBLIC_HOST/centos9-master/$LINKNAME/delorean.repo"
+export CANDIDATE_LINKNAME='current-tripleo'
+export PROMOTED_LINKNAME='current-tripleo-rdo'
 export RDO_VERSION_DIR='master'
 # The LOCATION var is handed off to the ansible-role-tripleo-image build (atrib) role to define where testing/staged images are uploaded
 export LOCATION='current-tripleo'
