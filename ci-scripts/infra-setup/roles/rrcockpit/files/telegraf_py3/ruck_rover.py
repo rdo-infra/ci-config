e6d0c0b44d77db982e55ecc277badcb977a2508d
Ronelle Landy <rlandy@redhat.com>
2022-02-22 22:24:31 +0000

    Revert "Skip SSL verify for ruck_rover.py"
    This reverts commit 379c8f054884fa83185c439bcbcc880a17d108b0.
    
    Reason for revert: Earlier patch was a workaround for timebeing,
    we are fixing the containers by binding certs from host to contianers.
    
    Also adding correct certs while making request calls.
    
    Change-Id: I0d62d605c8c3459678e184e1b81e6774f20552d8

--- ci-scripts/infra-setup/roles/rrcockpit/files/telegraf_py3/ruck_rover.py
+++ ci-scripts/infra-setup/roles/rrcockpit/files/telegraf_py3/ruck_rover.py
@@ -19,12 +19,13 @@ from jinja2 import Environment, FileSystemLoader
 from rich import print as rich_print
 from rich.console import Console
 from rich.table import Table
-from urllib3.exceptions import InsecureRequestWarning
 
 console = Console()
 
-# skip verifying SSL certificate when calling API from https server.
-dlrnapi_client.configuration.verify_ssl = False
+# Use system-provided CA bundle instead of the one installed with pip
+# in certifi.
+CERT_PATH = '/etc/pki/ca-trust/extracted/openssl/ca-bundle.trust.crt'
+dlrnapi_client.configuration.ssl_ca_cert = CERT_PATH
 
 
 def date_diff_in_seconds(dt2, dt1):
@@ -51,9 +52,7 @@ def convert_string_date_object(date_string):
 
 
 def download_file(url):
-    requests.packages.urllib3.disable_warnings(
-        category=InsecureRequestWarning)
-    response = requests.get(url, stream=True, verify=False)
+    response = requests.get(url, stream=True, verify=CERT_PATH)
     response.raise_for_status()
     file_descriptor, path = mkstemp(prefix="job-output-")
     with open(path, "wb") as file:
@@ -104,9 +103,7 @@ def find_failure_reason(url):
 
 def web_scrape(url):
     try:
-        requests.packages.urllib3.disable_warnings(
-            category=InsecureRequestWarning)
-        response = requests.get(url, verify=False)
+        response = requests.get(url, verify=CERT_PATH)
         response.raise_for_status()
     except requests.exceptions.HTTPError as err:
         raise SystemExit(err)
@@ -183,10 +180,8 @@ def get_consistent(url, component=None):
 
     if component is None:
         # integration build, use last promoted_components date
-        requests.packages.urllib3.disable_warnings(
-            category=InsecureRequestWarning)
-        response = requests.get(
-            short_url + dlrn_tag + '/delorean.repo', verify=False)
+        response = requests.get(short_url + dlrn_tag + '/delorean.repo',
+                                verify=CERT_PATH)
         if response.ok:
             cd = response.headers['Last-Modified']
             consistent_date = format_ts_from_last_modified(cd)
@@ -196,9 +191,7 @@ def get_consistent(url, component=None):
         # TO-DO normalize component and intergration config
         short_url = (short_url + '/component/'
                      + component + '/consistent/delorean.repo')
-        requests.packages.urllib3.disable_warnings(
-            category=InsecureRequestWarning)
-        response = requests.get(short_url, verify=False)
+        response = requests.get(short_url, verify=CERT_PATH)
         if response.ok:
             cd = response.headers['Last-Modified']
             consistent_date = format_ts_from_last_modified(cd)
@@ -220,9 +213,7 @@ def get_dlrn_versions_csv(base_url, component, tag):
 
 
 def get_csv(url):
-    requests.packages.urllib3.disable_warnings(
-        category=InsecureRequestWarning)
-    response = requests.get(url, verify=False)
+    response = requests.get(url, verify=CERT_PATH)
     if response.ok:
         content = response.content.decode('utf-8')
         f = StringIO(content)
