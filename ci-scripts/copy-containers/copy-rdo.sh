21c09c21903481e3a1a384cb03a1dffb87408b4d
Arx Cruz <arxcruz@redhat.com>
2022-03-10 14:59:08 +0100

    Add cron job to copy containers from quay to rdo
    This is a safeguard for when promotion takes longer than five days to
    promote, since rdo registry prune old containers every five days.
    This patch adds a cron job to copy the containers from quay.io to rdo
    registry for all supported releases.
    This also add a cleanup on the copy-quay script to delete the build
    directory in /tmp in order to avoid the toolbox disk to be full
    
    Change-Id: I75a1242136d1bf8ce8dfc0a6f65cc9f11a660527

---
+++ ci-scripts/copy-containers/copy-rdo.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+copy_quay() {
+    HASH=`curl 'https://trunk.rdoproject.org/api-$1/api/promotions?promote_name=current-tripleo&limit=1' 2>/dev/null | jq -r '.[0].aggregate_hash'`
+    /usr/local/bin/copy-quay --config /root/copy-quay/config.yaml --pull-registry quay.io --push-registry trunk.registry.rdoproject.org --token $TOKEN --push-hash $HASH --hash $HASH --release $2 copy
+}
+# Check if podman is logged
+podman login --get-login trunk.registry.rdoproject.org
+if [ "$?" -gt "0" ]; then
+    podman login trunk.registry.rdoproject.org --username $RDO_USERNAME --password $RDO_PASSWORD
+fi
+
+
+# Master Centos 9
+copy_quay centos9-master-uc mastercentos9
+
+# Wallaby Centos 8
+copy_quay centos8-wallaby wallaby
+
+# Wallaby Centos 9
+copy_quay centos9-wallaby wallabycentos9
+
+# Victoria Centos 8
+copy_quay centos8-victoria victoria
+
+# Ussuri Centos 8
+copy_quay centos8-ussuri ussuri
+
+# Train Centos 8
+copy_quay centos8-train train8
