05fd1a3b6b92f1b92a519f33db0a9fe2dd1a70f9
Arx Cruz <arxcruz@redhat.com>
2022-03-10 14:59:08 +0100

    Add cron job to copy containers from quay to rdo
    This is a safeguard for when promotion takes longer than five days to
    promote, since rdo registry prune old containers every five days.
    This patch adds a cron job to copy the containers from quay.io to rdo
    registry for all supported releases.
    This also add a cleanup on the copy-quay script to delete the build
    directory in /tmp in order to avoid the toolbox disk to be full
    
    Change-Id: I75a1242136d1bf8ce8dfc0a6f65cc9f11a660527

---
+++ ci-scripts/copy-containers/copy-rdo.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Check if podman is logged
+podman login --get-login trunk.registry.rdoproject.org
+if [ "$?" -gt "0" ]; then
+    podman login trunk.registry.rdoproject.org --username $RDO_USERNAME --password $RDO_PASSWORD
+fi
+
+# Master centos 9
+HASH=`curl 'https://trunk.rdoproject.org/api-centos9-master-uc/api/promotions?promote_name=current-tripleo&limit=1' 2>/dev/null | jq -r '.[0].aggregate_hash'`
+/usr/local/bin/copy-quay --config /root/copy-quay/config.yaml --pull-registry quay.io --push-registry trunk.registry.rdoproject.org --token $TOKEN --push-hash $HASH --hash $HASH --release mastercentos9 copy
+
+# Wallaby centos 8
+HASH=`curl 'https://trunk.rdoproject.org/api-centos8-wallaby/api/promotions?promote_name=current-tripleo&limit=1' 2>/dev/null | jq -r '.[0].aggregate_hash'`
+/usr/local/bin/copy-quay --config /root/copy-quay/config.yaml --pull-registry quay.io --push-registry trunk.registry.rdoproject.org --token $TOKEN --push-hash $HASH --hash $HASH --release wallaby copy
+
+# Wallaby centos 9
+HASH=`curl 'https://trunk.rdoproject.org/api-centos9-wallaby/api/promotions?promote_name=current-tripleo&limit=1' 2>/dev/null | jq -r '.[0].aggregate_hash'`
+/usr/local/bin/copy-quay --config /root/copy-quay/config.yaml --pull-registry quay.io --push-registry trunk.registry.rdoproject.org --token $TOKEN --push-hash $HASH --hash $HASH --release wallabycentos9 copy
+
+# Victoria
+HASH=`curl 'https://trunk.rdoproject.org/api-centos8-victoria/api/promotions?promote_name=current-tripleo&limit=1' 2>/dev/null | jq -r '.[0].aggregate_hash'`
+/usr/local/bin/copy-quay --config /root/copy-quay/config.yaml --pull-registry quay.io --push-registry trunk.registry.rdoproject.org --token $TOKEN --push-hash $HASH --hash $HASH --release victoria copy
+
+# Ussuri
+HASH=`curl 'https://trunk.rdoproject.org/api-centos8-ussuri/api/promotions?promote_name=current-tripleo&limit=1' 2>/dev/null | jq -r '.[0].aggregate_hash'`
+/usr/local/bin/copy-quay --config /root/copy-quay/config.yaml --pull-registry quay.io --push-registry trunk.registry.rdoproject.org --token $TOKEN --push-hash $HASH --hash $HASH --release ussuri copy
+
+# Train 8
+HASH=`curl 'https://trunk.rdoproject.org/api-centos8-train/api/promotions?promote_name=current-tripleo&limit=1' 2>/dev/null | jq -r '.[0].aggregate_hash'`
+/usr/local/bin/copy-quay --config /root/copy-quay/config.yaml --pull-registry quay.io --push-registry trunk.registry.rdoproject.org --token $TOKEN --push-hash $HASH --hash $HASH --release train8 copy
