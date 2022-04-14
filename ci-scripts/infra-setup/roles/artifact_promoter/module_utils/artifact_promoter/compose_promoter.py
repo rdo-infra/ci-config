a23297e6b3084719f8de0d89a2b373f7564e8e7f
Douglas Viroel <viroel@gmail.com>
2022-04-14 17:55:57 -0300

    Refactoring compose promoter role
    This patch refactors compose_promoter role to work as a generic
    file promoter that can be reused for other purposes.
    The role and module has been renamed to "artifact_promoter".
    This python module now supports two types of promotion:
    1. 'file' type promotion receives a file name and content and
    a target label. Promoter code creates artifacts in remote server
    and links to target label.
    2. 'centos-compose' for a given compose_url, the promoter will
    fetch latest-compose-id and create artifacts on remote server,
    and link to a target-label.
    Molecule job was updated to test both modes.
    
    Change-Id: Iba486585b39c06fe9dbda0625125f6e4449b5aee
    Signed-off-by: Douglas Viroel <viroel@gmail.com>

---
+++ ci-scripts/infra-setup/roles/artifact_promoter/module_utils/artifact_promoter/compose_promoter.py
@@ -0,0 +1,55 @@
+"""
+Compose promoter class
+"""
+import urllib.request
+
+from .artifact_promoter import FileArtifactPromoter, PromoterError
+
+
+class ComposePromoter(FileArtifactPromoter):
+    """
+    CentOS compose promoter class.
+    """
+
+    def __init__(self, client, working_dir, compose_url=None):
+        """Instantiate a new compose promoter.
+
+        :param client: client to be used for file operations
+        :param working_dir: working directory to perform file operations
+        :param compose_url: url used to fetch latest compose-id for an
+          specific distro.
+        """
+        super(ComposePromoter, self).__init__(client, working_dir)
+        self.compose_url = compose_url
+        # Current supported promotions for CentOS compose
+        self.supported_promotions = [
+            {'candidate': 'latest-compose', 'target': 'centos-ci-testing'},
+        ]
+
+    def retrieve_latest_compose(self):
+        """Retrieves the latest compose from centos url.
+
+        :return: String with the latest compose id.
+        """
+        try:
+            latest_compose_id = urllib.request.urlopen(
+                self.compose_url).readline().decode('utf-8')
+        except Exception:
+            msg = ("Failed to retrieve latest compose from url: %s"
+                   % self.compose_url)
+            self.log.error(msg)
+            raise PromoterError(details=msg)
+
+        self.log.info("Retrieved latest compose-id: %s", latest_compose_id)
+        return latest_compose_id
+
+    def get_promotion_content(self, target_label, candidate_label=None):
+        """Retrieves promotion file name and content for latest-compose."""
+        if candidate_label != 'latest-compose':
+            msg = ("Candidate label '%s' not supported." % candidate_label)
+            self.log.error(msg)
+            raise PromoterError(details=msg)
+
+        latest_compose_id = self.retrieve_latest_compose()
+        # File name and content are the compose_id
+        return latest_compose_id, latest_compose_id
