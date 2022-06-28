d129b813ad08f874d2aab44c6d450e88bfc76b9b
Dariusz Smigiel <dsmigiel@redhat.com>
2022-06-28 16:27:46 -0700

    Update skiplist tool to fetch all jobs
    Instead of manually providing release and job names,
    fetch all jobs from "openstack-periodic-weekend" pipeline,
    and return results at once.
    
    Changes:
    * updated URL from job to pipeline
    * updated regex to ignore "tearDownClass" keyword
    * specified constant value of 10 last test results
    * removed unused struct SkipTests
    * removed unused function loadYaml
    
    Change-Id: I97725a7c7a6373acb1fe343e3391e7e3781b7c18

--- ci-scripts/infra-setup/roles/rrcockpit/files/mariadb/skiplist.go
+++ ci-scripts/infra-setup/roles/rrcockpit/files/mariadb/skiplist.go
@@ -1,125 +1,77 @@
 package main
 
 import (
-    "encoding/csv"
-    "encoding/json"
-    "flag"
-    "fmt"
-    "io/ioutil"
-    "log"
-    "net/http"
-    "os"
-    "regexp"
-    "strconv"
-
-    "github.com/jedib0t/go-pretty/table"
-    "gopkg.in/yaml.v2"
+	"encoding/csv"
+	"encoding/json"
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"regexp"
+	"strconv"
+	"strings"
 )
 
-// SkipTests - Struct returned by the yaml file containing the skipped
-// tests
-type SkipTests struct {
-    KnownFailures []struct {
-        Test string `yaml:"test"`
-    } `yaml:"known_failures"`
-}
+const limit int = 10
 
-// ListJobs is the struct to return the list of jobs you want to check
-type ListJobs []string
-
-func (i *ListJobs) String() string {
-    return "List of jobs"
-}
-
-// Set the value parsed by the flags parser
-func (i *ListJobs) Set(value string) error {
-    *i = append(*i, value)
+type strslice string
+func (ss *strslice) UnmarshalJSON(data []byte) error {
+    var s string
+    if err := json.Unmarshal(data, &s); err != nil {
+        return err
+    }
+    // NOTE(dasm): Get last element from job_name, which
+    // represents release.
+    // Explicitly convert string to strslice (string)
+    *ss = strslice(s[strings.LastIndex(s, "-")+1:])
     return nil
 }
 
-// Builds - Basic struct returned by zuul api in json format
-// We don't need all the values, just these two,
-// Status and the URL
-type Builds struct {
+// Job - Basic struct returned by zuul api in json format
+// Release of job, Status (PASSED|FAILED|SKIPPED), URL to logs
+type Job struct {
+    Release strslice `json:"job_name"`
     Status string `json:"result"`
     URL    string `json:"log_url"`
 }
 
-// TestResult - This is the result of the test, wether is
-// failing or passing, it acts as a counter
-type TestResult struct {
-    Passing int
-    Failing int
+type Channel struct {
+    Release string
+    Logs string
 }
 
-var jobs ListJobs
 var is_csv bool
-var release string
-
 func parseArgs() {
-        // Default job values
-    jobs = ListJobs{}
-
-    flag.Var(&jobs, "job", "List of jobs to collect tempest result")
     flag.BoolVar(&is_csv, "csv", false, "Show csv output")
-    flag.StringVar(&release, "release", "master", "Release")
     flag.Parse()
 }
 
 func main() {
-
     parseArgs()
+    jobs := getJobs()
+    tests := make(map[string]map[string][]int)
 
-    tests := make(map[string]*TestResult)
-    lastTen := make(map[string]*TestResult)
-    builds := make(map[string][]Builds)
-
-    var sumBuilds, lastTenBuilds []Builds
-
-    for _, job := range jobs {
-        jobSlice := 10
-        buildsJob := getBuilds(job)
-        builds[job] = buildsJob
-        if len(buildsJob) < 10 {
-            jobSlice = len(buildsJob) - 1
-        }
-        lastTenBuilds = append(lastTenBuilds, buildsJob[:jobSlice]...)
-        sumBuilds = append(sumBuilds, buildsJob...)
-    }
-
-    collectData(sumBuilds, tests)
-    collectData(lastTenBuilds, lastTen)
-
-    if is_csv {
-        reportCSV(tests, lastTen)
-    } else {
-        reportTable(tests, lastTen)
-    }
+    collectData(jobs, tests)
+    reportCSV(tests)
+    // NOTE(arxcruz): Removing the logic behind csv and table, because go-pretty
+    // table is having problem to compile in the old container, but keeping
+    // here commented so we don't need to look again how to do it.
+    // if is_csv {
+    //     reportCSV(tests)
+    // } 
+    // else {
+    //     reportTable(tests)
+    // }
 }
 
-func loadYaml(yamlPath string) (*SkipTests, error) {
-    skipTests := &SkipTests{}
-
-    file, err := os.Open(yamlPath)
-    if err != nil {
-        return nil, err
-    }
-
-    defer file.Close()
-
-    d := yaml.NewDecoder(file)
-    if err := d.Decode(&skipTests); err != nil {
-        return nil, err
-    }
-    return skipTests, nil
-}
-
-func collectData(builds []Builds, tests map[string]*TestResult) {
-    ch := make(chan string)
+func collectData(jobs []Job, tests map[string]map[string][]int) {
+    ch := make(chan Channel)
     size := 0
-    for _, build := range builds {
-        if build.Status != "SKIPPED" {
-            go fetchLogs(build.URL, ch)
+    for _, job := range jobs {
+        if job.Status != "SKIPPED" {
+            go fetchLogs(job.Release, job.URL, ch)
             size++
         }
     }
@@ -127,38 +79,39 @@ func collectData(builds []Builds, tests map[string]*TestResult) {
     for i := 0; i < size; i++ {
         results := parseResults(ch)
         for _, result := range results {
-            if tests[result[0]] == nil {
-                tests[result[0]] = &TestResult{0, 0}
+            testname, testresult, release := result[0], result[1], result[2]
+
+            if tests[release] == nil {
+                tests[release] = make(map[string][]int)
             }
-            if result[1] == "FAILED" {
-                tests[result[0]].Failing++
+            if tests[release][testname] == nil {
+                    tests[release][testname] = []int{}
+            }
+
+            result := tests[release][testname]
+            if testresult == "FAILED" {
+                result = append(result, 0)
             } else {
-                tests[result[0]].Passing++
+                result = append(result, 1)
             }
+
+            tests[release][testname] = result
         }
     }
 }
 
-func reportCSV(results, lastTen map[string]*TestResult) {
+func reportCSV(tests map[string]map[string][]int) {
 
     w := csv.NewWriter(os.Stdout)
 
-    for key, value := range results {
-        lastTenPassing := 0
-        lastTenFailing := 0
-        if lastTen[key] != nil {
-            lastTenPassing = lastTen[key].Passing
-            lastTenFailing = lastTen[key].Failing
-        }
+    for release, results := range tests {
+        for key, value := range results {
+            passing, failing, lastPassing, lastFailing := sum_results(value)
+            record := []string{"0", release, key, passing, failing, lastPassing, lastFailing}
 
-        record := []string{"0", release, key,
-                           strconv.Itoa(value.Passing),
-                           strconv.Itoa(value.Failing),
-                           strconv.Itoa(lastTenPassing),
-                           strconv.Itoa(lastTenFailing)}
-
-        if err := w.Write(record); err != nil {
-            log.Fatalln("Error while writing record to csv: ", err)
+            if err := w.Write(record); err != nil {
+                log.Fatalln("Error while writing record to csv: ", err)
+            }
         }
     }
 
@@ -168,55 +121,88 @@ func reportCSV(results, lastTen map[string]*TestResult) {
     }
 }
 
-func reportTable(results, lastTen map[string]*TestResult) {
-    t := table.NewWriter()
-    t.SetOutputMirror(os.Stdout)
-    t.AppendHeader(table.Row{"Test name", "Passing", "Failing", "Last 10 passing", "Last 10 failing"})
-
-    for key, value := range results {
-        if lastTen[key] == nil {
-            lastTen[key] = &TestResult{}
-        }
-        t.AppendRow([]interface{}{key, value.Passing, value.Failing, lastTen[key].Passing, lastTen[key].Failing})
+func Reverse(input []int) []int {
+    var output []int
+    for i := len(input) - 1; i >= 0; i-- {
+        output = append(output, input[i])
     }
-
-    t.Render()
+    return output
 }
 
-func fetchLogs(url string, ch chan<- string) {
+func sum_results(results []int) (string, string, string, string) {
+    sum := 0
+    passing := 0
+    lastPassing := 0
+    lastFailing := 0
+    // NOTE(dasm): Reverse array and count last x indices
+    for index, value := range Reverse(results) {
+        passing += value
+        sum += 1
+        if index < limit {
+            lastPassing += value
+            if value == 0 {
+                lastFailing += 1
+            }
+        }
+    }
+    failing := sum - passing
+    return strconv.Itoa(passing), strconv.Itoa(failing), strconv.Itoa(lastPassing), strconv.Itoa(lastFailing)
+}
+
+/* Commenting the reportTable function
+
+func reportTable(tests map[string]map[string][]int) {
+    t := table.NewWriter()
+    t.SetOutputMirror(os.Stdout)
+    t.AppendHeader(table.Row{"Release", "Test name", "Passing", "Failing", "Last 10 passing", "Last 10 failing"})
+
+    for release, results := range tests {
+        for key, value := range results {
+            passing, failing, lastPassing, lastFailing := sum_results(value)
+            t.AppendRow([]interface{}{release, key, passing, failing, lastPassing, lastFailing})
+        }
+    }
+    t.SortBy([]table.SortBy{
+        {Name: "Release"},
+        {Name: "Test name"},
+    })
+    t.Render()
+
+} */
+
+func fetchLogs(release strslice, url string, ch chan Channel) {
     path := "logs/undercloud/var/log/tempest/tempest_run.log.txt.gz"
     fullURL := fmt.Sprintf("%s%s", url, path)
 
     response, err := http.Get(fullURL)
     if err != nil {
-        ch <- fmt.Sprintf("Error in get %s: %v", fullURL, err)
+        channel := Channel{string(release), fmt.Sprintf("Error in get %s: %v", fullURL, err)}
+        ch <- channel
         return
     }
 
     if response.StatusCode == 404 {
-        // Some releases still using validate-tempest
-        path := "logs/undercloud/home/zuul/tempest.log.txt.gz"
-        fullURL := fmt.Sprintf("%s%s", url, path)
-        response, err = http.Get(fullURL)
-        if err != nil {
-            ch <- fmt.Sprintf("Error in get %s: %v", fullURL, err)
-            return
-        }
+        // NOTE(dasm): Tempest didn't start
+        channel := Channel{string(release), fmt.Sprintf("Error in get %s: %v", fullURL, err)}
+        ch <- channel
+        return
     }
 
     data, err := ioutil.ReadAll(response.Body)
     if err != nil {
-        ch <- fmt.Sprintf("Error while reading data from %s: %v", fullURL, err)
+        channel := Channel{string(release), fmt.Sprintf("Error while reading data from %s: %v", fullURL, err)}
+        ch <- channel
         return
     }
 
-    ch <- string(data)
+    channel := Channel{string(release), string(data)}
+    ch <- channel
 }
 
-func getBuilds(jobName string) []Builds {
-    var url = "https://review.rdoproject.org/zuul/api/builds?job_name=%s"
-    var builds []Builds
-    response, err := http.Get(fmt.Sprintf(url, jobName))
+func getJobs() []Job {
+    var url = "https://review.rdoproject.org/zuul/api/builds?pipeline=openstack-periodic-weekend&limit=150"
+    var builds []Job
+    response, err := http.Get(url)
     if err != nil {
         log.Fatalf("The HTTP request failed with error %s\n", err)
     } else {
@@ -228,11 +214,15 @@ func getBuilds(jobName string) []Builds {
     return builds
 }
 
-func parseResults(ch chan string) [][2]string {
-    r, _ := regexp.Compile(`\{\d\}\s(setUpClass\s)?\(?(?P<test>[^\(].*[^\)])(\))?\s\[\d.*\.\d.*\]\s...\s(?P<status>ok|FAILED)`)
-    var result [][2]string
-    for _, matches := range r.FindAllStringSubmatch(<-ch, -1) {
-        result = append(result, [2]string{matches[2], matches[4]})
+func parseResults(ch chan Channel) [][3]string {
+    r, _ := regexp.Compile(`\{\d\}\s(setUpClass|tearDownClass\s)?\(?(?P<test>[^\(].*[^\)])(\))?\s\[\d.*\.\d.*\]\s...\s(?P<status>ok|FAILED)`)
+    var result [][3]string
+
+    channel := <-ch
+    data, release := channel.Logs, channel.Release
+
+    for _, matches := range r.FindAllStringSubmatch(data, -1) {
+        result = append(result, [3]string{matches[2], matches[4], release})
     }
     return result
 }
