abcb0ebd1e33a2980483e6fe0a46f7b63d7199bd
Amol Kahat <amolkahat@gmail.com>
2022-10-31 19:54:31 +0530

    Remove RDO registry traces from promoter
    This patch removes RDO registry uses from
    promoter server.
    
    Removed unused old jobs from copy-quay config.
    
    Removed old release from copy-rdo.sh, added Zed release.
    
    See topic[1] for more info.
    
    [1] https://review.rdoproject.org/r/q/topic:rdo_registry_decomission
    
    Signed-off-by: Amol Kahat <amolkahat@gmail.com>
    Change-Id: I659ebebdd5a1281ad71356fde97c26837de5dbc3

--- ci-scripts/copy-containers/copy-rdo.sh
+++ ci-scripts/copy-containers/copy-rdo.sh
@@ -2,26 +2,26 @@
 
 copy_quay() {
     HASH=$(curl "https://trunk.rdoproject.org/api-$1/api/promotions?promote_name=current-tripleo&limit=1" 2>/dev/null | jq -r '.[0].aggregate_hash')
-    /usr/local/bin/copy-quay --config /root/copy-quay/config.yaml --pull-registry quay.io --push-registry trunk.registry.rdoproject.org --token "$TOKEN" --push-hash "$HASH" --hash "$HASH" --release "$2" copy
+    /usr/local/bin/copy-quay --config /root/copy-quay/config.yaml --pull-registry quay.io --push-registry quay.rdoproject.org --token "$TOKEN" --push-hash "$HASH" --hash "$HASH" --release "$2" copy
 }
 # Check if podman is logged
-podman login --get-login trunk.registry.rdoproject.org
+podman login --get-login quay.rdoproject.org
 if [ "$?" -gt "0" ]; then
-    podman login trunk.registry.rdoproject.org --username "$RDO_USERNAME" --password "$RDO_PASSWORD"
+    podman login quay.rdoproject.org --username "$RDO_USERNAME" --password "$RDO_PASSWORD"
 fi
 
 
 # Master Centos 9
 copy_quay centos9-master-uc mastercentos9
 
+# Zed Centos 9
+copy_quay centos9-zed zedcentos9
+
 # Wallaby Centos 8
 copy_quay centos8-wallaby wallaby
 
 # Wallaby Centos 9
 copy_quay centos9-wallaby wallabycentos9
-
-# Victoria Centos 8
-copy_quay centos8-victoria victoria
 
 # Train Centos 8
 copy_quay centos8-train train8
