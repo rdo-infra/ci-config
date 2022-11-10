558aa3b2bb3e3e8b209af7bd559b2919c279e988
Jakob Meng <code@jakobmeng.de>
2022-11-10 09:45:47 +0100

    Prepare support for multiple OpenStack projects
    Change-Id: I3bce238949f735097cab810829776cfbd67e7bd8

--- ci-scripts/infra-setup/tests/nuke.sh
+++ ci-scripts/infra-setup/tests/nuke.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-ansible-playbook -vv -i ../inventories/inventory.ini -e @nuke.yml ../teardown.yml
+ansible-playbook -vv -i ../inventory/ -e cloud="vexxhost" -e @nuke.yml ../teardown.yml
