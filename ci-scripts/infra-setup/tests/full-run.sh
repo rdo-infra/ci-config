e3947a4d3680d5c1122f057b87072f94a68c52f4
Jakob Meng <code@jakobmeng.de>
2022-11-10 09:45:47 +0100

    Prepare support for multiple OpenStack projects
    Change-Id: I3bce238949f735097cab810829776cfbd67e7bd8

--- ci-scripts/infra-setup/tests/full-run.sh
+++ ci-scripts/infra-setup/tests/full-run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-ansible-playbook -vvvv -i ../inventories/inventory.ini ../full-run.yml -e bastion_private_key="$1" -e default_flavor=m1.medium
+ansible-playbook -vvvv -i ../inventory/ ../full-run.yml -e cloud="vexxhost" -e bastion_private_key="$1" -e default_flavor=m1.medium
