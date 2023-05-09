63482d695d2b8673203763546bd60f6b1800c042
Ronelle Landy <rlandy@redhat.com>
2023-05-09 16:47:19 -0400

    Remove Zed from the promoter
    Per the decision to not maintain TripleO after
    the Zed release [1], and Zed is not an imported
    release, the Zed integration line is being removed.
    
    This patch removes the Zed release from
    the list of releases included in the promoter.
    
    [1] https://review.opendev.org/c/openstack/governance/+/878799
    
    Depends-On: https://review.rdoproject.org/r/c/rdo-jobs/+/48504
    Change-Id: I7c504b22db7bd29ac97dd578604e3a70a8e21e84

--- ci-scripts/dlrnapi_promoter/dlrn-promoter.sh
+++ ci-scripts/dlrnapi_promoter/dlrn-promoter.sh
@@ -21,8 +21,7 @@ PROMOTER_TYPE="${PROMOTER_TYPE:=upstream}"
 if [[ $PROMOTER_TYPE == "upstream" ]]; then
     DEFAULT_RELEASES=( "CentOS-8/wallaby" \
                        "CentOS-8/train" \
-                       "CentOS-9/wallaby" \
-                       "CentOS-9/zed" )
+                       "CentOS-9/wallaby" )
 else
     DEFAULT_RELEASES=( "RedHat-8/rhos-16.2" "RedHat-8/rhos-17.1" \
                        "RedHat-9/rhos-17" "RedHat-9/rhos-17.1" \
