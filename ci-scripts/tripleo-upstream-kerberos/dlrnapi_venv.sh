364960431ed65373a28049b516c37ab805bcb3f8
Enrique Vallespi Gil <evallesp@redhat.com>
2023-07-03 17:06:50 +0200

    Adds kerberos version of dlrnapi_venv.
    Change-Id: Idde45726ab50f0dd3231381036afecdc47fc4cb1

---
+++ ci-scripts/tripleo-upstream-kerberos/dlrnapi_venv.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+function activate_dlrnapi_venv {
+    if [ ! -d "$WORKSPACE"/dlrnapi_venv ]; then
+        if [ "$(command -v virtualenv)" ]; then
+            virtualenv --system-site-packages "$WORKSPACE"/dlrnapi_venv
+        else
+            python3 -m venv  --system-site-packages "$WORKSPACE"/dlrnapi_venv
+        fi
+    fi
+    source "$WORKSPACE"/dlrnapi_venv/bin/activate
+    pip install -U dlrnapi-client[kerberos] shyaml
+}
+
+function deactivate_dlrnapi_venv {
+    # deactivate can fail with unbound variable, so we need +u
+    set +u
+    [[ "$VIRTUAL_ENV" = "$WORKSPACE"/dlrnapi_venv ]] && deactivate
+}
