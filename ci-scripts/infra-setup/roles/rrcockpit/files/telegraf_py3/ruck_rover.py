b50bfb6ad52300243876113b1a247e7cff2c0805
Douglas Viroel <viroel@gmail.com>
2023-07-21 13:06:12 -0300

    Add kerberos auth to ruck_rover script
    The ruck_rover.py script now supports kerberos
    authenticatin method. This is being automatically set
    when --release is a OSP release.
    No extra cli parameters where added since we only have
    kerberos auth for downstream DLRN for now.
    The user needs to authenticated with IPA server to
    support kerberos authentication method.
    The configuration file also needs to provide the proper
    kerberos principal configuration for downstream:
    
    downstream:
      dlrnapi_krb_principal: HTTP/principal@ipa.server
    
    Change-Id: Idb404b29ea6887a00078c1f7d81324fe84f1146e

--- ci-scripts/infra-setup/roles/rrcockpit/files/telegraf_py3/ruck_rover.py
+++ ci-scripts/infra-setup/roles/rrcockpit/files/telegraf_py3/ruck_rover.py
@@ -31,6 +31,11 @@ CERT_PATH = os.environ.get(
                 'CURL_CA_BUNDLE',
                 '/etc/pki/ca-trust/extracted/openssl/ca-bundle.trust.crt')
 dlrnapi_client.configuration.ssl_ca_cert = CERT_PATH
+# DLRN client configurations
+DLRN_AUTH_METHOD_BASIC = "basicAuth"
+DLRN_AUTH_METHOD_KRB = "kerberosAuth"
+dlrn_auth_method = DLRN_AUTH_METHOD_BASIC
+dlrn_force_auth = False
 
 MATRIX = {
     "centos-8": ["wallaby", "train"],
@@ -161,7 +166,9 @@ def fetch_hashes_from_commit_yaml(criteria):
 
 
 def find_results_from_dlrn_agg(api_url, test_hash):
-    api_client = dlrnapi_client.ApiClient(host=api_url)
+    api_client = dlrnapi_client.ApiClient(host=api_url,
+                                          auth_method=dlrn_auth_method,
+                                          force_auth=dlrn_force_auth)
     api_instance = dlrnapi_client.DefaultApi(api_client)
     params = dlrnapi_client.AggQuery(aggregate_hash=test_hash)
     api_response = api_instance.api_agg_status_get(params=params)
@@ -259,8 +266,9 @@ def get_dlrn_promotions(api_url, promotion_name, component=None):
     [2]: https://dlrn.readthedocs.io/en/latest/api.html#get-api-promotions
     """
     logging.debug("Getting promotion %s for %s", promotion_name, api_url)
-
-    api_client = dlrnapi_client.ApiClient(host=api_url)
+    api_client = dlrnapi_client.ApiClient(host=api_url,
+                                          auth_method=dlrn_auth_method,
+                                          force_auth=dlrn_force_auth)
     api_instance = dlrnapi_client.DefaultApi(api_client)
     query = dlrnapi_client.PromotionQuery(
         promote_name=promotion_name,
@@ -288,7 +296,9 @@ def find_results_from_dlrn_repo_status(api_url, commit_hash,
         :return api_response: from dlrnapi server containing result of
          passing/failing jobs
     """
-    api_client = dlrnapi_client.ApiClient(host=api_url)
+    api_client = dlrnapi_client.ApiClient(host=api_url,
+                                          auth_method=dlrn_auth_method,
+                                          force_auth=dlrn_force_auth)
     api_instance = dlrnapi_client.DefaultApi(api_client)
     params = dlrnapi_client.Params2(commit_hash=commit_hash,
                                     distro_hash=distro_hash,
@@ -963,20 +973,30 @@ def component_influx(config, distro, release, stream, test_component):
 @click.command()
 def main(release, distro, config_file, promotion_name, aggregate_hash,
          component, influx, verbose):
+    global dlrn_auth_method
+    global dlrn_force_auth
 
     if verbose:
         fmt = '%(asctime)s:%(levelname)s - %(funcName)s:%(lineno)s %(message)s'
         logging.basicConfig(format=fmt, encoding='utf-8', level=logging.DEBUG)
 
     stream = 'upstream'
-    if release in ('osp16-2', 'osp17', 'osp17-1', 'osp18'):
+    if release.startswith('osp'):
         stream = 'downstream'
+        # Downstream DLRN is KerberosAuth only
+        dlrn_auth_method = DLRN_AUTH_METHOD_KRB
+        dlrn_force_auth = True
         if config_file != os.path.dirname(__file__) + '/conf_ruck_rover.yaml':
             print('using custom config file: {}'.format(config_file))
         else:
             downstream_urls = 'https://url.corp.redhat.com/ruck-rover-0'
             config_file = download_file(downstream_urls)
     config = load_conf_file(config_file)
+
+    if dlrn_auth_method == DLRN_AUTH_METHOD_KRB:
+        dlrnapi_client.configuration.server_principal = (
+            config[stream]['dlrnapi_krb_principal']
+        )
 
     distros = REVERSED_MATRIX[release]
     releases = MATRIX[distro]
